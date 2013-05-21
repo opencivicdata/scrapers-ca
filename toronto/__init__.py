67a32c32a85872f512929f293cced478d6d478e6
James McKinney <james@slashpoundbang.com>
2013-05-20 20:33:05 -0400

    fix whitespace

--- toronto/__init__.py
+++ toronto/__init__.py
@@ -29,7 +29,7 @@ class Toronto(Jurisdiction):
     if scraper_type == 'person':
         return TorontoPersonScraper
 
-  def scrape_session_list(self): 
+  def scrape_session_list(self):
     page = self.lxmlize('http://app.toronto.ca/tmmis/findAgendaItem.do?function=doPrepare')
     return page.xpath("//select[@id='termId']//option[position()>1]/text()")
 
@@ -47,8 +47,9 @@ class TorontoPersonScraper(Scraper):
 
   def get_people(self):
     yield self.toronto_scrape_people
+
   def toronto_scrape_people(self):
-    page = self.lxmlize(COUNCIL_PAGE) 
+    page = self.lxmlize(COUNCIL_PAGE)
     table = page.xpath('//table[3]//td/font/a')
     for index, element in enumerate(table):
       url = element.attrib['href']
@@ -58,28 +59,29 @@ class TorontoPersonScraper(Scraper):
       else:
         p = self.scrape_councilor(url)
         yield p
+
   def scrape_councilor(self, url):
     page = self.lxmlize(url)
     info = page.xpath("//div[@class='main']")[0]
     name = info.xpath("//h3")[1].text_content().replace('Councillor','').strip()
     district = info.xpath("//p")[0].text_content()
     p = Legislator(name=name, district=district)
-    
+
     info = info.xpath("//div[@class='last']")[0]
 
     # add links
     p.add_source(url)
     p.add_source(COUNCIL_PAGE)
-     
+
     if "website:" in info.text_content():
       p.add_link(info.xpath('.//a')[1].attrib['href'], 'homepage')
 
     if "Facebook" in info.text_content():
       p.add_link(info.xpath('//a[contains(@href, "facebook.com")]')[0].attrib['href'],'facebook')
-   
+
     if "Twitter" in info.text_content():
-      p.add_link(info.xpath('//a[contains(@href,"twitter.com")]')[0].attrib['href'],'twitter') 
-    
+      p.add_link(info.xpath('//a[contains(@href,"twitter.com")]')[0].attrib['href'],'twitter')
+
     # add contact info
     p.add_contact('email', info.xpath('.//a')[0].text_content(),'')
    #//*[@id="content"]/div/div[1]/div[2]/p[1]
@@ -91,27 +93,24 @@ class TorontoPersonScraper(Scraper):
       if len(contact) >= 4:
         address = (contact[0]+", "+contact[2]).strip()
         p.add_contact('address',address,note)
-        if "Phone: " in contact[4]: 
+        if "Phone: " in contact[4]:
           phone = contact[4].replace("Phone: ",'').strip()
           p.add_contact('phone',phone,note)
-        if len(contact) > 5 and "Fax:" in contact[6]: 
+        if len(contact) > 5 and "Fax:" in contact[6]:
           fax = contact[6].replace("Fax: ",'').strip()
-          p.add_contact('fax',fax,note) 
-      else: 
+          p.add_contact('fax',fax,note)
+      else:
         phone = contact[0].strip()
         p.add_contact('phone',phone,note)
         fax = contact[2].strip()
         p.add_contact('fax',fax,note)
-
-
-    
 
   def scrape_mayor(self):
     url = 'http://www1.toronto.ca/wps/portal/contentonly?vgnextoid=e53332d0b6d1e310VgnVCM10000071d60f89RCRD&vgnextfmt=default'
     page = self.lxmlize(url)
     name = page.xpath("//div[@class='detail']//h1/text()")[0].replace("Toronto Mayor","").strip()
     p = Legislator(name,"Toronto")
-    
+
     p.add_source(COUNCIL_PAGE)
     p.add_source(url)
 
@@ -122,7 +121,7 @@ class TorontoPersonScraper(Scraper):
     info = page.xpath('//div[@class="detail"]')[0]
     address = (', ').join(info.xpath('.//p/text()')[0:6]).replace(",,",",")
     phone = info.xpath('.//p[3]/text()')[0]
-    
+
     p.add_contact('address',address,'Mailing')
     p.add_contact('phone',phone,'')
 
