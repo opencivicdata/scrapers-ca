77c3cad6c6b08d969e4ff395c27ff65364d11dd3
agarrow <alexio2@mac.com>
2013-08-01 11:06:31 -0400

    grand_prarie_no_1 people scraper

---
+++ grand_prarie_no_1/__init__.py
@@ -0,0 +1,45 @@
+from pupa.scrape import Jurisdiction
+
+# from .events import TorontoEventScraper
+from .people import Grand_Prarie_No_1PersonScraper
+# from .votes import TorontoVoteScraper
+from .utils import lxmlize
+
+import re
+
+class Grand_Prarie_No_1(Jurisdiction):
+  jurisdiction_id = 'ca-ab-grand_prarie_no_1'
+
+  def get_metadata(self):
+    return {
+      'name': 'Grand_Prarie_No_1',
+      'legislature_name': 'Grand_Prarie_No_1 City Council',
+      'legislature_url': 'http://cms.burlington.ca/Page110.aspx',
+      'terms': [{
+        'name': '2010-2014',
+        'sessions': ['2010-2014'],
+        'start_year': 2010,
+        'end_year': 2014,
+      }],
+      'provides': ['people'],
+      'parties': [],
+      'session_details': {
+        '2010-2014': {
+          '_scraped_name': '2010-2014',
+        }
+      },
+      'feature_flags': [],
+      # '_ignored_scraped_sessions': ['2006-2010'],
+    }
+
+  def get_scraper(self, term, session, scraper_type):
+    # if scraper_type == 'events':
+    #     return TorontoEventScraper
+    if scraper_type == 'people':
+        return Grand_Prarie_No_1PersonScraper
+    # if scraper_type == 'votes':
+    #     return TorontoVoteScraper
+
+  def scrape_session_list(self):
+    return ['2010-2014']
+    
---
+++ grand_prarie_no_1/people.py
@@ -0,0 +1,42 @@
+from pupa.scrape import Scraper, Legislator
+from larvae.person import Person
+from larvae.organization import Organization
+
+from .utils import lxmlize
+
+import re
+
+COUNCIL_PAGE = 'http://www.countygp.ab.ca/EN/main/government/council.html'
+
+class Grand_Prarie_No_1PersonScraper(Scraper):
+
+  def get_people(self):
+    page = lxmlize(COUNCIL_PAGE)
+
+    councillors = page.xpath('//table[@class="table-plain"]/tbody/tr/td[2]')
+    for councillor in councillors:
+      name = councillor.xpath('./h2')[0].text_content().split('Division')[0]
+      district = re.findall(r'(Division [0-9])', councillor.xpath('./h2')[0].text_content())[0]
+      
+      p = Legislator(name=name, district=district)
+      p.add_source(COUNCIL_PAGE)
+
+      address = councillor.xpath('./p[1]')[0].text_content()
+      email = councillor.xpath('.//a[contains(@href, "mailto:")]')[0].text_content()
+      
+      p.add_contact('address', address, None)
+      p.add_contact('email', email, None)
+
+      numbers = councillor.xpath('./p[2]')[0].text_content().replace('Email: ','').replace(email,'').split(':')
+      for index, number in enumerate(numbers):
+        if index == 0:
+          continue
+        contact_type = re.findall(r'[A-Za-z]+', numbers[index-1])[0]
+        number = re.findall(r'[0-9]{3}.[0-9]{3}.[0-9]{4}', number)[0].replace('.','-')
+        if contact_type == 'Fax':
+          p.add_contact('fax', number, None)
+        else:
+          p.add_contact('phone', number, contact_type)
+      
+
+ 
