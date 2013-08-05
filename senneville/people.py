2d04a93241708873122e3a4e22bdd2e870fac895
agarrow <alexio2@mac.com>
2013-08-05 17:04:23 -0400

    senneville people scraper

---
+++ senneville/people.py
@@ -0,0 +1,26 @@
+from pupa.scrape import Scraper, Legislator
+from larvae.person import Person
+from larvae.organization import Organization
+
+from .utils import lxmlize
+
+import re
+
+COUNCIL_PAGE = 'http://www.villagesenneville.qc.ca/fr/membres-du-conseil-municipal'
+
+class SennevillePersonScraper(Scraper):
+
+  def get_people(self):
+    page = lxmlize(COUNCIL_PAGE)
+
+    councillors = page.xpath('//div[@class="field-item even"]//tr')
+    for councillor in councillors:
+      district = councillor.xpath('./td[1]//strong/text()')[0]
+      if 'Maire' in district:
+        district = 'senneville'
+      name = councillor.xpath('./td[2]//strong/text()')[0].lower()
+      email = councillor.xpath('.//a/text()')[0]
+      p = Legislator(name=name, district=district)
+      p.add_source(COUNCIL_PAGE)
+      p.add_contact('email', email, None)
+      yield p
