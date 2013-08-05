3feeed8d2260e85e539224ad4dcd2ebd2245f434
agarrow <alexio2@mac.com>
2013-08-05 13:07:22 -0400

    newmarket people scraper

---
+++ newmarket/__init__.py
@@ -0,0 +1,45 @@
+from pupa.scrape import Jurisdiction
+
+# from .events import TorontoEventScraper
+from .people import NewmarketPersonScraper
+# from .votes import TorontoVoteScraper
+from .utils import lxmlize
+
+import re
+
+class Newmarket(Jurisdiction):
+  jurisdiction_id = 'ca-on-newmarket'
+
+  def get_metadata(self):
+    return {
+      'name': 'Newmarket',
+      'legislature_name': 'Newmarket City Council',
+      'legislature_url': 'http://www.newmarket.ca/en/townhall/contactinformationmayorandtowncouncil.asp',
+      'terms': [{
+        'name': '2010-2014',
+        'sessions': ['2010-2014'],
+        'start_year': 2010,
+        'end_year': 2014,
+      }],
+      'provides': ['people'],
+      'parties': [],
+      'session_details': {
+        '2010-2014': {
+          '_scraped_name': '2010-2014',
+        }
+      },
+      'feature_flags': [],
+      # '_ignored_scraped_sessions': ['2006-2010'],
+    }
+
+  def get_scraper(self, term, session, scraper_type):
+    # if scraper_type == 'events':
+    #     return TorontoEventScraper
+    if scraper_type == 'people':
+        return NewmarketPersonScraper
+    # if scraper_type == 'votes':
+    #     return TorontoVoteScraper
+
+  def scrape_session_list(self):
+    return ['2010-2014']
+    
---
+++ newmarket/people.py
@@ -0,0 +1,81 @@
+from pupa.scrape import Scraper, Legislator
+from larvae.person import Person
+from larvae.organization import Organization
+
+from .utils import lxmlize
+
+import re
+
+COUNCIL_PAGE = 'http://www.newmarket.ca/en/townhall/contactinformationmayorandtowncouncil.asp'
+
+class NewmarketPersonScraper(Scraper):
+
+  def get_people(self):
+    page = lxmlize(COUNCIL_PAGE)
+
+    councillors = page.xpath('//div[@id="printArea"]//table//tr//td')[4:-1]
+    for councillor in councillors:
+      if councillor == councillors[0]:
+        yield self.scrape_mayor(councillor)
+        continue
+      name = councillor.xpath('.//a/text()')[0]
+      district = councillor.xpath('.//strong/text()')[1].replace('Councillor- ', '')
+      district = district if not 'Regional' in district else 'newmarket'
+      url = councillor.xpath('.//a/@href')[0]
+      
+      p = Legislator(name=name, district=district)
+      p.add_source(COUNCIL_PAGE)
+      p.add_source(url)
+
+      page = lxmlize(url)
+      info = page.xpath('//div[@id="printArea"]')[0]
+      info = info.xpath('.//p[@class="heading"][2]/following-sibling::p')
+      address = info.pop(0).text_content().strip()
+      if not address:
+        address = info.pop(0).text_content().strip()
+      
+      if 'Ward' in info[0].text_content():
+        info.pop(0)
+
+      numbers = info.pop(0).text_content().split(':')
+      email = info.pop(0).xpath('.//a[contains(@href, "mailto:")]/text()')
+      if email:
+        p.add_contact('email', email[0], None)
+      for i, contact in enumerate(numbers):
+        if i==0:
+          continue
+        if '@' in contact:
+          email = re.findall(r'[a-z]+@.*\.ca', contact)[0]
+          p.add_contact('email', email, None)
+        else:
+          number = re.findall(r'([0-9]{3}-[0-9]{3}-[0-9]{4})', contact)[0]
+          ext = re.findall(r'(Ext\. [0-9]{3,4})', contact)
+          if ext:
+            number = number + ext[0].replace('Ext. ', ' x')
+          contact_type = re.findall(r'[A-Za-z]+$', numbers[i-1])[0]
+        if 'Fax' in contact_type:
+          p.add_contact('Fax', number, None)
+        elif 'Phone' in contact_type:
+          p.add_contact('Phone', number, None)
+        else:
+          p.add_contact('Phone', number, contact_type.lower())
+      site = page.xpath('.//a[contains(text(), "http://")]')
+      if site:
+        p.add_link(site[0].text_content(), 'personal site')
+      yield p
+
+  def scrape_mayor(self, div):
+    name = div.xpath('.//strong/text()')[0].replace(',', '')
+    p = Legislator(name=name, district='newmarket')
+    p.add_source(COUNCIL_PAGE)
+
+    numbers = div.xpath('.//p/text()')
+    for number in numbers:
+      num_type, number = number.split(':')
+      if 'Fax' in num_type:
+        p.add_contact('Fax', number, None)
+      else:
+        p.add_contact('Phone', number, num_type)
+    email = div.xpath('.//a[contains(@href, "mailto:")]')[0].text_content()
+    p.add_contact('email', email, None)
+    return p
