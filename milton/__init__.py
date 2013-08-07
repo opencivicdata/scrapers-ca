32e73fd0a124b9fec9cc2e8614e96dea3def7d98
agarrow <alexio2@mac.com>
2013-08-07 16:10:10 -0400

    milton people scraper

---
+++ milton/__init__.py
@@ -0,0 +1,45 @@
+from pupa.scrape import Jurisdiction
+
+# from .events import TorontoEventScraper
+from .people import MiltonPersonScraper
+# from .votes import TorontoVoteScraper
+from utils import lxmlize
+
+import re
+
+class Milton(Jurisdiction):
+  jurisdiction_id = 'ca-on-milton'
+
+  def get_metadata(self):
+    return {
+      'name': 'Milton',
+      'legislature_name': 'Milton City Council',
+      'legislature_url': 'http://www.milton.ca/en/townhall/mayorandcouncil.asp?_mid_=5972',
+      'terms': [{
+        'name': '2010-2014',
+        'sessions': ['2010-2014'],
+        'start_year': 2010,
+        'end_year': 2014,
+      }],
+      'provides': ['people'],
+      'parties': [],
+      'session_details': {
+        '2010-2014': {
+          '_scraped_name': '2010-2014',
+        }
+      },
+      'feature_flags': [],
+      # '_ignored_scraped_sessions': ['2006-2010'],
+    }
+
+  def get_scraper(self, term, session, scraper_type):
+    # if scraper_type == 'events':
+    #     return TorontoEventScraper
+    if scraper_type == 'people':
+        return MiltonPersonScraper
+    # if scraper_type == 'votes':
+    #     return TorontoVoteScraper
+
+  def scrape_session_list(self):
+    return ['2010-2014']
+    
---
+++ milton/people.py
@@ -0,0 +1,39 @@
+from pupa.scrape import Scraper, Legislator
+from larvae.person import Person
+from larvae.organization import Organization
+
+from utils import lxmlize
+
+import re
+
+COUNCIL_PAGE = 'http://www.milton.ca/en/townhall/mayorandcouncil.asp?_mid_=5972'
+
+class MiltonPersonScraper(Scraper):
+
+  def get_people(self):
+    page = lxmlize(COUNCIL_PAGE)
+
+    councillors = page.xpath('//table[@id="Table1table"]/tbody/tr')[1:]
+    for councillor in councillors:
+      name = councillor.xpath('./td[2]/p/text()')[1]
+      if len(councillor.xpath('./td[2]/p/text()'))<3:
+        district = 'milton'
+      else:
+        district = councillor.xpath('./td[2]/p/text()')[2]
+      
+      p = Legislator(name=name, district=district)
+      p.add_source(COUNCIL_PAGE)
+
+      if councillor == councillors[0]:
+        address = ', '.join(councillor.xpath('./td[3]/p[1]/text()')).replace('Email:','').strip()
+        p.add_contact('address', address, None)
+
+      numbers = councillor.xpath('./td[3]/p[2]/text()')
+      for number in numbers:
+        num_type, number = number.split(':')
+        number = number.replace(', ext ', ' x').strip()
+        if 'Fax' in num_type:
+          p.add_contact('fax', number, None)
+        else:
+          p.add_contact('phone', number, num_type)
+      yield p
