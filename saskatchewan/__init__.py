99e732b52ed6c54dbef68ad13062f35e3ec6a965
agarrow <alexio2@mac.com>
2013-08-08 16:49:16 -0400

    saskatchewan people scraper

---
+++ saskatchewan/__init__.py
@@ -0,0 +1,45 @@
+from pupa.scrape import Jurisdiction
+
+# from .events import TorontoEventScraper
+from .people import SaskatchewanPersonScraper
+# from .votes import TorontoVoteScraper
+from utils import lxmlize
+
+import re
+
+class Saskatchewan(Jurisdiction):
+  jurisdiction_id = 'ca-sk'
+
+  def get_metadata(self):
+    return {
+      'name': 'Saskatchewan',
+      'legislature_name': 'Saskatchewan City Council',
+      'legislature_url': 'http://www.municipal.gov.sk.ca/Programs-Services/Municipal-Directory-pdf',
+      'terms': [{
+        'name': '2010-2014',
+        'sessions': ['2010-2014'],
+        'start_year': 2010,
+        'end_year': 2014,
+      }],
+      'provides': ['people'],
+      'parties': [],
+      'session_details': {
+        '2010-2014': {
+          '_scraped_name': '2010-2014',
+        }
+      },
+      'feature_flags': [],
+      # '_ignored_scraped_sessions': ['2006-2010'],
+    }
+
+  def get_scraper(self, term, session, scraper_type):
+    # if scraper_type == 'events':
+    #     return TorontoEventScraper
+    if scraper_type == 'people':
+        return SaskatchewanPersonScraper
+    # if scraper_type == 'votes':
+    #     return TorontoVoteScraper
+
+  def scrape_session_list(self):
+    return ['2010-2014']
+    
