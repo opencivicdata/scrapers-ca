5e872f956856bbf085480dc231616a270d450cce
agarrow <alexio2@mac.com>
2013-08-09 15:38:09 -0400

    exception handling for scrape_people

--- scrape_people.py
+++ scrape_people.py
@@ -1,5 +1,11 @@
 import sys, os
 jurisdictions = os.listdir('.')
 jurisdictions = [x for x in jurisdictions if os.path.exists('./'+x+'/__init__.py')]
+errors = {}
 for jurisdiction in jurisdictions:
-  os.system("python -m pupa.cli update --people "+jurisdiction)
+  try:
+    os.system("python -m pupa.cli update --people "+jurisdiction)
+  except:
+    errors[jurisdiction] = sys.exc_info()[0]
+for key, value in errors.iteritems():
+  print key, ' failed with ', value
