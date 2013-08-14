8e993563745893a4bcc98fece44b2c3cba7e1e37
agarrow <alexio2@mac.com>
2013-08-14 15:11:43 -0400

    missed villeray-saint-michel-parc-extension

--- villeray-saint-michel-parc-extension/__init__.py
+++ villeray-saint-michel-parc-extension/__init__.py
@@ -9,7 +9,7 @@ import re
 
 class Villeray_Saint_Michel_Parc_Extension(Jurisdiction):
   jurisdiction_id = 'ca-qc-villeray-saint-michel-parc-extension'
-  geographic_code = 2466023
+
   def get_metadata(self):
     return {
       'name': 'Villeray-Saint-Michel-Parc-Extension',
