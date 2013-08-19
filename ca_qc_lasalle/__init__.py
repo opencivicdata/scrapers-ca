0d964836203b139f8bcfab5e536f16562db66c90
agarrow <alexio2@mac.com>
2013-08-19 16:23:03 -0400

    qc lasalle person scraper

---
+++ ca_qc_lasalle/__init__.py
@@ -0,0 +1,17 @@
+from ca_qc_montreal import MontrealPersonScraper
+from utils import CanadianJurisdiction
+
+class St_Laurent(CanadianJurisdiction):
+  jurisdiction_id = 'ca-qc-lasalle'
+
+  def _get_metadata(self):
+    return {
+      'name': 'LaSalle',
+      'legislature_name': 'LaSalle Borough Council',
+      'legislature_url': 'http://depot.ville.montreal.qc.ca/bd-elus/data.json',
+      'provides': ['people'],
+    }
+
+  def get_scraper(self, term, session, scraper_type):
+    if scraper_type == 'people':
+        return MontrealPersonScraper
