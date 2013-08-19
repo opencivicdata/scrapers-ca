c99c307d4a614a844248ee53fb1a93e83ff05ab8
agarrow <alexio2@mac.com>
2013-08-19 16:02:52 -0400

    markham people scraper

---
+++ ca_on_markham/__init__.py
@@ -0,0 +1,11 @@
+from utils import CanadianJurisdiction
+
+class Burlington(CanadianJurisdiction):
+  jurisdiction_id = 'ocd-jurisdiction/country:ca/csd:3519036/council'
+  geographic_code = 3519036
+  def _get_metadata(self):
+    return {
+      'name': 'Markham',
+      'legislature_name': 'Markham City Council',
+      'legislature_url': 'http://www.markham.ca/wps/portal/Markham/MunicipalGovernment/MayorAndCouncil/RegionalAndWardCouncillors/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOJN_N2dnX3CLAKNgkwMDDw9XcJM_VwCDUMDDfULsh0VAfz7Fis!/',
+    }
---
+++ ca_on_markham/people.py
@@ -0,0 +1,86 @@
+from pupa.scrape import Scraper, Legislator
+from pupa.models import Person
+from pupa.models import Organization
+
+from utils import lxmlize
+
+import re
+
+COUNCIL_PAGE = 'http://www.markham.ca/wps/portal/Markham/MunicipalGovernment/MayorAndCouncil/RegionalAndWardCouncillors/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOJN_N2dnX3CLAKNgkwMDDw9XcJM_VwCDUMDDfULsh0VAfz7Fis!/'
+
+class BurlingtonPersonScraper(Scraper):
+
+  def get_people(self):
+    page = lxmlize(COUNCIL_PAGE)
+
+    councillors = page.xpath('//div[@class="interiorContentWrapper"]//td')
+    for councillor in councillors:
+      if not councillor.text_content().strip() or "Address" in councillor.text_content():
+        continue
+      if not councillor.xpath('.//a'):
+        break
+      name = councillor.xpath('.//strong')[1].text_content().strip()
+      district = councillor.xpath('.//a//text()')[0].strip()
+      if 'Ward' in district:
+        district = district.replace('Councillor','')
+      else:
+        district = 'Markham'
+
+      url = councillor.xpath('.//a/@href')[0]
+      
+      if 'Ward 4' in district:
+        yield scrape_4(name, url)
+        continue
+
+      page = lxmlize(url)
+
+      p = Legislator(name=name, post_id=district)
+      p.add_source(COUNCIL_PAGE)
+      p.add_source(url)
+
+      contact = page.xpath('//div[@class="microSiteLinksWrapper"]')[1]
+      address = re.sub(r'\s{2,}' ,' ' , ' '.join(contact.xpath('.//p/text()')[:2])).strip()
+      phone = contact.xpath('.//p/text()')[2].split(':')[1].strip()
+      email = contact.xpath('.//a[contains(@href,"mailto:")]/text()')[0]
+      website = contact.xpath('.//a[not( contains(@href, "mailto:"))]/text()')
+      if website:
+        p.add_link(website[0], 'personal site')
+      p.add_contact('address', address, None)
+      p.add_contact('phone', phone, None)
+      p.add_contact('email', email, None)
+
+      get_links(p, contact)
+      yield p
+
+        
+
+def scrape_4(name, url):
+  page = lxmlize(url)
+
+  p = Legislator(name=name, post_id='Ward 4')
+  p.add_source(url)
+  p.add_source(COUNCIL_PAGE)
+
+  address = re.sub(r'\s{2,}' ,' ' , ' '.join(page.xpath('//div[@class="interiorContentWrapper"]/p[3]/text()')))
+  phone = page.xpath('//div[@class="interiorContentWrapper"]/p[4]/text()')[0].split(':')[1].strip()
+  email = page.xpath('//a[contains(@href, "mailto:")]/text()')[0]
+  p.add_contact('address', address, None)
+  p.add_contact('phone', phone, None)
+  p.add_contact('email', email, None)
+  return p
+
+def get_links(councillor, div):
+  links = div.xpath('.//a')
+  for link in links:
+    link = link.attrib['href']
+
+    if 'mailto:' in link:
+      continue
+    if 'facebook' in link:
+      councillor.add_link(link, 'facebook')
+    if 'twitter' in link:
+      councillor.add_link(link, 'twitter')
+    if 'linkedin' in link:
+      councillor.add_link(link, 'linkedin')
+    if 'google' in link:
+      councillor.add_link(link, 'google plus')
