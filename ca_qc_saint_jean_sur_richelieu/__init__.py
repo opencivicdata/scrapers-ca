f05dc99ae2c965f042f57cc189f363acf14174e5
James McKinney <james@slashpoundbang.com>
2013-08-21 14:55:49 -0400

    tidy saint jean

--- ca_qc_saint_jean_sur_richelieu/__init__.py
+++ ca_qc_saint_jean_sur_richelieu/__init__.py
@@ -1,11 +1,13 @@
 from utils import CanadianJurisdiction
 
-class Saint_Jean_Sur_Richelieu(CanadianJurisdiction):
+
+class SaintJeanSurRichelieu(CanadianJurisdiction):
   jurisdiction_id = 'ocd-jurisdiction/country:ca/csd:2456083/council'
   geographic_code = 2456083
+
   def _get_metadata(self):
     return {
       'name': 'Saint-Jean-sur-Richelieu',
       'legislature_name': 'Saint-Jean-sur-Richelieu City Council',
-      'legislature_url': 'http://www.ville.saint-jean-sur-richelieu.qc.ca/conseil-municipal/membres-conseil/Pages/membres-conseil.aspx',
+      'legislature_url': 'http://www.ville.saint-jean-sur-richelieu.qc.ca',
     }
