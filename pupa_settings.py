da7a9c702395d83eda2378a1567e068650f36135
James McKinney <james@slashpoundbang.com>
2014-01-09 17:33:07 -0500

    code review

--- pupa_settings.py
+++ pupa_settings.py
@@ -1,11 +1,11 @@
 import os
-if os.environ.get('MONGOHQ_URL'): # @todo
-    MONGO_HOST = os.environ['MONGOHQ_URL']
-    MONGO_DATABASE = 'app17409961'
-else:
-    MONGO_HOST = 'localhost'
-    MONGO_DATABASE = 'pupa'
+from urlparse import urlsplit
+url = os.getenv('MONGOHQ_URL', 'mongodb://localhost:27017/pupa')
+
+# @see https://raw.github.com/opencivicdata/pupa/master/pupa/core/default_settings.py
+MONGO_HOST = url
 MONGO_PORT = 27017
+MONGO_DATABASE = urlsplit(url).path[1:]
 
 SCRAPELIB_RPM = 60
 SCRAPELIB_TIMEOUT = 60
@@ -13,6 +13,8 @@ SCRAPELIB_RETRY_ATTEMPTS = 3
 SCRAPELIB_RETRY_WAIT_SECONDS = 20
 
 ENABLE_ELASTICSEARCH = False
+ELASTICSEARCH_HOST = 'localhost'
+ELASTICSEARCH_TIMEOUT = 2
 
 BILL_FILTERS = {}
 LEGISLATOR_FILTERS = {}
--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
-#!/usr/bin/env python
-from setuptools import setup
+# @see http://pythonhosted.org/an_example_pypi_project/setuptools.html
+# @see http://pythonhosted.org/setuptools/setuptools.html
+import os
+from setuptools import setup, find_packages
 
-long_description = open('README.md').read()
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name='mycityhall-scrapers',
-      version='0.0.1',
-      author='Open North Inc.',
-      author_email='info@opennorth.ca',
-      license='MIT',
-      url='https://github.com/opennorth/mycityhall-scrapers',
-      description='MyCityHall.ca Scrapers',
-      long_description=long_description,
-      platforms=['any'],
-      )
+setup(
+    name='scrapers_ca',
+    version='0.0.1',
+    author='Open North',
+    author_email='info@opennorth.ca',
+    description='Canadian legislative scrapers',
+    license='MIT',
+    url='https://github.com/opencivicdata/scrapers-ca',
+    packages=find_packages(),
+    long_description=read('README.md'),
+    install_requires=[
+        'lxml',
+    ]
+)
