6d66edecc78597164ef89bee26417fc595d6dc95
James McKinney <james@slashpoundbang.com>
2014-01-09 15:59:17 -0500

    set reasonable jurisdiction_id for ca_on_lambton

--- ca_on_lambton/__init__.py
+++ ca_on_lambton/__init__.py
@@ -2,7 +2,7 @@ from utils import CanadianJurisdiction
 
 
 class Lambton(CanadianJurisdiction):
-  jurisdiction_id = u'ca-on-lambton'
+  jurisdiction_id = u'ocd-jurisdiction/country:ca/cd:3538/council'
   division_name = 'Lambton'
   name = 'Lambton County Council'
   url = 'http://www.lambtononline.ca/home/government/accessingcountycouncil/countycouncillors/Pages/default.aspx'
