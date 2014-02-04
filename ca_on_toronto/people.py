63ba78163c1e8a73173d33eaa23d4f2c8bad16d0
James McKinney <james@slashpoundbang.com>
2014-02-04 15:58:33 -0500

    fix toronto people scraper

--- ca_on_toronto/people.py
+++ ca_on_toronto/people.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from pupa.scrape import Scraper, Legislator
 
 from utils import lxmlize
@@ -12,64 +13,79 @@ class TorontoPersonScraper(Scraper):
   def get_people(self):
     page = lxmlize(COUNCIL_PAGE)
 
-    a = page.xpath('//a[contains(@href,"mayor")]')[0]
-    yield self.scrape_mayor(a.attrib['href'])
-
     for a in page.xpath('//a[contains(@href,"councillors/")]'):
       if 'vacant' in a.attrib['href']:
         continue
       yield self.scrape_councilor(a.attrib['href'])
 
+    a = page.xpath('//a[contains(@href,"mayor")]')[0]
+    yield self.scrape_mayor(a.attrib['href'])
+
   def scrape_councilor(self, url):
     page = lxmlize(url)
-    info = page.xpath("//div[@class='main']")[0]
-    name = info.xpath("//h3")[1].text_content().replace('Councillor', '').strip()
-    if "Ana Bail" in name:
-      name = "Ana Bailao"
-    district = info.xpath("//p")[0].text_content()
+    info = page.xpath("//div[@class='single-item']")[0]
+    name = info.xpath(".//h1")[0].text_content().replace('Councillor', '').strip()
+    if name.startswith("Ana Bail"):
+      name = u"Ana Bailão"
+    district = info.xpath(".//p|.//h3")[0].text_content()
+    district = re.sub('\A(Ward \d+).+\Z', '\g<1>', district)
 
     p = Legislator(name=name, post_id=district, role='Councillor')
-
-    info = info.xpath("//div[@class='last']")[0]
-
-    # add links
     p.add_source(COUNCIL_PAGE)
     p.add_source(url)
 
-    p.image = page.xpath('//div[@class="two_column"]/div/img/@src')[0]
+    info = info.xpath(".//table")[0]
 
-    if "website:" in info.text_content():
-      p.add_link(info.xpath('.//a')[1].attrib['href'], None)
+    p.image = info.xpath('.//img/@src')[0]
 
     if "Facebook" in info.text_content():
-      p.add_link(info.xpath('//a[contains(@href, "facebook.com")]')[0].attrib['href'], None)
+      p.add_link(info.xpath('.//a[contains(@href, "facebook.com")]')[0].attrib['href'], None)
 
     if "Twitter" in info.text_content():
-      p.add_link(info.xpath('//a[contains(@href,"twitter.com")]')[0].attrib['href'], None)
+      p.add_link(info.xpath('.//a[contains(@href,"twitter.com")]')[0].attrib['href'], None)
 
     # add contact info
-    p.add_contact('email', info.xpath('.//a')[0].text_content(), None)
-   #//*[@id="content"]/div/div[1]/div[2]/p[1]
-    contacts = info.xpath('//div/p[text()[contains(.,"Phone:")]]')
-    for contact in contacts:
-      note = contact.xpath('.//strong')[0].text_content()
-      contact = contact.xpath('br/following-sibling::node()')
-      if len(contact) > 8:
-        continue
-      if len(contact) >= 4:
-        address = (contact[0] + ", " + contact[2]).strip()
-        p.add_contact('address', address, note)
-        if "Phone: " in contact[4]:
-          phone = contact[4].replace("Phone: ", '').strip()
-          p.add_contact('voice', phone, note)
-        if len(contact) > 5 and "Fax:" in contact[6]:
-          fax = contact[6].replace("Fax: ", '').strip()
-          p.add_contact('fax', fax, note)
+    p.add_contact('email', info.xpath('.//a[contains(@href,"mailto:")]')[0].text_content(), None)
+    contacts = info.xpath('.//p[text()[contains(.,"Phone:")]]')
+    for contact in contacts: # @todo
+      lines = [line.strip() for line in contact.xpath('.//text()')]
+
+      note = contact.getprevious()
+      if note is not None and len(note.xpath('.//text()')) == 1:
+        note = note.text_content()
       else:
-        phone = contact[0].strip()
-        p.add_contact('voice', phone, note)
-        fax = contact[2].strip()
-        p.add_contact('fax', fax, note)
+        note = lines.pop(0)
+
+      if note in ('Toronto City Hall', 'Office of the Deputy Mayor:'):
+        note = 'legislature'
+      elif note in ('Constituency Office', 'Constituency Office:', 'Community Office', 'East York Civic Centre Office'):
+        note = 'constituency'
+      else:
+        print 'Unexpected contact_details.note: %s' % note
+        raise Exception('Unexpected contact_details.note: %s' % note)
+
+      address_block = True
+      address_lines = []
+      for line in lines:
+        if line.startswith('Phone:'):
+          address_block = False
+          p.add_contact('voice', line.replace("Phone:", '').strip(), note)
+        elif line.startswith('Fax:'):
+          address_block = False
+          p.add_contact('fax', line.replace("Fax:", '').strip(), note)
+        elif line.startswith('Constituency Office:'):
+          address_block = False
+          p.add_contact('voice', line.replace("Phone:", '').strip(), 'constituency')
+        elif line.startswith('Constituency Fax:'):
+          address_block = False
+          p.add_contact('fax', line.replace("Fax:", '').strip(), 'constituency')
+        elif address_block:
+          address_lines.append(line)
+        else:
+          raise Exception('Unexpected contact detail: %s' % line)
+
+      if address_lines:
+        p.add_contact('address', '\n'.join(address_lines), note)
     return p
 
   def scrape_mayor(self, url):
@@ -83,11 +99,12 @@ class TorontoPersonScraper(Scraper):
     p.image = page.xpath('//div[@class="image"]/img/@src')[0]
 
     url = page.xpath('//a[contains(text(), "Contact the Mayor")]')[0].attrib['href']
+    url = url.replace('www.', 'www1.') # @todo fix lxmlize to use the redirected URL to make links absolute
     p.add_source(url)
     page = lxmlize(url)
 
     info = page.xpath('//div[@class="detail"]')[0]
-    address = (', ').join(info.xpath('.//p/text()')[0:6]).replace(",,", ",")
+    address = ''.join(info.xpath('.//p/text()')[0:6]).replace(",,", ",")
     phone = info.xpath('.//p[3]/text()')[0]
 
     p.add_contact('address', address, 'legislature')
--- utils.py
+++ utils.py
@@ -113,7 +113,6 @@ class AggregationLegislator(Person):
 def lxmlize(url, encoding='utf-8'):
   entry = urlopen(url).encode(encoding)
   page = lxml.html.fromstring(entry)
-
   meta = page.xpath('//meta[@http-equiv="refresh"]')
   if meta:
     _, url = meta[0].attrib['content'].split('=', 1)
