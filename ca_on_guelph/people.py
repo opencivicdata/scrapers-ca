a0799ab76c48e74974d97ad7b90cf004b0ba8eef
Thomas Rohée <roheethomas@gmail.com>
2014-02-04 21:54:31 -0500

    fixed mayor infos and councillors images

--- ca_on_guelph/people.py
+++ ca_on_guelph/people.py
@@ -38,7 +38,7 @@ class GuelphPersonScraper(Scraper):
 
       page = lxmlize(url)
 
-      p.image = page.xpath('//div[@class="entry-content"]/div/img/@src')[0]
+      p.image = page.xpath('//header/img/@src')[0]
 
       address = re.findall(r'Address: (.*)Phone', page.xpath('//div[@class="entry-content"]')[0].text_content())
       if address:
@@ -68,9 +68,9 @@ class GuelphPersonScraper(Scraper):
     email = div.xpath('.//a[contains(@href,"mailto:")]')[0].text_content()
 
     page = lxmlize(url)
-
-    address = re.findall(r'Address: (.*)Phone', page.xpath('//div[@class="entry-content"]')[0].text_content())[0]
-    fax = re.findall(r'Fax:(.*)Email', page.xpath('//div[@class="entry-content"]')[0].text_content())[0]
+    
+    address = re.findall(r'Address: (.*)\r', page.xpath('//div[@class="entry-content"]')[0].text_content())[0].encode('utf-8')
+    fax = re.findall(r'Fax: (.*)\r', page.xpath('//div[@class="entry-content"]')[0].text_content())[0].encode('utf-8')
 
     p.add_contact('voice', phone, 'legislature')
     p.add_contact('email', email, None)
@@ -79,6 +79,6 @@ class GuelphPersonScraper(Scraper):
     p.add_link(div.xpath('.//a[2]')[0].attrib['href'], None)
     p.add_link(page.xpath('//div[@class="entry-content"]//a[contains(@href, "facebook")]')[0].attrib['href'], None)
     p.add_link(page.xpath('//div[@class="entry-content"]//a[contains(@href, "twitter")]')[0].attrib['href'], None)
-    p.image = page.xpath('//div[@class="entry-content"]/div/img/@src')[0]
+    p.image = page.xpath('//header/img/@src')[0]
 
     return p
