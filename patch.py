04cc4ef33660440565133c1c3124fe84939b8a4c
James McKinney <james@slashpoundbang.com>
2014-02-13 23:58:38 -0500

    move some sanity scripts into validations

--- patch.py
+++ patch.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import inspect
 import re
 
 from pupa.models.utils import DatetimeValidator
@@ -23,12 +24,32 @@ _contact_details['items']['properties']['note']['enum'] = [
 ]
 
 # We must copy the subschema for each model.
+membership_contact_details   = deepcopy(_contact_details)
 membership_links             = deepcopy(_links)
 organization_contact_details = deepcopy(_contact_details)
 organization_links           = deepcopy(_links)
 person_contact_details       = deepcopy(_contact_details)
 person_links                 = deepcopy(_links)
 
+social_re   = re.compile(r'(?:facebook|twitter|youtube)\.com')
+facebook_re = re.compile(r'facebook\.com')
+twitter_re  = re.compile(r'twitter\.com')
+youtube_re  = re.compile(r'youtube\.com')
+
+# A membership should not have notes on emails, should have notes on non-emails,
+# should have at most one email.
+membership_contact_details['maxMatchingItems'] = [
+  (
+    0,
+    lambda x: x['type'] == 'email' and x['note'] != None
+  ), (
+    0,
+    lambda x: x['type'] != 'email' and x['note'] == None
+  ), (
+    1,
+    lambda x: x['type'] == 'email'
+  ),
+]
 # A membership should not have links.
 membership_links['maxItems'] = 0
 # An organization should not have contact details.
@@ -39,11 +60,25 @@ organization_links['maxItems'] = 0
 person_contact_details['maxItems'] = 0
 # A person should not have notes on links.
 person_links['items']['properties']['note']['type'] = 'null'
-# A person should have at most one link per social media website.
-person_links['maxSocialItems'] = 1
-# A person should have, in most cases, at most one non-social media link.
-person_links['maxNonSocialItems'] = 1
+# A person should have, in most cases, at most one non-social media link, and
+# should have at most one link per social media website.
+person_links['maxMatchingItems'] = [
+  (
+    1,
+    lambda x: not social_re.search(x['url'])
+  ), (
+    1,
+    lambda x: facebook_re.search(x['url'])
+  ), (
+    1,
+    lambda x: twitter_re.search(x['url'])
+  ), (
+    1,
+    lambda x: youtube_re.search(x['url'])
+  ),
+]
 
+membership_schema['properties']['contact_details']   = membership_contact_details
 membership_schema['properties']['links']             = membership_links
 organization_schema['properties']['contact_details'] = organization_contact_details
 organization_schema['properties']['links']           = organization_links
@@ -51,37 +86,17 @@ person_schema['properties']['contact_details']       = person_contact_details
 person_schema['properties']['links']                 = person_links
 
 
-social_re = re.compile(r'(?:facebook|twitter|youtube)\.com')
-social_re_list = [
-  re.compile(r'facebook\.com'),
-  re.compile(r'twitter\.com'),
-  re.compile(r'youtube\.com'),
-]
-
-def validate_maxSocialItems(self, x, fieldname, schema, length=None):
+def validate_maxMatchingItems(self, x, fieldname, schema, pairs=None):
   value = x.get(fieldname)
-  for pattern in social_re_list:
-    count = 0
-    for link in value:
-      if pattern.search(link['url']):
-        count += 1
-      if count > length:
-        self._error("Number of items in %(value)r for field '%(fieldname)s' "
-                    "with the same social media URL "
-                    "must be less than or equal to %(length)d",
-                    value, fieldname, length=length)
+  if isinstance(value, list):
+    for length, match in pairs:
+      count = 0
+      for v in value:
+        if match(v):
+          count += 1
+        if count > length:
+          self._error("Items in %(value)r for field '%(fieldname)s' matching %(match)s "
+                      "must be less than or equal to %(length)d",
+                      value, fieldname, match=inspect.getsource(match).strip(), length=length)
 
-def validate_maxNonSocialItems(self, x, fieldname, schema, length=None):
-  value = x.get(fieldname)
-  count = 0
-  for link in value:
-    if not social_re.search(link['url']):
-      count += 1
-    if count > length:
-      self._error("Number of items in %(value)r for field '%(fieldname)s' "
-                  "with a non-social media URL "
-                  "must be less than or equal to %(length)d",
-                  value, fieldname, length=length)
-
-DatetimeValidator.validate_maxSocialItems = validate_maxSocialItems
-DatetimeValidator.validate_maxNonSocialItems = validate_maxNonSocialItems
+DatetimeValidator.validate_maxMatchingItems = validate_maxMatchingItems
