a8be85dd43305e57934f2f0c1909476604d0a076
James McKinney <james@slashpoundbang.com>
2014-02-18 17:39:59 -0500

    remove posts task

--- tasks.py
+++ tasks.py
@@ -369,11 +369,3 @@ def populations():
           print '  u"%s": %s,' % (expected['name'], row[4])
     else:
       break
-
-
-# Update posts.rb in the represent-dashboard repository.
-@task
-def posts():
-  for row in csv_reader('https://raw.github.com/opencivicdata/ocd-division-ids/master/mappings/country-ca-posts/ca_municipal_subdivisions.csv'):
-    expected = get_definition(row[0])
-    print '  "%s" => %s,' % (slugify(expected['name']), row[1])
