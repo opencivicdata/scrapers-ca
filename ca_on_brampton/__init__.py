12efb71143a18e191e05a1b5f0e6d7c59854e0ba
Matthew Leon <ml@matthewleon.com>
2014-03-10 13:43:53 -0600

    fix brampton scraper class name

--- ca_on_brampton/__init__.py
+++ ca_on_brampton/__init__.py
@@ -2,7 +2,7 @@
 from utils import CanadianJurisdiction
 
 
-class London(CanadianJurisdiction):
+class Brampton(CanadianJurisdiction):
   jurisdiction_id = u'ocd-jurisdiction/country:ca/csd:3521010/council'
   geographic_code = 3521010
   division_name = u'Brampton'
