90dcb0a386c719f6add776ca6512707f0354bb85
Matthew Leon <ml@matthewleon.com>
2014-03-15 19:53:54 -0600

    strathcona county scraper

---
+++ ca_ab_strathcona_county/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from utils import CanadianJurisdiction
+
+
+class StrathconaCounty(CanadianJurisdiction):
+  jurisdiction_id = u'ocd-jurisdiction/country:ca/csd:4811052/council'
+  geographic_code = 4811052
+  division_name = u'Strathcona County'
+  name = u'Strathcona County Municipal Council'
+  url = 'http://www.strathcona.ab.ca'
