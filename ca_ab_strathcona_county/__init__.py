d75811d198f9528677dfd43b64ada4e5a0a0fa1c
James McKinney <james@slashpoundbang.com>
2014-03-16 11:59:18 -0400

    rename strathcona county jurisdiction

--- ca_ab_strathcona_county/__init__.py
+++ ca_ab_strathcona_county/__init__.py
@@ -6,5 +6,5 @@ class StrathconaCounty(CanadianJurisdiction):
   jurisdiction_id = u'ocd-jurisdiction/country:ca/csd:4811052/council'
   geographic_code = 4811052
   division_name = u'Strathcona County'
-  name = u'Strathcona County Municipal Council'
+  name = u'Strathcona County Council'
   url = 'http://www.strathcona.ab.ca'
