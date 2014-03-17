9f933e1bcbec5aa9401904a355d7305b6a311068
Matthew Leon <ml@matthewleon.com>
2014-03-17 11:57:11 -0600

    halifax scraper

---
+++ ca_ns_halifax/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from utils import CanadianJurisdiction
+
+
+class Halifax(CanadianJurisdiction):
+  jurisdiction_id = u'ocd-jurisdiction/country:ca/csd:1209034/council'
+  geographic_code = 1209034
+  division_name = u'Halifax'
+  name = u'Halifax Regional Council'
+  url = 'http://www.halifax.ca'
---
+++ ca_ns_halifax/people.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+from pupa.scrape import Scraper
+
+from utils import lxmlize, CanadianLegislator as Legislator
+
+import re
+
+COUNCIL_PAGE = 'http://www.halifax.ca/councillors/index.html'
+MAYOR_PAGE = 'http://www.halifax.ca/mayor/'
+MAYOR_CONTACT_URL = 'http://www.halifax.ca/mayor/contact.php'
+
+
+class HalifaxPersonScraper(Scraper):
+
+  def get_people(self):
+    page = lxmlize(COUNCIL_PAGE)
+    nodes = page.xpath('//table[@width="484"]//tr')
+    try:
+      for district_row, councillor_row, contact_row, _ in chunks(nodes, 4):
+        post_id = district_row.xpath('string(.//strong)')
+        name = councillor_row.xpath('string(.)')[len('Councillor '):]
+        # TODO: phone numbers on site don't include area code. Add manually?
+        #phone = contact_row.xpath('string(td[2]/text())')
+        email = contact_row.xpath('string(td[4]/a)').replace('[at]', '@')
+
+        p = Legislator(name=name, post_id=post_id, role='Councillor')
+        p.add_source(COUNCIL_PAGE)
+        #p.add_contact('voice', phone, 'legislature')
+        p.add_contact('email', email, None)
+        yield p
+    except ValueError:
+      # on the last run through, there will be less than 4 rows to unpack
+      pass
+
+    mayor_page = lxmlize(MAYOR_PAGE)
+    name = mayor_page.xpath('string(//h1[contains(., "Bio")])')[:-len(' Bio')]
+    contact_page = lxmlize(MAYOR_CONTACT_URL)
+    email = contact_page.xpath('string(//a[contains(., "@")][1])')
+
+    p = Legislator(name=name, post_id='Halifax', role='Councillor')
+    p.add_source(MAYOR_PAGE)
+    p.add_source(MAYOR_CONTACT_URL)
+    p.add_contact('email', email, None)
+    yield p
+
+
+def chunks(l, n):
+  """ Yield successive n-sized chunks from l.
+  """
+  for i in xrange(0, len(l), n):
+    yield l[i:i+n]
+
