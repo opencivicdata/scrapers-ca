3eb48a103fa9e42a9c643afd16e168666dee6adc
Matthew Leon <ml@matthewleon.com>
2014-03-17 14:55:43 -0600

    regina scraper

---
+++ ca_sk_regina/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from utils import CanadianJurisdiction
+
+
+class Regina(CanadianJurisdiction):
+  jurisdiction_id = u'ocd-jurisdiction/country:ca/csd:4706027/council'
+  geographic_code = 4706027
+  division_name = u'Regina'
+  name = u'Regina City Council'
+  url = 'http://www.regina.ca'
