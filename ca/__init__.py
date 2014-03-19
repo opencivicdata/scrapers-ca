382edaa8e3468e9efdc7ab3e9f68bb782299178e
Matthew Leon <ml@matthewleon.com>
2014-03-18 18:42:44 -0600

    canadian federal scraper

--- ca/__init__.py
+++ ca/__init__.py
@@ -6,24 +6,5 @@ class Canada(CanadianJurisdiction):
   jurisdiction_id = u'ocd-jurisdiction/country:ca/legislature'
   geographic_code = 1
   division_name = u'Canada'
-  name = u'Parliament of Canada'
-  url = 'http://www.parl.gc.ca'
-  chambers = {
-    'lower': {
-      'name': 'House of Commons',
-      'title': 'MP',
-    },
-    'upper': {
-      'name': 'Senate',
-      'title': 'Senator',
-    },
-  }
-  parties = [
-    {'name': u'Bloc Québécois'},
-    {'name': u'Conservative'},
-    {'name': u'Conservative Independent'},
-    {'name': u'Green Party'},
-    {'name': u'Independent'},
-    {'name': u'Liberal'},
-    {'name': u'NDP'},
-  ]
+  name = u'House of Commons'
+  url = ''
