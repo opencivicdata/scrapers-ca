66d4a7ad7d54122e2e7e30ebebd5281661014056
James McKinney <james@slashpoundbang.com>
2014-03-28 16:03:32 -0400

    fix saguenay encoding

--- ca_qc_saguenay/people.py
+++ ca_qc_saguenay/people.py
@@ -31,8 +31,7 @@ class SaguenayPersonScraper(Scraper):
     councillors = page.xpath('//div[@class="box"]//div')
     for councillor in councillors:
       district = councillor.xpath('./h3')[0].text_content().replace('#', '')
-      # Name is double-encoded.
-      name = councillor.xpath('.//p/text()')[0].encode('latin-1').decode('utf-8').encode('latin-1').decode('utf-8')
+      name = councillor.xpath('.//p/text()')[0].encode('latin-1').decode('utf-8')
       name = name.replace('M. ', '').replace('Mme ', '').strip()
       phone = councillor.xpath('.//p/text()')[1].split(':')[1].strip().replace(' ', '-')
       email = councillor.xpath('.//a[contains(@href, "mailto:")]')[0].text_content()
