3e40c1870ab4776dd82fd0d804d11c422d00f7db
Matthew Leon <ml@matthewleon.com>
2014-03-31 22:15:37 -0600

    ca_pe scrapers

---
+++ ca_pe/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from utils import CanadianJurisdiction
+
+
+class PrinceEdwardIsland(CanadianJurisdiction):
+  jurisdiction_id = u'ocd-jurisdiction/country:ca/province:pe/legislature'
+  geographic_code = 11
+  division_name = u'Prince Edward Island'
+  name = u'Legislative Assembly of Prince Edward Island'
+  url = ''
