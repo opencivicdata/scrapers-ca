adff4da10fc33e4bd59017641cb6b47fd06bb39e
Matthew Leon <ml@matthewleon.com>
2014-03-31 22:01:24 -0600

    ca_on scraper

---
+++ ca_on/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from utils import CanadianJurisdiction
+
+
+class Ontario(CanadianJurisdiction):
+  jurisdiction_id = u'ocd-jurisdiction/country:ca/province:on/legislature'
+  geographic_code = 35
+  division_name = u'Ontario'
+  name = u'Legislative Assembly of Ontario'
+  url = ''
