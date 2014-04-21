83c39763330efced404426a0038d2f519ca1d423
Matthew Leon <ml@matthewleon.com>
2014-04-21 17:06:16 -0500

    add note about nl parties

--- ca_nl/__init__.py
+++ ca_nl/__init__.py
@@ -8,3 +8,5 @@ class NewfoundlandAndLabrador(CanadianJurisdiction):
   division_name = u'Newfoundland and Labrador'
   name = u'Newfoundland and Labrador House of Assembly'
   url = 'http://www.assembly.nl.ca'
+  # parties not listed on site
+
