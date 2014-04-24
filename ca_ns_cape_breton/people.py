ddcae12c51c8cb40349230c7a02e82e8f46a10b3
Matthew Leon <ml@matthewleon.com>
2014-04-24 14:10:20 -0500

    cape breton mayor

--- ca_ns_cape_breton/people.py
+++ ca_ns_cape_breton/people.py
@@ -3,13 +3,15 @@ from pupa.scrape import Scraper
 from utils import lxmlize, CanadianLegislator as Legislator
 
 import re
+from urlparse import urljoin
 
 COUNCIL_PAGE = 'http://www.cbrm.ns.ca/councillors.html'
-
+MAYOR_PAGE = 'http://www.cbrm.ns.ca/mayor.html'
 
 class CapeBretonPersonScraper(Scraper):
 
   def get_people(self):
+    """
     page = lxmlize(COUNCIL_PAGE)
 
     councillors = page.xpath('//table[@class="table_style"]/tbody/tr')[1:]
@@ -32,3 +34,26 @@ class CapeBretonPersonScraper(Scraper):
       page = lxmlize(councillor_url)
       p.image = page.xpath('//img[@class="image_left"]/@src')[0]
       yield p
+
+    """
+    mayorpage = lxmlize(MAYOR_PAGE)
+    name_elem = mayorpage.xpath('//strong[contains(text(), "About")]')[0]
+    name = re.search('About Mayor (.+):', name_elem.text).group(1)
+    photo_url_rel = mayorpage.xpath(
+      'string(//*[@id="content-left-main"]/table/tbody/tr/td/span/'
+      'table/tbody/tr/td[2]/p[1]/span/img/@src)')
+    photo_url = urljoin(MAYOR_PAGE, photo_url_rel)
+    address_and_tel_elem = mayorpage.xpath(
+      '//strong[contains(text(), "Contact")]/ancestor::p/'
+      'following-sibling::p[1]')[0]
+    address = address_and_tel_elem[0].text_content()
+    phone = address_and_tel_elem[2].text.split(':')[1]
+
+    p = Legislator(name=name, post_id='Cape Breton', role='Mayor')
+    p.add_source(MAYOR_PAGE)
+    p.add_contact('address', address, 'legislature')
+    p.add_contact('voice',  phone, 'legislature')
+    # email is protected through JS
+    p.image = photo_url
+    yield p
+
