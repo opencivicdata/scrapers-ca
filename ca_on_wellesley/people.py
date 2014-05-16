bfad9bba12158ed339a8241eb07ba9371ac33221
Matthew Leon <ml@matthewleon.com>
2014-05-16 16:32:02 -0500

    ca_on_wellesley rewrite

--- ca_on_wellesley/people.py
+++ ca_on_wellesley/people.py
@@ -6,35 +6,39 @@ import re
 
 COUNCIL_PAGE = 'http://www.wellesley.ca/council/councillors/?q=council/councillors'
 
+def post_number(name):
+    return {
+      'Ward One': 'Ward 1',
+      'Ward Two': 'Ward 2',
+      'Ward Three': 'Ward 3',
+      'Ward Four': 'Ward 4'
+    }[name]
 
 class WellesleyPersonScraper(Scraper):
 
   def get_people(self):
     page = lxmlize(COUNCIL_PAGE)
 
-    councillors = page.xpath('//table[@class="tbl w100"][2]//tr/td')[1::2]
-    councillors = councillors + page.xpath('//table[@class="tbl w100"][1]//td[2]')
-    for councillor in councillors:
-      district = councillor.xpath('./preceding-sibling::td/text()')
-      if district:
-        district = district[-1]
-        role = 'Councillor'
+    councillors = page.xpath('//div[@class="img_four"][1]/div[1]')
+    councillors = councillors + page.xpath('//div[@class="img_four"][2]/div')
+    for councillor_elem in councillors:
+      name, position = councillor_elem.xpath('string(./p/strong)').split(',')
+      position = position.strip()
+      if ' ' in position:
+        position, post_id = position.split(' ', 1)
+        post_id = post_number(post_id)
       else:
-        district = 'Wellesley'
-        role = 'Mayor'
-
-      image = councillor.xpath('./preceding-sibling::td/img/@src')[-1]
-
-      name = councillor.xpath('./span/text()')[0]
-      address = councillor.xpath('./text()')
-      address = re.sub(r'\s{2,}', ' ', ' '.join(address[:4]))
-      phone = councillor.xpath('./text()')[4].strip().replace('.', '-')
-      email = councillor.xpath('./a/text()')[0]
-
-      p = Legislator(name=name, post_id=district, role=role)
+        post_id = 'Wellesley'
+      addr = '\n'.join(addr_str.strip() for addr_str in 
+                      councillor_elem.xpath('./p/text()')).strip()
+      phone = councillor_elem.xpath('string(.//a[starts-with(@href, "tel:")])')
+      email = councillor_elem.xpath(
+          'string(.//a[starts-with(@href, "mailto:")])')
+      image = councillor_elem.xpath('string(.//img[1]/@src)')
+      p = Legislator(name=name, post_id=post_id, role=position, image=image)
       p.add_source(COUNCIL_PAGE)
-      p.add_contact('address', address, 'legislature')
+      p.add_contact('address', addr, 'legislature')
       p.add_contact('voice', phone, 'legislature')
       p.add_contact('email', email, None)
-      p.image = image
       yield p
+
