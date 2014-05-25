9e73f515e828e2f3af422eb4851762d0a415e376
Matthew Leon <ml@matthewleon.com>
2014-05-25 16:45:47 -0500

    add ca_qc_trois_rivieres

---
+++ ca_qc_trois_rivieres/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from utils import CanadianJurisdiction
+
+
+class TroisRivieres(CanadianJurisdiction):
+  jurisdiction_id = u'ocd-jurisdiction/country:ca/csd:2437067/council'
+  geographic_code = 2437067
+  division_name = u'Trois-Rivières'
+  name = u'Conseil municipal de Trois-Rivières'
+  url = 'http://www.v3r.net/portail/index.aspx'
