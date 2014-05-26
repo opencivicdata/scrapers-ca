763b04b02552e67d4337990e454b2ffd358f4c16
Matthew Leon <ml@matthewleon.com>
2014-05-26 12:31:54 -0500

    ca_qc_terrebonne scraper

---
+++ ca_qc_terrebonne/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from utils import CanadianJurisdiction
+
+
+class Terrebonne(CanadianJurisdiction):
+  jurisdiction_id = u'ocd-jurisdiction/country:ca/csd:2464008/council'
+  geographic_code = 2464008
+  division_name = u'Terrebonne'
+  name = u'Conseil municipal de Terrebonne'
+  url = 'http://www.ville.terrebonne.qc.ca'
