1a540d2b8e2a7ef0e53f065f372457eb922dc423
Matthew Leon <ml@matthewleon.com>
2014-05-26 18:20:44 -0500

    ca_on_oshawa scraper

---
+++ ca_on_oshawa/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from utils import CanadianJurisdiction
+
+
+class Oshawa(CanadianJurisdiction):
+  jurisdiction_id = u'ocd-jurisdiction/country:ca/csd:3518013/council'
+  geographic_code = 3518013
+  division_name = u'Oshawa'
+  name = u'Oshawa City Council'
+  url = 'http://www.oshawa.ca'
