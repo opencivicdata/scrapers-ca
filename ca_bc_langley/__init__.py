07f1907407f70b450d54b9c378882fe7acb94de2
Thomas Rohée <roheethomas@gmail.com>
2014-05-27 21:57:51 -0400

    added bc_langley

---
+++ ca_bc_langley/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from utils import CanadianJurisdiction
+
+
+class Langley(CanadianJurisdiction):
+  jurisdiction_id = u'ocd-jurisdiction/country:ca/csd:5915002/council'
+  geographic_code = 5915002
+  division_name = u'Langley'
+  name = u'Langley City Council'
+  url = 'http://www.city.langley.bc.ca'
