85004dda211f9b40d53d59d42ad7e1e03f36834d
Matthew Leon <ml@matthewleon.com>
2014-05-29 21:10:54 -0500

    ca_bc_saanich scraper

---
+++ ca_bc_saanich/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from utils import CanadianJurisdiction
+
+
+class Saanich(CanadianJurisdiction):
+  jurisdiction_id = u'ocd-jurisdiction/country:ca/csd:5917021/council'
+  geographic_code = 5917021
+  division_name = u'Saanich'
+  name = u'Saanich District municipality Council'
+  url = 'http://www.saanich.ca'
---
+++ ca_bc_saanich/people.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+from pupa.scrape import Scraper
+
+from utils import lxmlize, CanadianLegislator as Legislator
+
+import re
+
+COUNCIL_PAGE = 'http://www.saanich.ca/living/mayor/council/index.html'
+
+
+class SaanichPersonScraper(Scraper):
+
+  def get_people(self):
+    page = lxmlize(COUNCIL_PAGE)
+    for link in page.xpath('//div[@class="section"]//a'):
+      url = link.attrib['href']
+      if url.endswith('address.html'):
+        continue
+      page = lxmlize(url)
+      role, name = page.xpath('string(//div[@id="content"]/h1)').split(' ', 1)
+      name = ' '.join(name.split()[:-1])
+      photo_url = page.xpath('string(//img[@class="float-right"]/@src)')
+      email = page.xpath('string(//a[starts-with(@href, "mailto:")])')
+
+      p = Legislator(name=name, post_id='Saanich', role=role, image=photo_url)
+      p.add_source(COUNCIL_PAGE)
+      p.add_contact('email', email, None)
+      yield p
