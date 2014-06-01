f0d32b11a1b580cce1a2cbd0368ff3f6976bfb7b
James McKinney <james@slashpoundbang.com>
2014-06-01 17:50:16 -0400

    fix kelowna warnings

--- ca_bc_kelowna/people.py
+++ ca_bc_kelowna/people.py
@@ -15,11 +15,11 @@ class KelownaPersonScraper(Scraper):
     links = page.xpath('//td[@width=720]//a[contains(text(), "Councillor") or '
                        'contains(text(), "Mayor")]')
     for link in links:
-      role, name = link.text.split(' ', 1)
+      role, name = link.text_content().replace(u'\xa0', u' ').split(' ', 1)
       url = link.attrib['href']
       page = lxmlize(url)
       photo_url = page.xpath('string(//li/img/@src)')
-      phone = page.xpath('string(//strong[last()])')
+      phone = page.xpath('//strong')[-1].text_content()
       email = page.xpath('string(//a[starts-with(@href, "mailto:")])')
 
       p = Legislator(name=name, post_id='Kelowna', role=role, image=photo_url)
