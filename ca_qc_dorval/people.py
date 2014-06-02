082f244ed746f2458f81283d432191f02252569b
James McKinney <james@slashpoundbang.com>
2014-06-01 20:36:02 -0400

    fix ca_qc_dorval

--- ca_qc_dorval/people.py
+++ ca_qc_dorval/people.py
@@ -4,7 +4,7 @@ from utils import lxmlize, CanadianLegislator as Legislator
 
 import re
 
-COUNCIL_PAGE = 'http://www.ville.dorval.qc.ca/en/default.asp?contentID=516'
+COUNCIL_PAGE = 'http://www.ville.dorval.qc.ca/en/democratic-life/municipal-council'
 
 
 class DorvalPersonScraper(Scraper):
@@ -12,9 +12,9 @@ class DorvalPersonScraper(Scraper):
   def get_people(self):
     page = lxmlize(COUNCIL_PAGE)
 
-    councillors = page.xpath('//p[@align="center"]')
+    councillors = page.xpath('//td/p[./strong]')
     for councillor in councillors:
-      info = councillor.xpath('.//strong/text()')
+      info = councillor.xpath('./strong/text()')
       name = info[0]
       if len(info) < 3:
         district = 'Dorval'
@@ -25,7 +25,7 @@ class DorvalPersonScraper(Scraper):
       p = Legislator(name=name, post_id=district, role=role)
       p.add_source(COUNCIL_PAGE)
 
-      p.image = councillor.xpath('./parent::td/parent::tr/preceding-sibling::tr//img/@src')[0]
+      p.image = councillor.xpath('./preceding-sibling::p/img/@src')[0]
 
       email = councillor.xpath('.//a[contains(@href, "mailto:")]/text()')[0]
       p.add_contact('email', email, None)
