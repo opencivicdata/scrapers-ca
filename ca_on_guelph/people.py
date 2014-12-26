35f43f50bff2bbc422428b44f0153bfdf1b006e7
James McKinney <james@slashpoundbang.com>
2014-12-25 21:15:21 -0500

    use guelph csv

--- ca_on_guelph/people.py
+++ ca_on_guelph/people.py
@@ -1,76 +1,33 @@
 from __future__ import unicode_literals
 from utils import CanadianScraper, CanadianPerson as Person
 
-import re
-
 COUNCIL_PAGE = 'http://guelph.ca/city-hall/mayor-and-council/city-council/'
+COUNCIL_CSV_URL = 'http://open.guelph.ca/wp-content/uploads/2014/12/GuelphCityCouncil2014-2018ElectedOfficalsContactInformation.csv'
 
 
 class GuelphPersonScraper(CanadianScraper):
 
     def scrape(self):
-        page = self.lxmlize(COUNCIL_PAGE)
+        csv = self.csv_reader(COUNCIL_CSV_URL, header=True)
+        for row in csv:
+            district = row['District name']
+            role = row['Primary role']
+            name = '%s %s' % (row['First name'], row['Last name'])
 
-        councillors = page.xpath('//*[@class="two_third last"]')
-        for councillor in councillors:
-            if councillor == councillors[0]:
-                yield self.scrape_mayor(councillor)
-                continue
+            address = row['Address line 1']
+            if row['Address line 2']:
+                address += '\n%s' % row['Address line 2']
+            address += '\n%s ON  %s' % (row['Locality'], row['Postal code'])
 
-            name = councillor.xpath('.//a')[0].text_content().replace('Councillor', '').replace('Mayor', '')
-            info = councillor.xpath('.//text()[normalize-space()]')
-            district = info[2]
-            url = councillor.xpath('.//a')[0].attrib['href']
-
-            p = Person(primary_org='legislature', name=name, district=district, role='Councillor')
-            p.add_source(COUNCIL_PAGE)
-            p.add_source(url)
-
-            p.add_contact('voice', info[3].replace('extension', 'x'), 'legislature')
-            email = self.get_email(councillor)
-            p.add_contact('email', email)
-
-            site = councillor.xpath('.//a[contains(text(),"Website")]')
-            if site:
-                p.add_link(site[0].attrib['href'])
-
-            page = self.lxmlize(url)
-
-            p.image = page.xpath('//header/img/@src')[0]
-
-            address = re.findall(r'Address: (.*)Phone', page.xpath('//div[@class="entry-content"]')[0].text_content())
-            if address:
-                p.add_contact('address', address[0], 'legislature')
-
-            blog = page.xpath('//a[contains(text(),"Blog")]')
-            if blog:
-                p.add_link(blog[0].attrib['href'])
-
-            facebook = page.xpath('//div[@class="entry-content"]//a[contains(@href, "facebook")]')
-            if facebook:
-                p.add_link(facebook[0].attrib['href'])
-            twitter = page.xpath('//div[@class="entry-content"]//a[contains(@href, "twitter")]')
-            if twitter:
-                p.add_link(twitter[0].attrib['href'])
+            p = Person(primary_org='legislature', name=name, district=district, role=role)
+            p.add_source(COUNCIL_CSV_URL)
+            p.add_link(row['Source URL'])
+            p.gender = row['Gender']
+            p.image = row['Photo URL']
+            p.add_contact('email', row['Email'])
+            p.add_contact('address', address, 'legislature')
+            p.add_contact('voice', row['Phone'], 'legislature')
+            p.add_contact('fax', row['Fax'], 'legislature')
+            if row['Phone (mobile)']:
+                p.add_contact('cell', row['Phone (mobile)'], 'legislature')
             yield p
-
-    def scrape_mayor(self, div):
-        name = div.xpath('.//a')[0].text_content().replace('Mayor', '')
-        url = div.xpath('.//a')[0].attrib['href']
-
-        p = Person(primary_org='legislature', name=name, district='Guelph', role='Mayor')
-        p.add_source(COUNCIL_PAGE)
-        p.add_source(url)
-
-        phone = div.xpath('.//text()[normalize-space()]')[2]
-        email = self.get_email(div)
-
-        page = self.lxmlize(url)
-
-        p.add_contact('voice', phone, 'legislature')
-        p.add_contact('email', email)
-        p.add_link(page.xpath('//div[@class="entry-content"]//a[contains(@href, "facebook")]')[0].attrib['href'])
-        p.add_link(page.xpath('//div[@class="entry-content"]//a[contains(@href, "twitter")]')[0].attrib['href'])
-        p.image = page.xpath('//header/img/@src')[0]
-
-        return p
