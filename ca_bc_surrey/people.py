e4ad1da6dad60c1b7303a617a627537e59a3d1dd
Thomas Rohée <roheethomas@gmail.com>
2014-12-31 16:39:49 -0500

    Surrey: fixed scraper when no picture or contact details appear and mayor's info

--- ca_bc_surrey/people.py
+++ ca_bc_surrey/people.py
@@ -15,29 +15,33 @@ class SurreyPersonScraper(CanadianScraper):
             role, name = link.text.split(' ', 1)
             url = link.attrib['href']
             councillor_page = self.lxmlize(url)
-            photo_url = councillor_page.xpath('.//div[@class="inner-wrapper"]//img/@src')[0]
-            phone = councillor_page.xpath('//text()[contains(., "hone:")][1]|//text()[contains(., "604-")]')[0]
-            email = self.get_email(councillor_page)
 
-            if role == 'Mayor':
-                district = 'Surrey'
-            else:
+            photo_node = councillor_page.xpath('.//div[@class="inner-wrapper"]//img/@src')
+            if photo_node:
+                photo_url = photo_node[0]
+                phone = councillor_page.xpath('//text()[contains(., "hone:")][1]|//text()[contains(., "604-")]')[0]
+                email = self.get_email(councillor_page)
                 district = 'Surrey (seat %d)' % councillor_seat_number
                 councillor_seat_number += 1
 
-            p = Person(primary_org='legislature', name=name, district=district, role=role, image=photo_url)
-            p.add_source(COUNCIL_PAGE)
-            p.add_source(url)
-            if phone:
+                p = Person(primary_org='legislature', name=name, district=district, role=role, image=photo_url)
+                p.add_source(COUNCIL_PAGE)
+                p.add_source(url)
                 p.add_contact('voice', phone, 'legislature')
-            p.add_contact('email', email)
+                p.add_contact('email', email)
+            else:
+                p = Person(primary_org='legislature', name=name, district=district, role=role)
+                p.add_source(COUNCIL_PAGE)
+                p.add_source(url)
+
             yield p
 
         mayor_link = page.xpath('//div[@class="inner-wrapper"]//a[contains(text(), "Mayor")]')[0]
         mayor_url = mayor_link.attrib['href']
-        name = mayor_link.text.split(' ', 2)[1]
         mayor_page = self.lxmlize(mayor_url)
-        photo_url = mayor_page.xpath('//img[contains(@src, "Mayor")]/@src')[0]
+        name_node = mayor_page.xpath('//div[@class="inner-wrapper"]//div[@class="inner-wrapper"]/p/text()')[0]
+        name = name_node.split(' ', 3)[-1]
+        photo_url = mayor_page.xpath('//div[@class="inner-wrapper"]//img/@src')[0]
         phone = mayor_page.xpath('//text()[contains(., "Office:")]')[0]
         # no email
 
