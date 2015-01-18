ac382d07e1a79394b40f83b2072532eeb6500df3
Thomas Rohée <roheethomas@gmail.com>
2015-01-18 17:32:00 -0500

    Windsor: fixed scraper for new layout

--- ca_on_windsor/people.py
+++ ca_on_windsor/people.py
@@ -1,10 +1,8 @@
 from __future__ import unicode_literals
 from utils import CanadianScraper, CanadianPerson as Person
 
-from six.moves.urllib.parse import urljoin
-
 COUNCIL_PAGE = 'http://www.citywindsor.ca/mayorandcouncil/City-Councillors/Pages/City-Councillors.aspx'
-MAYOR_PAGE = 'http://www.citywindsor.ca/mayorandcouncil/Pages/Biography-of-the-Mayor.aspx'
+MAYOR_COUNCIL_PAGE = 'http://www.citywindsor.ca/mayorandcouncil/Pages/Mayor-and-City-Council.aspx'
 
 
 class WindsorPersonScraper(CanadianScraper):
@@ -12,40 +10,41 @@ class WindsorPersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
 
-        councillor_links = page.xpath('//span[@class="textimagetype"]//a[contains(text(), "- Ward")]')
-        for councillor_link in councillor_links:
-            name, district = councillor_link.text.split(' - ')
-            cpage_url = councillor_link.attrib['href']
-            cpage = self.lxmlize(cpage_url)
+        councillors = page.xpath('//h2/ancestor::div[1]')
+        for councillor in councillors:
+            district, name = councillor.xpath('./h2//text()')[0].split(' – ')
             p = Person(primary_org='legislature', name=name, district=district, role='Councillor')
             p.add_source(COUNCIL_PAGE)
-            p.add_source(cpage_url)
 
-            email = self.get_email(cpage)
+            email = self.get_email(councillor)
             p.add_contact('email', email)
 
-            phone = cpage.xpath('//text()[contains(., "Phone")]')[0].split(':')[1]
+            phone = self.get_phone(councillor, [519])
             p.add_contact('voice', phone, 'legislature')
 
-            img_url_rel = cpage.xpath('(//span/img)[1]/@src')[0]
-            img_url = urljoin(cpage_url, img_url_rel)
-            p.image = img_url
+            p.image = councillor.xpath('./h2//img/@src')[0]
 
             yield p
 
-        page = self.lxmlize(MAYOR_PAGE)
-        name = ' '.join(page.xpath('//p[contains(text(), "is married to")]/text()')[0].split()[:2])
-        address = ' '.join(page.xpath('//p[contains(text(), "Mayor\'s Office")]/text()')[1:])
-        phone, fax = page.xpath('//p[contains(text(), "Phone:")]/text()')[:-1]
+        page = self.lxmlize(MAYOR_COUNCIL_PAGE)
+        contact_details = page.xpath('//p[contains(.//text(), "Mayor\'s Office")]')[0]
+        address = ' '.join(contact_details.xpath('text()')[:5])
+        phone, fax = contact_details.xpath('text()')[5:7]
         phone = phone.strip().replace('(', '').replace(') ', '-')
         fax = fax.strip().replace('(', '').replace(') ', '-').split(':')[1]
-        email = self.get_email(page)
+        email = self.get_email(contact_details)
+        image = page.xpath('//p[@class="sectioning"]/a[contains(@title, "Mayor")]/img/@src')[0]
+
+        mayor_url = page.xpath('//a[contains(@title, "Mayor")]/@href')[0]
+        mayor_page = self.lxmlize(mayor_url)
+        name = mayor_page.xpath('//div[@id="CCWMainContent2"]//h1//text()')[0].split(' ', 1)[1]
 
         p = Person(primary_org='legislature', name=name, district='Windsor', role='Mayor')
-        p.add_source(MAYOR_PAGE)
+        p.add_source(MAYOR_COUNCIL_PAGE)
+        p.add_source(mayor_url)
         p.add_contact('address', address, 'legislature')
         p.add_contact('voice', phone, 'legislature')
         p.add_contact('fax', fax, 'legislature')
         p.add_contact('email', email)
-        p.image = page.xpath('//div[@class="sectioning"]//img[contains(@title, "Mayor")]/@src')[0]
+        p.image = image
         yield p
