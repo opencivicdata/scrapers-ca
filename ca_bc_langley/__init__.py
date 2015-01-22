31edaca583612d2428b5012ae49986c473ad449a
James McKinney <james@slashpoundbang.com>
2015-01-21 21:39:02 -0500

    add missing files

---
+++ ca_bc_langley/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+from utils import CanadianJurisdiction
+
+
+class Langley(CanadianJurisdiction):
+    classification = 'legislature'
+    division_id = 'ocd-division/country:ca/csd:5915001'
+    division_name = 'Langley'
+    name = 'Langley Township Council'
+    url = 'http://www.tol.ca'
