68a83d9a1740e7a098e121166492024f01d0fb2e
James McKinney <james@slashpoundbang.com>
2015-02-02 01:11:13 -0500

    add toronto session list

--- ca_on_toronto/__init__.py
+++ ca_on_toronto/__init__.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 from utils import CanadianJurisdiction
 
+import lxml.html
+import requests
+
 
 class Toronto(CanadianJurisdiction):
     classification = 'legislature'
@@ -8,3 +11,7 @@ class Toronto(CanadianJurisdiction):
     division_name = 'Toronto'
     name = 'Toronto City Council'
     url = 'http://www.toronto.ca'
+
+    def get_session_list(self):
+        response = requests.get('http://app.toronto.ca/tmmis/getAdminReport.do?function=prepareMemberVoteReport')
+        return lxml.html.fromstring(response.text).xpath('//select[@name="termId"][1]/option/text()')
