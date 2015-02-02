13d36c7e66ed39feeb16d9764336d7f979c59cf8
Thomas Rohée <roheethomas@gmail.com>
2015-02-02 11:37:54 -0500

    Burlington: updated scraper for new website

--- ca_on_burlington/people.py
+++ ca_on_burlington/people.py
@@ -1,9 +1,8 @@
 from __future__ import unicode_literals
 from utils import CanadianScraper, CanadianPerson as Person
 
-import re
 
-COUNCIL_PAGE = 'http://cms.burlington.ca/Page110.aspx'
+COUNCIL_PAGE = 'http://www.burlington.ca/en/services-for-you/Council-Members-and-Wards.asp?_mid_=754'
 
 
 class BurlingtonPersonScraper(CanadianScraper):
@@ -11,61 +10,34 @@ class BurlingtonPersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
 
-        councillors = page.xpath('//div[@id="subnav"]//a')
+        councillors = page.xpath('//div[@id="landingNav"]//a')
         for councillor in councillors:
-            name = councillor.xpath('./span/text()')[0].strip()
-            district = councillor.xpath('.//strong')[0].text_content()
+            district = councillor.xpath('./text()')[0]
+
+            if district == 'Mayor':
+                role = 'Mayor'
+                district = 'Burlington'
+            else:
+                role = 'Councillor'
 
             url = councillor.attrib['href']
+            councillor_page = self.lxmlize(url)
 
-            if councillor == councillors[0]:
-                yield self.scrape_mayor(name, url)
-                continue
+            name = councillor_page.xpath('//h1/text()')[0].split(' ', 1)[1]
+            address = ' '.join(councillor_page.xpath('//div[@id="printAreaContent"]//p[contains(text(),"City of Burlington")]/text()'))
 
-            page = self.lxmlize(url)
+            contact_info = councillor_page.xpath('//h2[contains(text(), "Contacts")]/following-sibling::p[1]//text()')
+            phone = contact_info[1].split('Tel:')[1].replace('Ext. ', 'x').replace('#', 'x')
+            fax = contact_info[2].split('Fax:')[1]
+            email = contact_info[-1]
 
-            address = page.xpath('//div[@id="content"]//p[contains(text(),"City of Burlington,")]')
-            contact = page.xpath('//div[@id="subnav"]//p[contains(text(),"Phone")]')[0]
-            phone = re.findall(r'Phone: (.*)', contact.text_content())[0].replace('Ext. ', 'x').replace('#', 'x')
-            fax = re.findall(r'Fax: (.*)', contact.text_content())[0]
-            email = self.get_email(contact)
-
-            p = Person(primary_org='legislature', name=name, district=district, role='Councillor')
+            # Pictures are embedded in a banner
+            p = Person(primary_org='legislature', name=name, district=district, role=role)
             p.add_source(COUNCIL_PAGE)
             p.add_source(url)
-
-            p.image = page.xpath('//div[@id="subnav"]//img/@src')[0]
-
-            if address:
-                p.add_contact('address', address[0].text_content(), 'legislature')
+            p.add_contact('address', address, 'legislature')
             p.add_contact('voice', phone, 'legislature')
             p.add_contact('fax', fax, 'legislature')
             p.add_contact('email', email)
 
             yield p
-
-    def scrape_mayor(self, name, url):
-        page = self.lxmlize(url)
-
-        contact = page.xpath('//div[@id="subnav"]/p/text()')
-        phone = contact[0]
-        fax = contact[1].split(':')[1]
-        email = self.get_email(page, '//div[@id="subnav"]/p')
-
-        mayor_page = self.lxmlize('http://www.burlingtonmayor.com')
-        contact_url = mayor_page.xpath('//div[@class="menu"]//a[contains(text(),"Contact")]')[0].attrib['href']
-        mayor_page = self.lxmlize(contact_url)
-        address = mayor_page.xpath('//div[@class="entry-content"]//p[contains(text(),"City Hall")]')[0].text_content()
-
-        p = Person(primary_org='legislature', name=name, district="Burlington", role='Mayor')
-        p.add_source(COUNCIL_PAGE)
-        p.add_source(url)
-        p.add_source('http://www.burlingtonmayor.com')
-
-        p.image = page.xpath('//div[@id="subnav"]/img/@src')[0]
-        p.add_contact('voice', phone, 'legislature')
-        p.add_contact('fax', fax, 'legislature')
-        p.add_contact('email', email)
-        p.add_contact('address', address, 'legislature')
-
-        return p
