1cdabc67056c4353af2fdcb4002c2ea337acacf7
James McKinney <james@slashpoundbang.com>
2015-02-02 01:10:15 -0500

    change logging levels

--- pupa_settings.py
+++ pupa_settings.py
@@ -23,10 +23,10 @@ LOGGING = {
     },
     'loggers': {
         '': {
-            'handlers': ['default'], 'level': 'WARN', 'propagate': True  # DEBUG
+            'handlers': ['default'], 'level': 'DEBUG', 'propagate': True  # DEBUG
         },
         'scrapelib': {
-            'handlers': ['default'], 'level': 'WARN', 'propagate': False  # INFO
+            'handlers': ['default'], 'level': 'INFO', 'propagate': False  # INFO
         },
         'requests': {
             'handlers': ['default'], 'level': 'WARN', 'propagate': False
