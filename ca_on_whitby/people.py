c9a8621e3e20ecc57a5e603037f85720dae2cc44
Thomas Rohée <roheethomas@gmail.com>
2015-02-02 14:09:23 -0500

    Whitby: updated scraper for new layout

--- ca_on_whitby/people.py
+++ ca_on_whitby/people.py
@@ -8,29 +8,37 @@ class WhitbyPersonScraper(CanadianScraper):
 
     def scrape(self):
         regional_councillor_seat_number = 1
-
         page = self.lxmlize(COUNCIL_PAGE)
 
-        person_elems = page.xpath('//tr')
-        for person in person_elems:
-            if len(person) > 1:
-                info = person[1]
-                try:  # Mayor and regional councillors
-                    name, role = info[0].text_content().split(',')
-                    role = role.strip()
-                    if role == 'Deputy Mayor':
-                        role = 'Regional Councillor'
-                    if role == 'Regional Councillor':
-                        district = 'Whitby (seat %d)' % regional_councillor_seat_number
-                        regional_councillor_seat_number += 1
-                    else:
-                        district = 'Whitby'
-                except ValueError:
-                    district = ' '.join(info[0].text_content().split()[:2])
-                    name, role = info[1].text_content().split(', ')
-                email = self.get_email(info)
-                image = person.xpath('.//img/@src')[0]
-                p = Person(primary_org='legislature', name=name, district=district, role=role, image=image)
-                p.add_source(COUNCIL_PAGE)
-                p.add_contact('email', email)
-                yield p
+        yield self.scrape_mayor(page)
+
+        councillor_nodes = page.xpath('//h3[contains(text(), "Councillors")]/following-sibling::p')[:-1]
+        for councillor_node in councillor_nodes:
+            name, role_district = councillor_node.xpath('./strong/text()')
+            name = name.replace(',', '')
+            if 'Regional Councillor' in role_district:
+                role = role_district
+                district = 'Whitby (seat %d)' % regional_councillor_seat_number
+                regional_councillor_seat_number += 1
+            else:
+                role, district = role_district.strip().split(', ')
+                district = district.split(' (')[0]
+
+            email = self.get_email(councillor_node)
+            image = councillor_node.xpath('./img/@src')[0]
+            p = Person(primary_org='legislature', name=name, district=district, role=role, image=image)
+            p.add_source(COUNCIL_PAGE)
+            p.add_contact('email', email)
+            yield p
+
+    def scrape_mayor(self, page):
+        mayor_node = page.xpath('//p[strong[contains(text(), "Mayor")]]')[0]
+        name = mayor_node.xpath('./strong')[0].text_content().replace(',', '')
+        email = self.get_email(mayor_node)
+        phone = page.xpath('//p[contains(text(), "Phone")]/text()')[0].split(':')[1]
+        image = mayor_node.xpath('./preceding-sibling::img/@src')[0]
+        p = Person(primary_org='legislature', name=name, district='Whitby', role='Mayor', image=image)
+        p.add_source(COUNCIL_PAGE)
+        p.add_contact('voice', phone, 'legislature')
+        p.add_contact('email', email)
+        return p
