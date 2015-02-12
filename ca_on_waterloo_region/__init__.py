5ecfbaf020b01bcbdb09f7b26c9b8a5ae3a555e7
James McKinney <james@slashpoundbang.com>
2015-02-12 13:25:00 -0500

    fix waterloo region posts

--- ca_on_waterloo_region/__init__.py
+++ ca_on_waterloo_region/__init__.py
@@ -14,18 +14,18 @@ class Waterloo(CanadianJurisdiction):
         organization = Organization(self.name, classification=self.classification)
 
         organization.add_post(role='Chair', label='Waterloo')
-        organization.add_post(role='Councillor', label='Cambridge')
-        organization.add_post(role='Councillor', label='Kitchener')
-        organization.add_post(role='Councillor', label='Waterloo')
-        organization.add_post(role='Councillor', label='North Dumfries')
-        organization.add_post(role='Councillor', label='Wellesley')
-        organization.add_post(role='Councillor', label='Wilmot')
-        organization.add_post(role='Councillor', label='Woolwich')
+        organization.add_post(role='Regional Councillor', label='Cambridge')
+        organization.add_post(role='Regional Councillor', label='Kitchener')
+        organization.add_post(role='Regional Councillor', label='Waterloo')
+        organization.add_post(role='Regional Councillor', label='North Dumfries')
+        organization.add_post(role='Regional Councillor', label='Wellesley')
+        organization.add_post(role='Regional Councillor', label='Wilmot')
+        organization.add_post(role='Regional Councillor', label='Woolwich')
         for i in range(2):
-            organization.add_post(role='Councillor', label='Cambridge (seat %d)' % (i + 1))
+            organization.add_post(role='Regional Councillor', label='Cambridge (seat %d)' % (i + 1))
         for i in range(4):
-            organization.add_post(role='Councillor', label='Kitchener (seat %d)' % (i + 1))
+            organization.add_post(role='Regional Councillor', label='Kitchener (seat %d)' % (i + 1))
         for i in range(2):
-            organization.add_post(role='Councillor', label='Waterloo (seat %d)' % (i + 1))
+            organization.add_post(role='Regional Councillor', label='Waterloo (seat %d)' % (i + 1))
 
         yield organization
