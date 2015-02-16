23d4aec2f38a9b56fc6ba8ac40cea11f90f299d5
Thomas Rohée <roheethomas@gmail.com>
2015-02-16 14:33:43 -0500

    Saskatoon: updated links and scraper for new website

--- ca_sk_saskatoon/people.py
+++ ca_sk_saskatoon/people.py
@@ -3,8 +3,9 @@ from utils import CanadianScraper, CanadianPerson as Person
 
 import re
 
-COUNCIL_PAGE = "http://www.saskatoon.ca/CITY%20COUNCIL/YOUR%20WARD%20COUNCILLORS/Pages/default.aspx"
-EMAIL_URL = "http://apps2.saskatoon.ca/app/aForms/councillor.aspx"
+COUNCIL_PAGE = "https://www.saskatoon.ca/city-hall/mayor-city-councillors/city-councillors"
+MAYOR_PAGE = "https://www.saskatoon.ca/city-hall/mayor-city-councillors/mayors-office"
+EMAIL_URL = "https://www.saskatoon.ca/city-hall/mayor-city-councillors/contact-your-city-councillor"
 
 
 class SaskatoonPersonScraper(CanadianScraper):
@@ -12,65 +13,58 @@ class SaskatoonPersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
 
-        mayor_url = page.xpath('//td[@class="sask_LeftNavLinkContainer"]/a/@href')[0]
-        yield self.scrape_mayor(mayor_url)
+        yield self.scrape_mayor()
 
         email_page = self.lxmlize(EMAIL_URL)
-        c_options = email_page.xpath('//select[@id="councillorList"]/option[contains(text(), "Ward")]')
+        c_options = email_page.xpath('//select[@id="edit-submitted-councillor"]/option[contains(text(), "Ward")]')
         email_dict = dict((opt.text.split(' - ')[0], opt.attrib['value']) for
                           opt in c_options)
 
-        councillors = page.xpath('//td[@class="sask_LeftNavChildNodeContainer"]//a')
+        councillors = page.xpath('//h2[@class="landing-block-title"]/a')[:-1]
         for councillor in councillors:
-            district, name = councillor.text_content().split(' - Councillor ')
             url = councillor.attrib['href']
+            page = self.lxmlize(url)
+
+            district = page.xpath('//div[@id="main-content"]/h1/text()')[0]
+            name = page.xpath('//div[@id="main-content"]/h2/text()')[0]
 
             p = Person(primary_org='legislature', name=name, district=district, role='Councillor')
             p.add_source(COUNCIL_PAGE)
             p.add_source(EMAIL_URL)
             p.add_source(url)
 
-            page = self.lxmlize(url)
-            try:
+            if district in email_dict:
                 p.add_contact('email', email_dict[district])
-            except KeyError:
-                email = self.get_email(page)
-                p.add_contact('email', email)
 
-            contacts = page.xpath('//p[@class="para12"]')[0]
-            if not contacts.text_content().strip():
-                contacts = page.xpath('//p[@class="para12"]')[1]
-            contacts = re.split(r'\xa0', contacts.text_content())
-            contacts = [x for x in contacts if x.strip()]
-            for i, contact in enumerate(contacts):
-                if 'Contact' in contact:
+            contacts = page.xpath('//aside[@class="page-sidebar"]/div[1]/p')
+            for contact in contacts[:-1]:
+                contact_type = contact.xpath('./strong/text()')[0]
+                if 'Contact' in contact_type:
                     continue
-                if contact == contacts[-1]:
-                    break
-                contact_type = contact.replace(':', '').strip()
-                value = contacts[i + 1].replace('(', '').replace(') ', '-').strip()
+                value = contact.xpath('./a/text()')[0]
                 if 'Fax' in contact_type:
                     p.add_contact('fax', value, 'legislature')
                 if 'Phone' in contact_type:
                     p.add_contact(contact_type, value, contact_type)
+
             yield p
 
-    def scrape_mayor(self, url):
-        page = self.lxmlize(url)
-        name = page.xpath('//tr/td/p')[-1]
-        name = name.text_content().replace('Mayor', '')
-        image = page.xpath('//div[@class="sask_ArticleBody"]//img/@src')[0]
+    def scrape_mayor(self):
+        page = self.lxmlize(MAYOR_PAGE)
+        image = page.xpath('//img[contains(@alt, "Mayor")]/@src')[0]
 
         contact_url = page.xpath('//a[contains(text(), "Contact the Mayor")]/@href')[0]
-        page = self.lxmlize(contact_url)
+        contact_page = self.lxmlize(contact_url)
 
-        address = ' '.join(page.xpath('//div[@id="ctl00_PlaceHolderMain_RichHtmlField1__ControlWrapper_RichHtmlField"]/p[4]/text()')[1:])
-        phone = page.xpath('//div[@id="ctl00_PlaceHolderMain_RichHtmlField1__ControlWrapper_RichHtmlField"]/p[5]/span/text()')[0].replace('(', '').replace(') ', '-')
-        fax = page.xpath('//div[@id="ctl00_PlaceHolderMain_RichHtmlField1__ControlWrapper_RichHtmlField"]/p[6]/span/text()')[0].replace('(', '').replace(') ', '-')
+        infos = contact_page.xpath('//h4[contains(text(), "Address")]/following-sibling::p')
+        name = ' '.join(infos[0].text_content().split('\n')[0].split()[2:])
+        address = ' '.join(infos[0].text_content().split('\n')[1:])
+        phone = infos[1].text_content().split('\n')[0].replace('Phone', '')
+        fax = infos[1].text_content().split('\n')[1].replace('Fax', '')
 
         p = Person(primary_org='legislature', name=name, district='Saskatoon', role='Mayor')
-        p.add_source(COUNCIL_PAGE)
-        p.add_source(url)
+        p.add_source(MAYOR_PAGE)
+        p.add_source(contact_url)
         p.image = image
         p.add_contact('address', address, 'legislature')
         p.add_contact('voice', phone, 'legislature')
