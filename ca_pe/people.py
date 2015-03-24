d5fc8e704dd8554169d96a8a9e7e0ac782270c0e
Thomas Rohée <roheethomas@gmail.com>
2015-03-23 21:29:44 -0400

    PE: rewrote for new layout

--- ca_pe/people.py
+++ ca_pe/people.py
@@ -3,38 +3,39 @@ from utils import CanadianScraper, CanadianPerson as Person
 
 import re
 
-COUNCIL_PAGE = 'http://www.assembly.pe.ca/index.php3?number=1024584&lang=E'
+COUNCIL_PAGE = 'http://www.assembly.pe.ca/index.php3?number=1024555&lang=E'
 
 
 class PrinceEdwardIslandPersonScraper(CanadianScraper):
 
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
-        table = page.cssselect('table')[0]
-        rows = table.cssselect('tr')[1:]
-        assert len(rows) == 27  # There should be 27 districts
+        councillors = page.xpath('//table[1]//tr')
 
-        for row in rows:
-            districtnumcell, districtcell, membercell, dummy2 = row.cssselect('td')
+        for councillor in councillors:
+            name = councillor.xpath('./td[2]//a[1]//text()')[0]
 
-            district_name = districtcell.cssselect('a')[0].text_content().strip()
-            district = district_name.replace(' - ', '-')
-            name = (membercell.cssselect('a')[0].text_content().replace('Hon. ', '')
-                    .replace(' (LIB)', '').replace(' (PC)', '').strip())
-            url = membercell.cssselect('a')[0].get('href')
-            email, phone, photo_url = self.scrape_extended_info(url)
-            p = Person(primary_org='legislature', name=name, district=district, role='MLA', image=photo_url)
+            district_name = councillor.xpath('./td[2]//a[contains(.//text(), "MLA")]//text()')[0].split(':')[1].replace('St ', 'St. ').split('-')
+            district = district_name[0].strip() + '-' + district_name[1].strip()
+            url = councillor.xpath('./td[2]//a[1]/@href')[0]
+            ext_infos = self.scrape_extended_info(url)
+            p = Person(primary_org='legislature', name=name, district=district, role='MLA')
             p.add_source(COUNCIL_PAGE)
             p.add_source(url)
-            p.add_contact('email', email)
-            p.add_contact('voice', phone, 'legislature')
+
+            if ext_infos:  # councillor pages might return errors
+                email, phone, photo_url = ext_infos
+                p.image = photo_url
+                p.add_contact('email', email)
+                p.add_contact('voice', phone, 'legislature')
             yield p
 
     def scrape_extended_info(self, url):
         root = self.lxmlize(url)
-        main = root.cssselect('#content table')[0]
-        photo_url = main.cssselect('img')[0].get('src')
-        contact_cell = main.cssselect('td:contains("Contact information")')[0]
-        phone = re.search(r'(?:Telephone|Tel|Phone):(.+?)\n', contact_cell.text_content()).group(1)
-        email = self.get_email(contact_cell)
-        return email, phone, photo_url
+        if 'DB Error' not in root.text_content():
+            main = root.cssselect('#content table')[0]
+            photo_url = main.cssselect('img')[0].get('src')
+            contact_cell = main.cssselect('td:contains("Contact information")')[0]
+            phone = re.search(r'(?:Telephone|Tel|Phone):(.+?)\n', contact_cell.text_content()).group(1)
+            email = self.get_email(contact_cell)
+            return email, phone, photo_url
