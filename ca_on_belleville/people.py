2e82b43e7def681e03ed569a8570f425f8e266d2
Thomas Rohée <roheethomas@gmail.com>
2015-05-28 12:14:14 -0400

    Belleville reenabled: fixed scraper

--- ca_on_belleville/people.py
+++ ca_on_belleville/people.py
@@ -13,8 +13,8 @@ class BellevillePersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
 
-        mayor_name_elem = page.xpath('//div[@class="content-field"]//a[1]')[0]
-        yield self.person_from_elem(mayor_name_elem, 'Belleville', 'Mayor')
+        mayor_node = page.xpath('//div[@class="content-field"]/h3[contains(./text(), "Mayor")]/following-sibling::p[2]')[0]
+        yield self.scrape_mayor(mayor_node)
 
         ward_elems = page.xpath('//h3[contains(text(), "Councillors")]')
         for ward_elem in ward_elems:
@@ -28,14 +28,25 @@ class BellevillePersonScraper(CanadianScraper):
     def person_from_elem(self, name_elem, district, role):
         name = name_elem.text_content()
         phone = name_elem.xpath('./following-sibling::text()[2]')[0].split(': ')[1]
-        if not phone.startswith('613-'):
-            corrected_phone = '613-' + phone
-        else:
-            corrected_phone = phone
         email = name_elem.xpath('./following-sibling::a//text()')[0]
-        photo_url = name_elem.xpath('./parent::p/preceding::img[1]/@src')[0]
+        photo_url = name_elem.xpath('./preceding::img[1]/@src')[0]
+
         p = Person(primary_org='legislature', name=name, district=district, role=role, image=photo_url)
         p.add_source(COUNCIL_PAGE)
-        p.add_contact('voice', corrected_phone, 'legislature')
+        p.add_contact('voice', phone, 'legislature')
+        p.add_contact('email', email)
+        return p
+
+    def scrape_mayor(self, node):
+        name = node.xpath('./strong/text()')[0]
+        phone = node.xpath('./text()')[2].split(': ')[1]
+        fax = node.xpath('./text()')[3].split(': ')[1]
+        email = node.xpath('./a/text()')[0]
+        photo_url = node.xpath('./preceding::p//img/@src')[0]
+
+        p = Person(primary_org='legislature', name=name, district='Belleville', role='Mayor', image=photo_url)
+        p.add_source(COUNCIL_PAGE)
+        p.add_contact('voice', phone, 'legislature')
+        p.add_contact('fax', fax, 'legislature')
         p.add_contact('email', email)
         return p
