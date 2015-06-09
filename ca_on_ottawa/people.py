8343efc085feb9490f3528d38d2396f0f791e7f1
Thomas Rohée <roheethomas@gmail.com>
2015-06-08 20:04:10 -0400

    Ottawa: updated csv url

--- ca_on_ottawa/people.py
+++ ca_on_ottawa/people.py
@@ -6,4 +6,4 @@ COUNCIL_PAGE = 'http://ottawa.ca/en/city-council'
 
 
 class OttawaPersonScraper(CSVScraper):
-    csv_url = 'http://data.ottawa.ca/en/storage/f/2015-01-22T170049/Elected-Officials-%282014-2018%29-v.2.csv'
+    csv_url = 'http://data.ottawa.ca/en/dataset/fd26ae83-fe1a-40d8-8951-72df40021c82/resource/33a437d3-a06d-4c56-a7fe-4fd622364ce6/download/elected-officials-282014-201829-v.2.csv'
