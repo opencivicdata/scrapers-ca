ba92f1d94847fb27172c48e69d3a3ba8cda3ba06
Thomas Rohée <roheethomas@gmail.com>
2015-11-04 17:12:08 -0500

    Terrebonne: update scraper for new website

--- ca_qc_terrebonne/people.py
+++ ca_qc_terrebonne/people.py
@@ -18,6 +18,7 @@ class TerrebonnePersonScraper(CanadianScraper):
             p = Person(primary_org='legislature', name=name, district=district, role='Conseiller',
                        image=photo_url)
             p.add_source(COUNCIL_PAGE)
+            p.add_contact('voice', phone, 'legislature')
             if email_mailto:
                 email = email_mailto[0].split('mailto:')[1]
                 p.add_contact('email', email)
@@ -31,6 +32,7 @@ class TerrebonnePersonScraper(CanadianScraper):
         p = Person(primary_org='legislature', name=name, district='Terrebonne', role='Maire',
                    image=photo_url)
         p.add_source(COUNCIL_PAGE)
+        p.add_contact('voice', phone, 'legislature')
         if email_mailto:
             email = email_mailto[0].split('mailto:')[1]
             p.add_contact('email', email)
