79be8aa139bac6bfa6dba5f2e646971b9b5f2bb7
James McKinney <james@slashpoundbang.com>
2015-12-22 15:13:44 -0500

    ca_nl: some fixes

--- ca_nl/people.py
+++ ca_nl/people.py
@@ -20,49 +20,43 @@ def get_party(abbr):
 class NewfoundlandAndLabradorPersonScraper(CanadianScraper):
 
     def scrape(self):
-        member_parties = dict(process_parties(self.lxmlize(PARTY_PAGE)))
+        member_parties = {}
+        for elem in self.lxmlize(PARTY_PAGE).xpath('//h3/u'):
+            party = elem.text
+            members = elem.xpath('./ancestor::tr/following-sibling::tr/td')
+            member_names = [elem.text_content().replace('\xa0', ' ') for elem in members]
+            for name in member_names:
+                member_parties[name] = party
 
         page = self.lxmlize(COUNCIL_PAGE)
-        for row in page.xpath('//table[not(@id="footer")]/tr')[1:]:
-            try:
-                name, district, _, email = [cell.text_content().replace('\xa0', ' ') for cell in row]
-                email = email.split()[0]
-            except ValueError:
-                continue
-            phone = row[2].xpath('text()[1]')[0]
-            try:
-                photo_page_url = row[0].xpath('./a/@href')[0]
-            except IndexError:
-                continue  # there is a vacant district
+        for row in page.xpath('//table[not(@id="footer")][not(@style)]/tr')[1:]:
+            name, district, _, email = [cell.text_content().replace('\xa0', ' ') for cell in row]
+
+            text = row[2].xpath('./text()')
+            if text and text[0].strip():
+                phone = text[0]
+            else:
+                phone = None
+
             district = district.replace(' - ', '—')  # m-dash
             district = district.replace('Bay De Verde', 'Bay de Verde')
             party = get_party(member_parties[name.strip()])
 
-            photo_page = self.lxmlize(photo_page_url)
-            photo_node = photo_page.xpath('//table//img/@src')
+            p = Person(primary_org='legislature', name=name, district=district, role='MHA', party=party)
 
-            # handling case when no picture is available (Terry French isn't councillor anymore)
-            if photo_node:
-                photo_url = photo_node[0]
-                p = Person(primary_org='legislature', name=name, district=district, role='MHA',
-                           party=party, image=photo_url)
-            else:
-                p = Person(primary_org='legislature', name=name, district=district, role='MHA',
-                           party=party)
+            p.add_contact('email', email)
+            if phone:
+                p.add_contact('voice', phone, 'legislature')
 
             p.add_source(COUNCIL_PAGE)
             p.add_source(PARTY_PAGE)
-            p.add_source(photo_page_url)
-            p.add_contact('email', email)
-            p.add_contact('voice', phone, 'legislature')
+
+            photo_page_url = row[0].xpath('./a/@href')
+            if photo_page_url:
+                photo_page = self.lxmlize(photo_page_url[0])
+                photo_node = photo_page.xpath('//table//img/@src')
+                photo_url = photo_node[0]
+                p.image = photo_url
+                p.add_source(photo_page_url)
+
             yield p
-
-
-def process_parties(partypage):
-    # return generator of (name, party) tuples for MHAs
-    for elem in partypage.xpath('//h3/u'):
-        party = elem.text
-        members = elem.xpath('./ancestor::tr/following-sibling::tr/td/a')
-        member_names = [elem.text.replace('\xa0', ' ') for elem in members]
-        for name in member_names:
-            yield (name, party)
