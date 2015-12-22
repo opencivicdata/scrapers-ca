c97a7a5b458f92026b72c4700fa7235ec53ae7ec
James McKinney <james@slashpoundbang.com>
2015-12-22 14:48:05 -0500

    ca_qc_gatineau: fix

--- ca_qc_gatineau/people.py
+++ ca_qc_gatineau/people.py
@@ -15,23 +15,26 @@ class GatineauPersonScraper(CanadianScraper):
         # it's all javascript rendered on the client... wow.
         js = page.xpath('string(//div[@class="inner_container"]/div/script[2])')  # allow string()
         districts = re.findall(r'arrayDistricts\[a.+"(.+)"', js)
-        members = re.findall(r'arrayMembres\[a.+"(.+)"', js)
+        names = re.findall(r'arrayMembres\[a.+"(.+)"', js)
         urls = re.findall(r'arrayLiens\[a.+"(.+)"', js)
         # first item in list is mayor
-        p = Person(primary_org='legislature', name=members[0], district='Gatineau', role='Maire')
+        p = Person(primary_org='legislature', name=names[0], district='Gatineau', role='Maire')
         p.add_source(COUNCIL_PAGE)
         p.add_source(MAYOR_CONTACT_PAGE)
         email = 'maire@gatineau.ca'  # hardcoded
         p.add_contact('email', email)
         yield p
 
-        for raw_district, member, url in list(zip(districts, members, urls))[1:]:
+        for raw_district, name, url in list(zip(districts, names, urls))[1:]:
+            if name == 'Vacant':
+                continue
+
             profile_url = COUNCIL_PAGE + '/' + url.split('/')[-1]
             profile_page = self.lxmlize(profile_url)
             photo_url = profile_page.xpath('//img/@src')[0]
             district = 'District ' + re.search('\d+', raw_district).group(0)
             email = self.get_email(profile_page)
-            p = Person(primary_org='legislature', name=member, district=district, role='Conseiller')
+            p = Person(primary_org='legislature', name=name, district=district, role='Conseiller')
             p.add_source(COUNCIL_PAGE)
             p.add_source(profile_url)
             p.image = photo_url
