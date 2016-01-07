6d15f10f4adf8cd9b76e48cdfa0c19dde85d4e98
James McKinney <james@slashpoundbang.com>
2016-01-07 16:20:53 -0500

    try to fix ssl error

--- utils.py
+++ utils.py
@@ -77,7 +77,7 @@ email_re = re.compile(r'([A-Za-z0-9._-]+@(?:[A-Za-z0-9-]+\.)+[A-Za-z]{2,})')
 
 styles_of_address = {}
 for gid in range(3):
-    response = requests.get('https://docs.google.com/spreadsheets/d/11qUKd5bHeG5KIzXYERtVgs3hKcd9yuZlt-tCTLBFRpI/pub?single=true&gid={}&output=csv'.format(gid))
+    response = requests.get('https://docs.google.com/spreadsheets/d/11qUKd5bHeG5KIzXYERtVgs3hKcd9yuZlt-tCTLBFRpI/pub?single=true&gid={}&output=csv'.format(gid), verify='/usr/lib/ssl/certs/ca-certificates.crt')
     if response.status_code == 200:
         response.encoding = 'utf-8'
         for row in csv.DictReader(StringIO(response.text)):
