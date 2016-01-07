4ce206a52c81f15a8b2754f0265e49eef978262c
James McKinney <james@slashpoundbang.com>
2016-01-07 17:15:30 -0500

    ca_qc_longueuil: fix exclude_type_ids

--- ca_qc_longueuil/__init__.py
+++ ca_qc_longueuil/__init__.py
@@ -8,4 +8,4 @@ class Longueuil(CanadianJurisdiction):
     division_name = 'Longueuil'
     name = 'Conseil municipal de Longueuil'
     url = 'http://www.longueuil.ca'
-    exclude_type_ids = ['district']
+    exclude_type_ids = ['borough']
