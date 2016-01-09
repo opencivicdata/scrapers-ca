9c4e2195676a3733d97a5a7580bbebc980376a85
Patrick Connolly <patrick.c.connolly@gmail.com>
2016-01-08 20:34:00 -0800

    Temporarily scrape from amended people CSV hosted in gist.

--- ca_on_toronto/people.py
+++ ca_on_toronto/people.py
@@ -11,3 +11,7 @@ class TorontoPersonScraper(CSVScraper):
         'Norman Kelly': ['Norm Kelly'],
         'Justin Di Ciano': ['Justin J. Di Ciano'],
     }
+
+    # TODO: Remove once original file is fixed.
+    # See: https://github.com/opencivicdata/scrapers-ca/issues/155#issuecomment-169855328
+    csv_url = 'https://gist.githubusercontent.com/patcon/70656c30f544efd3673a/raw/4fc61631243da8dea0a4f4b31f5d5a5b99f26c75/Toronto_Elected_Officials.csv'
