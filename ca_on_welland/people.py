86b4b03fbdd2f25de2692d4c325b2d693b6e7ac3
James McKinney <james@slashpoundbang.com>
2016-06-02 17:37:07 -0400

    ca_on_welland: update url

--- ca_on_welland/people.py
+++ ca_on_welland/people.py
@@ -5,6 +5,6 @@ COUNCIL_PAGE = 'http://www.welland.ca/Council/index.asp'
 
 
 class WellandPersonScraper(CSVScraper):
-    csv_url = 'http://www.welland.ca/open/Datasheets/Welland_mayor_and_council_members.csv'
+    csv_url = 'https://www.niagaraopendata.ca//dataset/b38e2f85-bcd4-43fd-95d5-f513919514d9/resource/590c9110-01f3-4256-8ac1-9c5cbeca88c5/download/city-of-welland-mayor-and-council-members.csv'
     encoding = 'windows-1252'
     many_posts_per_area = True
