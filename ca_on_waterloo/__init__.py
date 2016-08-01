25c4e2eed858adbb4126eed8935ed298587551ee
James McKinney <james@slashpoundbang.com>
2016-08-01 15:09:02 -0400

    Remove unnecessary get_organizations method

--- ca_on_waterloo/__init__.py
+++ ca_on_waterloo/__init__.py
@@ -9,12 +9,3 @@ class Waterloo(CanadianJurisdiction):
     division_name = 'Waterloo'
     name = 'Waterloo City Council'
     url = 'http://www.waterloo.ca'
-
-    def get_organizations(self):
-        organization = Organization(self.name, classification=self.classification)
-
-        organization.add_post(role='Mayor', label='Waterloo', division_id=self.division_id)
-        for ward_number in range(1, 8):
-            organization.add_post(role='Councillor', label='Ward {}'.format(ward_number))
-
-        yield organization
