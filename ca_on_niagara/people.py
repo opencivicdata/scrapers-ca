94aec9e4a6501e875dbd6b59df57598f742a82da
James McKinney <james@slashpoundbang.com>
2016-08-01 16:28:42 -0400

    ca_on_niagara: Add comments about new file

--- ca_on_niagara/people.py
+++ ca_on_niagara/people.py
@@ -5,5 +5,10 @@ COUNCIL_PAGE = 'http://www.niagararegion.ca/test/sherpa-list-to-csv.aspx?list=co
 
 
 class NiagaraPersonScraper(CSVScraper):
+    # The new data file:
+    # * has underscores in headers
+    # * yses "District_ID" instead of "District name"
+    # * prefixes "District_ID" with "Niagara Region - "
+    # https://www.niagaraopendata.ca//dataset/ee767222-c7fc-4541-8cad-a27276a3522b/resource/af5621ad-c2e4-4569-803f-4aadca4173be/download/councilelectedofficials.csv
     csv_url = 'http://www.niagararegion.ca/test/sherpa-list-to-csv.aspx?list=council-elected-officials-csv'
     many_posts_per_area = True
