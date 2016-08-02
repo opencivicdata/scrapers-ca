69aaaf65a483c08ebacd92ed48a56084055e62b0
James McKinney <james@slashpoundbang.com>
2016-08-02 09:09:47 -0400

    ca_on_st_catharines: switch to CSV

--- ca_on_st_catharines/__init__.py
+++ ca_on_st_catharines/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from utils import CanadianJurisdiction
+from pupa.scrape import Organization
 
 
 class StCatharines(CanadianJurisdiction):
@@ -8,3 +9,13 @@ class StCatharines(CanadianJurisdiction):
     division_name = 'St. Catharines'
     name = 'St. Catharines City Council'
     url = 'http://www.stcatharines.ca'
+
+    def get_organizations(self):
+        organization = Organization(self.name, classification=self.classification)
+
+        organization.add_post(role='Mayor', label='St. Catharines', division_id=self.division_id)
+        for ward_name in ('Grantham', 'Merritton', 'Port Dalhousie', "St. Andrew's", "St. George's", "St. Patrick's"):
+            for seat_number in range(1, 3):
+                organization.add_post(role='Councillor', label='{} (seat {})'.format(ward_name, seat_number))
+
+        yield organization
--- ca_on_st_catharines/people.py
+++ ca_on_st_catharines/people.py
@@ -1,73 +1,9 @@
 from __future__ import unicode_literals
-from utils import CanadianScraper, CanadianPerson as Person
+from utils import CSVScraper
 
 COUNCIL_PAGE = 'http://www.stcatharines.ca/en/governin/MayorCouncil.asp'
 
 
-class StCatharinesPersonScraper(CanadianScraper):
-
-    def scrape(self):
-        # TODO Use https://niagaraopendata.ca/dataset/st-catharines-councilors
-        page = self.lxmlize(COUNCIL_PAGE)
-
-        council = page.xpath('//ul[@id="subNav"]/li[@class="withChildren"]/ul/li/a')
-        yield self.scrape_mayor(council[0].attrib['href'])
-
-        wards = council[1:]
-
-        for ward in wards:
-            url = ward.attrib['href']
-            ward_page = self.lxmlize(url)
-
-            district = ward_page.xpath('//div[@class="contentArea"]/div[1]/h1/text()')[0].split('-')[1]
-            councillors = ward_page.xpath('//div[@class="contentArea"]/div[1]/h2')
-            for councillor in councillors:
-                name = councillor.text_content().replace('Coun.', '')
-
-                p = Person(primary_org='legislature', name=name, district=district, role='Councillor')
-                p.add_source(COUNCIL_PAGE)
-                p.add_source(url)
-
-                image = councillor.xpath('./following-sibling::p[1]//@src')
-                if image:
-                    p.image = image[0]
-
-                if councillor.xpath('./following-sibling::p[1]//a'):
-                    position = 1
-                elif councillor.xpath('./following-sibling::p[2]//a'):
-                    position = 2
-                else:
-                    position = 3
-                phone = self.get_phone(councillor.xpath('./following-sibling::p[{}]'.format(position))[0], area_codes=[289, 905])
-                email = self.get_email(councillor.xpath('./following-sibling::p[{}]'.format(position))[0])
-                address = ' '.join(councillor.xpath('./following-sibling::p[{}]/text()'.format(position + 1)))
-                address = address.replace('Mail:', '')
-
-                p.add_contact('address', address, 'legislature')
-                p.add_contact('voice', phone, 'legislature')
-                p.add_contact('email', email)
-                yield p
-
-    def scrape_mayor(self, url):
-        mayor_page = self.lxmlize(url)
-        mayor_info = mayor_page.xpath('//div[@class="contentArea"]/div[1]')[0]
-        name = mayor_info.xpath('./h2/text()')[0].replace('Mayor', '')
-
-        p = Person(primary_org='legislature', name=name, district='St. Catharines', role='Mayor')
-        p.add_source(COUNCIL_PAGE)
-        p.add_source(url)
-
-        p.image = mayor_info.xpath('./h2/img/@src')[0]
-
-        phone = mayor_info.xpath('./p[1]/text()')[1]
-        address = ' '.join(mayor_info.xpath('./p[2]/text()'))
-
-        phone = phone.split('or')[0].replace('Phone:', '')
-        address = address.replace('Mail:', '')
-        email = 'mayorsadministration@stcatharines.ca'
-
-        p.add_contact('address', address, 'legislature')
-        p.add_contact('voice', phone, 'legislature')
-        p.add_contact('email', email)
-
-        return p
+class StCatharinesPersonScraper(CSVScraper):
+    csv_url = 'https://www.niagaraopendata.ca//dataset/ccb9c7f1-d3b0-4049-9c08-e4f7b048722c/resource/128a39f0-8234-4708-b69b-9c73f7a55475/download/stcathcounsilors.csv'
+    many_posts_per_area = True
