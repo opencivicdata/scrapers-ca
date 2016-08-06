2cb27d9ccacc5510c8c7acf8e4a46b56da10fc8b
James McKinney <james@slashpoundbang.com>
2016-08-06 18:48:44 -0400

    ca_on_guelph: fix multiple memberships per post

--- ca_on_guelph/__init__.py
+++ ca_on_guelph/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from utils import CanadianJurisdiction
+from pupa.scrape import Organization
 
 
 class Guelph(CanadianJurisdiction):
@@ -9,3 +10,13 @@ class Guelph(CanadianJurisdiction):
     name = 'Guelph City Council'
     url = 'http://guelph.ca'
     use_type_id = True
+
+    def get_organizations(self):
+        organization = Organization(self.name, classification=self.classification)
+
+        organization.add_post(role='Mayor', label='Guelph', division_id=self.division_id)
+        for ward_number in range(1, 7):
+            for seat_number in range(1, 3):
+                organization.add_post(role='Councillor', label='Ward {} (seat {})'.format(ward_number, seat_number))
+
+        yield organization
--- ca_on_guelph/people.py
+++ ca_on_guelph/people.py
@@ -6,3 +6,4 @@ COUNCIL_PAGE = 'http://guelph.ca/city-hall/mayor-and-council/city-council/'
 
 class GuelphPersonScraper(CSVScraper):
     csv_url = 'http://open.guelph.ca/wp-content/uploads/2015/01/GuelphCityCouncil2014-2018ElectedOfficalsContactInformation1.csv'
+    many_posts_per_area = True
