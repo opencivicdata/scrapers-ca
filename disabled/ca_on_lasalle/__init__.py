69b9038f7dd4127381a5d85e51fc31858c67bb3d
James McKinney <james@slashpoundbang.com>
2016-08-06 19:58:58 -0400

    Easier copy-paste

--- disabled/ca_on_lasalle/__init__.py
+++ disabled/ca_on_lasalle/__init__.py
@@ -13,7 +13,7 @@ class LaSalle(CanadianJurisdiction):
     def get_organizations(self):
         organization = Organization(self.name, classification=self.classification)
 
-        organization.add_post(role='Mayor', label='LaSalle', division_id=self.division_id)
+        organization.add_post(role='Mayor', label=self.division_name, division_id=self.division_id)
         organization.add_post(role='Deputy Mayor', label='LaSalle', division_id=self.division_id)
         for seat_number in range(1, 6):
             organization.add_post(role='Councillor', label='LaSalle (seat {})'.format(seat_number), division_id=self.division_id)
--- disabled/ca_on_pickering/__init__.py
+++ disabled/ca_on_pickering/__init__.py
@@ -13,7 +13,7 @@ class Pickering(CanadianJurisdiction):
     def get_organizations(self):
         organization = Organization(self.name, classification=self.classification)
 
-        organization.add_post(role='Mayor', label='Pickering', division_id=self.division_id)
+        organization.add_post(role='Mayor', label=self.division_name, division_id=self.division_id)
         for ward_number in range(1, 4):
             organization.add_post(role='Regional Councillor', label='Ward {}'.format(ward_number))
             organization.add_post(role='Councillor', label='Ward {}'.format(ward_number))
