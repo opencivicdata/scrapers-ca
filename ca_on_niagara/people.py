d34de839b4f91689804282dd074012da4adcd7d2
James McKinney <james@slashpoundbang.com>
2016-09-11 15:08:10 -0400

    Correct typo

--- ca_on_niagara/people.py
+++ ca_on_niagara/people.py
@@ -7,7 +7,7 @@ COUNCIL_PAGE = 'http://www.niagararegion.ca/test/sherpa-list-to-csv.aspx?list=co
 class NiagaraPersonScraper(CSVScraper):
     # The new data file:
     # * has underscores in headers
-    # * yses "District_ID" instead of "District name"
+    # * uses "District_ID" instead of "District name"
     # * prefixes "District_ID" with "Niagara Region - "
     # https://www.niagaraopendata.ca//dataset/ee767222-c7fc-4541-8cad-a27276a3522b/resource/af5621ad-c2e4-4569-803f-4aadca4173be/download/councilelectedofficials.csv
     csv_url = 'http://www.niagararegion.ca/test/sherpa-list-to-csv.aspx?list=council-elected-officials-csv'
