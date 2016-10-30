967069f25ccaa1c55dd2fffc72d06b45c233f8a5
James McKinney <james@slashpoundbang.com>
2016-10-30 11:13:19 -0400

    ca_on_toronto: fix

--- ca_on_toronto/people.py
+++ ca_on_toronto/people.py
@@ -7,6 +7,11 @@ COUNCIL_PAGE = 'http://www1.toronto.ca/wps/portal/contentonly?vgnextoid=c3a83293
 class TorontoPersonScraper(CSVScraper):
     csv_url = 'http://www1.toronto.ca/City%20Of%20Toronto/Information%20&%20Technology/Open%20Data/Data%20Sets/Assets/Files/Toronto_Elected_Officials.csv'
     district_name_format_string = '{district name} ({district id})'
+    corrections = {
+        'last name': {
+            'BailÆo': 'Bailão',
+        },
+    }
     other_names = {
         'Norman Kelly': ['Norm Kelly'],
         'Justin Di Ciano': ['Justin J. Di Ciano'],
