c978bcf724bcdba4da7b351346e877a8da6243ae
James McKinney <james@slashpoundbang.com>
2016-11-14 13:25:57 -0500

    ca_qc_montreal_est: fix

--- ca_qc_montreal_est/people.py
+++ ca_qc_montreal_est/people.py
@@ -2,30 +2,29 @@
 from __future__ import unicode_literals
 from utils import CanadianScraper, CanadianPerson as Person
 
-import re
-
-COUNCIL_PAGE = 'http://ville.montreal-est.qc.ca/site2/index.php?option=com_content&view=article&id=12&Itemid=59'
+COUNCIL_PAGE = 'http://ville.montreal-est.qc.ca/la-ville/conseil-municipal/conseils-municipaux/'
 
 
 class MontrealEstPersonScraper(CanadianScraper):
 
     def scrape(self):
-        page = self.lxmlize(COUNCIL_PAGE, user_agent='Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)')
+        page = self.lxmlize(COUNCIL_PAGE)
 
-        councillors = page.xpath('//table[last()]//tr/td[1]//strong')
-        for i, councillor in enumerate(councillors):
-            name = councillor.text_content().strip()
-            if not name:
-                continue
+        councillors = page.xpath('//table')
+        assert len(councillors), 'No councillors found'
+        for councillor in councillors:
+            name = councillor.xpath('.//h3')[0].text_content()
+
             if 'maire' in name:
-                name = name.split('maire')[1].strip()
+                name = name.split(' ', 2)[-1]
                 district = 'Montréal-Est'
+                role = 'Maire'
             else:
-                district = councillor.xpath('./ancestor::td/following-sibling::td//strong')[-1].text_content()
-                district = 'District {}'.format(re.sub('\D+', '', district))
-            email = self.get_email(councillor, './ancestor::tr/following-sibling::tr')
-            role = 'Maire' if i == 0 else 'Conseiller'
+                district = 'District {}'.format(councillor.xpath('.//h3')[1].text_content()[-1])
+                role = 'Conseiller'
+
             p = Person(primary_org='legislature', name=name, district=district, role=role)
+            p.image = councillor.xpath('.//@src')[0]
+            p.add_contact('email', self.get_email(councillor))
             p.add_source(COUNCIL_PAGE)
-            p.add_contact('email', email)
             yield p
