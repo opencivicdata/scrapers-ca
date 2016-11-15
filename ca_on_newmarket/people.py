631bf34dddccb9b1edc9892d7866ac21f807ffda
James McKinney <james@slashpoundbang.com>
2016-11-15 14:38:51 -0500

    ca_on_newmarket: add

---
+++ ca_on_newmarket/people.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+from utils import CSVScraper
+
+COUNCIL_PAGE = 'http://www.newmarket.ca/en/townhall/contactinformationmayorandtowncouncil.asp'
+
+
+class NewmarketPersonScraper(CSVScraper):
+    # There's no permalink to the ZIP or CSV.
+    # http://open.newmarket.ca/opendata/navigo/#/show/SQLQ6R6C_10?disp=D1579414319D
+    csv_url = 'http://represent.opennorth.ca.s3.amazonaws.com/data/mayor_and_councillors_contact_information_2014_2018.csv'
