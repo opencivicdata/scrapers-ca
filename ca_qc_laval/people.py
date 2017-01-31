4c02b86d21757dd7e71d89676e36cb26e6824f61
James McKinney <james@slashpoundbang.com>
2017-01-31 12:47:49 -0500

    ca_qc_laval: fix

--- ca_qc_laval/people.py
+++ ca_qc_laval/people.py
@@ -5,7 +5,6 @@ from utils import CSVScraper
 class LavalPersonScraper(CSVScraper):
     csv_url = 'https://www.donneesquebec.ca/recherche/dataset/8fe69713-fade-4751-a0b4-7e57a81886b1/resource/bb38e19e-26ab-495c-a0f7-ed6b3268b6e6/download/cusersapp.netappdatalocaltemp288c1490-df30-472a-8170-dd06728f449alistedeselus2013-2017.csv'
     # Absurdly, Québec has decided "les en-têtes ne comportent pas de caractères accentués ou d'espaces".
-    # @todo Waiting for Laval to restore accents and cedillas. (2016-08-01)
     header_converter = lambda self, s: {
         'Prenom': 'first name',
         'Nom': 'last name',
@@ -24,3 +23,15 @@ class LavalPersonScraper(CSVScraper):
         "URL-d'une-photo": 'photo url',
         'URL-source': 'source url',
     }.get(s, s)
+    # Québec also removes accents and cedillas from data.
+    corrections = {
+        'district name': {
+            'Concorde - Bois-de-Boulogne': 'Concorde-Bois-de-Boulogne',
+            "L'Abord-a-Plouffe": 'Abord-à-Plouffe',
+            "L'Oree-des-bois": 'Orée-des-Bois',
+            'Laval-les-Iles': 'Laval-les-Îles',
+            'Marc-Aurele-Fortin': 'Marc-Aurèle-Fortin',
+            'Saint-Francois': 'Saint-François',
+            'Sainte-Dorothee': 'Sainte-Dorothée',
+        },
+    }
