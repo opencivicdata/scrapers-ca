b9cc16fb07775471c123bb9ddc253a69acf2f7a2
Cameron <cameron@cameronmonagle.com>
2017-02-01 15:52:51 -0500

    Updating Edmonton scraper for site redesign

--- ca_ab_edmonton/people.py
+++ ca_ab_edmonton/people.py
@@ -4,27 +4,29 @@ from utils import CanadianScraper, CanadianPerson as Person
 COUNCIL_PAGE = 'http://www.edmonton.ca/city_government/city_organization/city-councillors.aspx'
 MAYOR_PAGE = 'http://www.edmonton.ca/city_government/city_organization/the-mayor.aspx'
 
-
 class EdmontonPersonScraper(CanadianScraper):
 
     def scrape(self):
         yield self.scrape_mayor()
-
         page = self.lxmlize(COUNCIL_PAGE)
-        councillors = page.xpath('//th[contains(text(), "Ward")]')
+
+
+        councillors = page.xpath('//div[contains(@class, "documentexcerpt-module__item")]')
         assert len(councillors), 'No councillors found'
         for cell in councillors:
-            district = cell.text
             name = cell[1].text
             if name != 'Vacant':
-                page_url = cell[1].attrib['href']
+                page_url = cell[0].attrib['href']
                 page = self.lxmlize(page_url)
+                district_name = page.xpath('//h1[contains(@class, "page-title")]')[0].text_content()
+                district = district_name.split(' - ')[0]
+                name = district_name.split(' - ')[1]
 
                 p = Person(primary_org='legislature', name=name, district=district, role='Councillor')
                 p.add_source(COUNCIL_PAGE)
                 p.add_source(page_url)
 
-                image = page.xpath('//div[@id="contentArea"]//img/@src')
+                image = page.xpath('//div[contains(@class, "content")]//img/@src')
                 if image:
                     p.image = image[0]
 
@@ -32,8 +34,9 @@ class EdmontonPersonScraper(CanadianScraper):
                 if address:
                     address = address[0].text_content()
                     p.add_contact('address', address, 'legislature')
+                    print(address)
 
-                contacts = page.xpath('//table[@class="contactListing"]//tr')
+                contacts = page.xpath('//table[@summary="Contact information"]//tr')
                 for contact in contacts:
                     contact_type = contact.xpath('./th/text()')[0]
                     value = contact.xpath('./td//text()')[0]
@@ -42,6 +45,7 @@ class EdmontonPersonScraper(CanadianScraper):
                     elif 'Website' in contact_type or 'Facebook' in contact_type or 'Twitter' in contact_type:
                         value = contact.xpath('./td/a/text()')[0]
                         p.add_link(value)
+                        print(value)
                     elif 'Telephone' in contact_type:
                         p.add_contact('voice', value, 'legislature')
                     elif 'Fax' in contact_type:
