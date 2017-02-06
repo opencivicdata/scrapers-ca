304e590731e5dddbedad4476bed398078c9005cc
James McKinney <james@slashpoundbang.com>
2017-02-06 17:55:13 -0500

    ca_nb_saint_john: fix

--- ca_nb_saint_john/__init__.py
+++ ca_nb_saint_john/__init__.py
@@ -14,6 +14,7 @@ class SaintJohn(CanadianJurisdiction):
         organization = Organization(self.name, classification=self.classification)
 
         organization.add_post(role='Mayor', label=self.division_name, division_id=self.division_id)
+        organization.add_post(role='Deputy Mayor', label=self.division_name, division_id=self.division_id)
         for seat_number in range(1, 3):
             organization.add_post(role='Councillor', label='Saint John (seat {})'.format(seat_number), division_id=self.division_id)
         for ward_number in range(1, 5):
--- ca_nb_saint_john/people.py
+++ ca_nb_saint_john/people.py
@@ -6,6 +6,7 @@ class SaintJohnPersonScraper(CSVScraper):
     csv_url = 'http://www.saintjohn.ca/site/media/SaintJohn/Saint_John_NB_Elected_Officials_Contact_Info.zip'
     filename = 'Saint_John_NB_Elected_Officials_Contact_Info.csv'
     many_posts_per_area = True
+    unique_roles = ('Mayor', 'Deputy Mayor')
     corrections = {
         'district name': {
             'City of Saint John': 'Saint John',
