34b1bf0f4404295cce56b7a4ca3ffb3d51ad7474
James McKinney <james@slashpoundbang.com>
2017-02-11 20:42:55 -0500

    ca_nb_moncton: fix

--- ca_nb_moncton/people.py
+++ ca_nb_moncton/people.py
@@ -6,3 +6,4 @@ class MonctonPersonScraper(CSVScraper):
     csv_url = 'http://www.moncton.ca/OpenData/Elected_Officials_Contact_Information-Coordonnes_des_elus.zip'
     encoding = 'windows-1252'
     filename = 'Elected Officials Contact Information.csv'
+    many_posts_per_area = True
