f2647961ffaa2ac44d7e3568c37a5ec3fdca7378
James McKinney <james@slashpoundbang.com>
2017-02-14 18:30:51 -0500

    Remove TODO, remove check_sessions (removed from Pupa)

--- ca_on_toronto/__init__.py
+++ ca_on_toronto/__init__.py
@@ -10,15 +10,10 @@ class Toronto(TorontoJurisdiction):
     division_name = 'Toronto'
     name = 'Toronto City Council'
     url = 'http://www.toronto.ca'
-    check_sessions = True
     legislative_sessions = []
 
     def __init__(self):
         super(Toronto, self).__init__()
-        # TODO: Accommodate legacy format pages. (bad old PDF days)
-        # {'identifier': '1998-2000'},
-        # {'identifier': '2000-2003'},
-        # {'identifier': '2003-2006'},
         self.legislative_sessions = [self.legislative_session(session) for session in self.sessions()]
 
     def get_session_list(self):
