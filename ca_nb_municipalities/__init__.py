634a0a1e1038300d96e43e9a9d0bc59f42e8f17f
James McKinney <james@slashpoundbang.com>
2017-02-16 08:22:56 -0500

    Match name in Represent

--- ca_nb_municipalities/__init__.py
+++ ca_nb_municipalities/__init__.py
@@ -5,7 +5,7 @@ class NewBrunswickMunicipalities(CanadianJurisdiction):
     classification = 'executive'  # just to avoid clash
     division_id = 'ocd-division/country:ca/province:nb'
     division_name = 'New Brunswick'
-    name = 'New Brunswick Municipalities'
+    name = 'New Brunswick municipal councils'
     url = 'http://www2.gnb.ca/content/gnb/en/departments/elg/local_government/content/community_profiles.html'
 
     def get_organizations(self):
