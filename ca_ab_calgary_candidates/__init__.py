12914acc1682230cb7e86897875af10eeaf891b2
James McKinney <james@slashpoundbang.com>
2017-08-26 13:54:28 -0400

    ca_ab_calgary_candidates: Fix posts

--- ca_ab_calgary_candidates/__init__.py
+++ ca_ab_calgary_candidates/__init__.py
@@ -1,4 +1,6 @@
 from utils import CanadianJurisdiction
+from opencivicdata.divisions import Division
+from pupa.scrape import Organization
 
 
 class CalgaryCandidates(CanadianJurisdiction):
@@ -7,3 +9,12 @@ class CalgaryCandidates(CanadianJurisdiction):
     division_name = 'Calgary'
     name = 'Calgary City Council'
     url = 'http://www.calgary.ca'
+
+    def get_organizations(self):
+        organization = Organization(self.name, classification=self.classification)
+
+        organization.add_post(role='candidate', label=self.division_name, division_id=self.division_id)
+        for division in Division.get(self.division_id).children('ward'):
+            organization.add_post(role='candidate', label=division.name, division_id=division.id)
+
+        yield organization
