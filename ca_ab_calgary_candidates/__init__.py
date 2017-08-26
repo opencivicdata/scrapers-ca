dc9e99203c2bda31649d2ec748c90be3dac7ae42
James McKinney <james@slashpoundbang.com>
2017-08-26 13:48:25 -0400

    Add ca_ab_calgary_candidates

---
+++ ca_ab_calgary_candidates/__init__.py
@@ -0,0 +1,9 @@
+from utils import CanadianJurisdiction
+
+
+class CalgaryCandidates(CanadianJurisdiction):
+    classification = 'executive'  # just to avoid clash
+    division_id = 'ocd-division/country:ca/csd:4806016'
+    division_name = 'Calgary'
+    name = 'Calgary City Council'
+    url = 'http://www.calgary.ca'
