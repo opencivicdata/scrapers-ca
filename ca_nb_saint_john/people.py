e30a1a0436ec0dbc645f7b0abffd56a7d1905afd
James McKinney <james@slashpoundbang.com>
2018-03-11 13:05:58 -0400

    ca_nb_saint_john: fix

--- ca_nb_saint_john/people.py
+++ ca_nb_saint_john/people.py
@@ -2,7 +2,7 @@ from utils import CSVScraper
 
 
 class SaintJohnPersonScraper(CSVScraper):
-    csv_url = 'http://www.saintjohn.ca/site/media/SaintJohn/Saint_John_NB_Elected_Officials_Contact_Info.zip'
+    csv_url = 'https://opendata.arcgis.com/datasets/256aed00cd13455cac721eb56769c4d4_0.csv'
     filename = 'Saint_John_NB_Elected_Officials_Contact_Info.csv'
     many_posts_per_area = True
     corrections = {
