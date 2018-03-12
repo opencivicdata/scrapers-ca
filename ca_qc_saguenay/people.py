f9ed831c7697a97f6fca804382573376f4349358
James McKinney <james@slashpoundbang.com>
2018-03-11 20:28:17 -0400

    ca_qc_saguenay: fix

--- ca_qc_saguenay/people.py
+++ ca_qc_saguenay/people.py
@@ -1,34 +1,33 @@
 from utils import CanadianScraper, CanadianPerson as Person
 
-COUNCIL_PAGE = 'http://ville.saguenay.ca/fr/administration-municipale/conseils-municipaux-et-darrondissement/membres-des-conseils'
+COUNCIL_PAGE = 'https://ville.saguenay.ca/la-ville-et-vie-democratique/conseils-municipaux-et-darrondissement/membres-des-conseils'
+MAYOR_PAGE = 'https://ville.saguenay.ca'
+CONTACT_PAGE = 'https://ville.saguenay.ca/la-ville-et-vie-democratique/cabinet'
 
 
 class SaguenayPersonScraper(CanadianScraper):
     def scrape(self):
+        mayor_page = self.lxmlize(MAYOR_PAGE)
+        contact_page = self.lxmlize(CONTACT_PAGE)
+
+        name = mayor_page.xpath('//span/text()[contains(., "maire")]')[0].split(', ', 1)[0]
+        p = Person(primary_org='legislature', name=name, district='Saguenay', role='Maire')
+        p.add_source(MAYOR_PAGE)
+        p.add_source(CONTACT_PAGE)
+        node = contact_page.xpath('//h2[contains(., "Coordonnées du cabinet")]/following-sibling::p')[1]
+        p.add_contact('voice', self.get_phone(node, area_codes=[418]), 'legislature')
+        p.add_contact('email', self.get_email(node))
+        yield p
+
         page = self.lxmlize(COUNCIL_PAGE)
-
-        mayor = page.xpath('//div[./div/h3[contains(text(), "Maire")]]/p/text()')
-        m_name = mayor[0].strip().split('.')[1].strip()
-        m_phone = mayor[1].strip().split(':')[1].strip()
-
-        m = Person(primary_org='legislature', name=m_name, district='Saguenay', role='Maire')
-        m.add_source(COUNCIL_PAGE)
-        m.add_contact('voice', m_phone, 'legislature')
-
-        yield m
-
-        councillors = page.xpath('//div[./div/h3[contains(text(), "District")]]')
+        councillors = page.xpath('//div[contains(./h3, "District")]')
         assert len(councillors), 'No councillors found'
         for councillor in councillors:
-            district = councillor.xpath('./div/h3')[0].text_content().replace('#', '')
+            district = councillor.xpath('./h3/text()')[0].replace('#', '')
             name = councillor.xpath('.//p/text()')[0]
-            name = name.replace('M. ', '').replace('Mme ', '').strip()
-            phone = councillor.xpath('.//p/text()')[1].split(':')[1].strip().replace(' ', '-')
-            email = self.get_email(councillor)
 
             p = Person(primary_org='legislature', name=name, district=district, role='Conseiller')
             p.add_source(COUNCIL_PAGE)
-
-            p.add_contact('voice', phone, 'legislature')
-            p.add_contact('email', email)
+            p.add_contact('voice', self.get_phone(councillor), 'legislature')
+            p.add_contact('email', self.get_email(councillor))
             yield p
