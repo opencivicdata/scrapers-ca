8a5a02e1be9fd5dadfaf39304859df8473a37da0
James McKinney <james@slashpoundbang.com>
2018-03-26 00:50:44 -0400

    ca_qc_brossard: fix

--- ca_qc_brossard/people.py
+++ ca_qc_brossard/people.py
@@ -2,7 +2,7 @@ from utils import CanadianScraper, CanadianPerson as Person
 
 import re
 
-COUNCIL_PAGE = 'http://www.ville.brossard.qc.ca/Ma-ville/conseil-municipal.aspx?lang=en-CA'
+COUNCIL_PAGE = 'http://www.ville.brossard.qc.ca/Ma-ville/conseil-municipal/Municipal-council.aspx?lang=en-ca'
 CONTACT_PAGE = 'http://www.ville.brossard.qc.ca/Ma-ville/conseil-municipal/Municipal-council/Municipal-council-members-%E2%80%93-Contact-information.aspx'
 
 
@@ -12,39 +12,37 @@ class BrossardPersonScraper(CanadianScraper):
         contact_page = self.lxmlize(CONTACT_PAGE)
 
         councillors = page.xpath('//a[contains(@class, "slide item-")]')
-        email_links = contact_page.xpath('//a[contains(@href, "mailto:")]')
+        emails = contact_page.xpath('//a[contains(@href, "mailto:")]')
 
         assert len(councillors), 'No councillors found'
-        for elem in councillors:
-            name = elem.xpath('.//div[@class="titre"]/text()')[0]
+        for councillor in councillors:
+            name = councillor.xpath('.//div[@class="titre"]/text()')[0]
             if name == 'Poste vacant':
                 continue
-            position = elem.xpath('.//div[@class="poste"]/text()')[0]
+            if name == 'Sylvie Desgroseilliers':
+                name = 'Sylvie DesGroseilliers'
+
+            position = councillor.xpath('.//div[@class="poste"]/text()')[0]
             role = 'Conseiller'
+
             district = re.search(r'District \d+', position)
-            if district:
-                district = district.group(0)
-            elif 'Mayor' in position:
+            if 'Mayor' in position:
                 district = 'Brossard'
                 role = 'Maire'
+            else:
+                district = district.group(0)
 
-            photo = re.search(r'url\((.+)\)', elem.attrib['style']).group(1)
+            photo = re.search(r'url\((.+)\)', councillor.attrib['style']).group(1)
 
             p = Person(primary_org='legislature', name=name, district=district, role=role, image=photo)
             p.add_source(COUNCIL_PAGE)
             p.add_source(CONTACT_PAGE)
 
-            email_elem = [link for link in email_links
-                          if name in link.text_content().replace('\u2019', "'")][0]
-            email = re.match('mailto:(.+@brossard.ca)', email_elem.attrib['href']).group(1)
-            p.add_contact('email', email)
-            phone = email_elem.xpath('./following-sibling::text()[contains(., "450")]')
-            if len(phone) == 0:
-                # one field is misordered.
-                assert name == "Francine Raymond"
-                phone = "450 923-6304, poste 6503"
-            else:
-                phone = phone[0]
+            index = [i for i, link in enumerate(emails) if name in link.text_content().replace('\u2019', "'")][0]
+            email = emails[index + 1]
+            p.add_contact('email', re.match('mailto:(.+@brossard.ca)', email.attrib['href']).group(1))
+            phone = email.xpath('./preceding-sibling::text()[contains(., "450")]')
+            phone = phone[-1]
             p.add_contact('voice', phone, 'legislature')
 
             yield p
