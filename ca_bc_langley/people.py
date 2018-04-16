70e3d4ff190ba4f2e3b4cc8b6f054637a1f3d064
James McKinney <james@slashpoundbang.com>
2018-04-16 13:50:54 -0400

    ca_bc_langley: Fix

--- ca_bc_langley/people.py
+++ ca_bc_langley/people.py
@@ -2,5 +2,5 @@ from utils import CSVScraper
 
 
 class LangleyPersonScraper(CSVScraper):
-    csv_url = 'https://data.tol.ca/api/views/ykn8-vbpf/rows.csv?accessType=DOWNLOAD'
+    csv_url = 'https://opendata.arcgis.com/datasets/daa3ea0f01d24e9b80d837cf2178eb71_0.csv'
     many_posts_per_area = True
