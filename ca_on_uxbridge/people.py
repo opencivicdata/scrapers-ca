de6098568f30e5c72e88f31e6be74dacb18b9c8f
James McKinney <james@slashpoundbang.com>
2018-04-16 13:19:52 -0400

    ca_on_uxbridge: update updated_at

--- ca_on_uxbridge/people.py
+++ ca_on_uxbridge/people.py
@@ -7,5 +7,5 @@ class UxbridgePersonScraper(CSVScraper):
     csv_url = 'https://docs.google.com/spreadsheets/d/1NIW_hM8gm0AlbTvWGBXaBU9DTeom74z5ZhrA10Z94G4/pub?gid=171849574&single=true&output=csv'
     many_posts_per_area = True
     unique_roles = ('Mayor', 'Regional Councillor')
-    updated_at = date(2016, 11, 23)
+    updated_at = date(2018, 4, 16)
     contact_person = 'joe.murray@jmaconsulting.biz'
