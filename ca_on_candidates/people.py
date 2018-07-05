98adeafa8b48711b2280668fa34edd35e06b599f
James McKinney <james@slashpoundbang.com>
2018-07-05 18:34:00 -0400

    ca_on_candidates: Restore

---
+++ ca_on_candidates/people.py
@@ -0,0 +1,18 @@
+from utils import CSVScraper
+
+from datetime import date
+
+
+class OntarioCandidatesPersonScraper(CSVScraper):
+    csv_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQhrWSeOEC9DaNN2iDKcPC9IH701Al0pELevzSO62maI9WXt1TGvFH2fzUkXjUfujc3ontePcroFbT2/pub?gid=881365071&single=true&output=csv'
+    encoding = 'utf-8'
+    updated_at = date(2018, 1, 31)
+    contact_person = 'andrew@newmode.net'
+    corrections = {
+        'district name': {
+            'Brantford-Brant': 'Brantford\u2014Brant',
+        }
+    }
+
+    def is_valid_row(self, row):
+        return any(row.values()) and row['last name'] and row['first name']
