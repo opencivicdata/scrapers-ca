20b1d04170bedbeb84878e7ac9bbeb4898949e83
James McKinney <james@slashpoundbang.com>
2018-08-04 14:29:54 -0400

    Fix default delimiter

--- utils.py
+++ utils.py
@@ -230,7 +230,7 @@ class CSVScraper(CanadianScraper):
     """
     Set the CSV file's delimiter.
     """
-    delimiter = None
+    delimiter = ','
     """
     Set the CSV file's encoding, like 'windows-1252' ('utf-8' by default).
     """
