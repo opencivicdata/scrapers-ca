a1d6e0f7eebb3bbe006d994199e92ce993cb61de
James McKinney <james@slashpoundbang.com>
2018-08-04 14:30:03 -0400

    ca_on_guelph: Fix CSV URL

--- ca_on_guelph/people.py
+++ ca_on_guelph/people.py
@@ -2,5 +2,5 @@ from utils import CSVScraper
 
 
 class GuelphPersonScraper(CSVScraper):
-    csv_url = 'http://open.guelph.ca/wp-content/uploads/2015/01/GuelphCityCouncil2014-2018ElectedOfficalsContactInformation1.csv'
+    csv_url = 'http://data.open.guelph.ca/datafiles/guelph-mayor-and-councillors-contact-information-2014-2018.csv'
     many_posts_per_area = True
