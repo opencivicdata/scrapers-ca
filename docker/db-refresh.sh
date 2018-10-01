19c5a29099b3751ed65468739716589fed9aac42
Andrew Clement <djac@djac.ca>
2018-10-01 16:32:53 -0400

    Move Docker related shell scripts to the README as simple text

--- docker/db-refresh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker-compose exec scrapers-ca sudo -u postgres dropdb pupa
-docker-compose exec scrapers-ca sudo -u postgres createdb pupa
--- docker/shell.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Open docker shell.
-
-echo 'Opening docker shell. Use exit command to return.'
-docker-compose exec scrapers-ca /bin/bash
