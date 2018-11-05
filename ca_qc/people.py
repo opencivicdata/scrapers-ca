fdf8b77403f71d624eab817b2657f173f87c1241
James McKinney <james@slashpoundbang.com>
2018-11-05 08:18:36 -0500

    ca_qc: fix

--- ca_qc/people.py
+++ ca_qc/people.py
@@ -12,27 +12,35 @@ class QuebecPersonScraper(CanadianScraper):
         assert len(members), 'No members found'
         for row in members:
             name_comma, division = [cell.text_content() for cell in row[:2]]
+
             name = ' '.join(reversed(name_comma.strip().split(',')))
+
+            division = division.replace('–', '-')  # n-dash, hyphen
+
             party = row[2].text_content().strip()
-            email = self.get_email(row[3], error=False)
-            detail_url = row[0][0].attrib['href']
-            detail_page = self.lxmlize(detail_url)
-            photo_url = detail_page.xpath('//img[@class="photoDepute"]/@src')
-            division = division.replace('–', '—')  # n-dash, m-dash
             if party == 'Indépendante':
                 party = 'Indépendant'
 
-            p = Person(primary_org='legislature', name=name, district=division, role='MNA',
-                       party=party)
+            email = self.get_email(row[3], error=False)
+
+            detail_url = row[0][0].attrib['href']
+            detail_page = self.lxmlize(detail_url)
+
+            contact_url = detail_url.replace('index.html', 'coordonnees.html')
+            contact_page = self.lxmlize(contact_url)
+
+            photo_url = detail_page.xpath('//img[@class="photoDepute"]/@src')
+
+            p = Person(primary_org='legislature', name=name, district=division, role='MNA', party=party)
             p.add_source(COUNCIL_PAGE)
             p.add_source(detail_url)
+            p.add_source(contact_url, note='For telephone number(s)')
+
             if photo_url:
                 p.image = photo_url[0]
             if email:
                 p.add_contact('email', email)
-            contact_url = detail_url.replace('index.html', 'coordonnees.html')
-            contact_page = self.lxmlize(contact_url)
-            p.add_source(contact_url, note='For telephone number(s)')
+
             for div in contact_page.xpath('//div[@class="blockAdresseDepute"]'):
                 try:
                     phone = self.get_phone(div)
