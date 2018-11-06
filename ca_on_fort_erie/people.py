8ef1421ac3fa31f529157aedf7c90f06809dab99
James McKinney <james@slashpoundbang.com>
2018-11-06 13:24:05 -0500

    ca_on_fort_erie: add

---
+++ ca_on_fort_erie/people.py
@@ -0,0 +1,6 @@
+from utils import CSVScraper
+
+
+class FortEriePersonScraper(CSVScraper):
+    # https://niagaraopendata.ca/dataset/town-of-fort-erie-elected-officials
+    csv_url = 'https://niagaraopendata.ca/dataset/744965a4-ed48-4ca9-a838-48d07c76a36b/resource/731205b6-bc59-4e08-90bf-80705033e4d4/download/fort-erie-elected-officials.csv'
