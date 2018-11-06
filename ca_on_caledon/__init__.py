8a5f3ef185175d1f75281b221aa7877b434892a6
James McKinney <james@slashpoundbang.com>
2018-11-06 16:04:24 -0500

    ca_on_caledon: re-enable

--- ca_on_caledon/__init__.py
+++ ca_on_caledon/__init__.py
@@ -7,14 +7,14 @@ class Caledon(CanadianJurisdiction):
     division_id = 'ocd-division/country:ca/csd:3521024'
     division_name = 'Caledon'
     name = 'Caledon Town Council'
-    url = 'http://www.town.caledon.on.ca'
+    url = 'https://www.caledon.ca'
 
     def get_organizations(self):
         organization = Organization(self.name, classification=self.classification)
 
         organization.add_post(role='Mayor', label=self.division_name, division_id=self.division_id)
         for ward_name in ('Ward 1', 'Ward 2', 'Wards 3 and 4', 'Ward 5'):
-            for seat_number in range(1, 3):
-                organization.add_post(role='Councillor', label='{} (seat {})'.format(ward_name, seat_number), division_id=self.division_id)
+            organization.add_post(role='Regional Councillor', label=ward_name, division_id=self.division_id)
+            organization.add_post(role='Area Councillor', label=ward_name, division_id=self.division_id)
 
         yield organization
--- ca_on_caledon/people.py
+++ ca_on_caledon/people.py
@@ -1,10 +1,51 @@
-from utils import CSVScraper
+from utils import CanadianScraper, CanadianPerson as Person
 
-from datetime import date
+import re
+
+COUNCIL_PAGE = 'https://www.caledon.ca/en/townhall/council.asp'
 
 
-class CaledonPersonScraper(CSVScraper):
-    csv_url = 'https://docs.google.com/spreadsheets/d/1wf91UJK7dluBFHV3v7ubINzL4lCxnoaFqRcYHdFv9oo/pub?gid=1255809422&single=true&output=csv'
-    updated_at = date(2016, 11, 8)
-    contact_person = 'andrew@newmode.net, shamus@newmode.net'
-    many_posts_per_area = True
+class CaledonPersonScraper(CanadianScraper):
+    def scrape(self):
+        page = self.lxmlize(COUNCIL_PAGE)
+
+        node = page.xpath('//td[@rowspan="2"]')[0]
+        name = node.xpath('.//h3/strong/text()')[0]
+        image = node.xpath('.//@src')[0]
+        voice = self.get_phone(node)
+        url = node.xpath('.//a[contains(., "Visit")]/@href')[0]
+
+        p = Person(primary_org='legislature', name=name, district='Caledon', role='Mayor')
+        p.add_source(COUNCIL_PAGE)
+        p.add_source(url)
+
+        p.image = image
+        p.add_contact('voice', voice, 'legislature')
+        p.add_contact('email', self.get_email(self.lxmlize(url)))
+
+        yield p
+
+        councillors = page.xpath('//div[@id="printAreaContent"]//table[2]//td')
+        councillors = councillors[:12] + councillors[16:]
+        assert len(councillors), 'No councillors found'
+        for i in range(len(councillors) // 3):
+            i = i // 4 * 12 + i % 4
+            district, role = councillors[i].xpath('.//h3/text()')
+            image = councillors[i + 4].xpath('.//@src')[0]
+            name = councillors[i + 8].xpath('.//strong/text()')[0]
+            voice = self.get_phone(councillors[i + 8])
+            url = councillors[i + 8].xpath('.//a[contains(., "Visit")]/@href')[0]
+
+            district = district.replace('\xa0', ' ')
+            if ' and ' in district:
+                district = district.replace('Ward ', 'Wards ')
+
+            p = Person(primary_org='legislature', name=name, district=district, role=role)
+            p.add_source(COUNCIL_PAGE)
+            p.add_source(url)
+
+            p.image = image
+            p.add_contact('voice', voice, 'legislature')
+            p.add_contact('email', self.get_email(self.lxmlize(url)))
+
+            yield p
