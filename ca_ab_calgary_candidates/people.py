bd270c70c7a4dbe2937d97565c45eefcf4c20b1c
James McKinney <james@slashpoundbang.com>
2018-11-06 09:28:28 -0500

    Delete old elections

--- ca_ab_calgary_candidates/people.py
+++
@@ -1,9 +0,0 @@
-from utils import CSVScraper
-
-from datetime import date
-
-
-class CalgaryCandidatesPersonScraper(CSVScraper):
-    csv_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQhrWSeOEC9DaNN2iDKcPC9IH701Al0pELevzSO62maI9WXt1TGvFH2fzUkXjUfujc3ontePcroFbT2/pub?gid=757164835&single=true&output=csv'
-    updated_at = date(2017, 8, 26)
-    contact_person = 'shamus@newmode.net'
