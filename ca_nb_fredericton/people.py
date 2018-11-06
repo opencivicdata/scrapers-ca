7e0f366936cecd37891d6c8b1956c8a82ca049e3
James McKinney <james@slashpoundbang.com>
2018-11-06 14:23:36 -0500

    ca_nb_fredericton: fix

--- ca_nb_fredericton/people.py
+++ ca_nb_fredericton/people.py
@@ -10,23 +10,30 @@ class FrederictonPersonScraper(CanadianScraper):
         councillors = page.xpath('//div[contains(@class, "view-people")]//div[contains(@class, "views-row")]')
         assert len(councillors), 'No councillors found'
         for councillor in councillors:
-            name = councillor.xpath('.//div[@property="dc:title"]')[0].text_content()
-            role_and_district = councillor.xpath('.//div[contains(@class, "field-name-field-sub-title")]//p')[-2].text_content().replace('\xa0', ' ')
+            name = councillor.xpath('.//div[@property="dc:title"]//text()')[0]
+            paras = councillor.xpath('.//div[contains(@class, "field-name-field-sub-title")]//p/text()')
 
-            if role_and_district == 'Mayor':
+            if paras[0] == 'Mayor':
                 district = 'Fredericton'
                 role = 'Mayor'
             else:
-                district = role_and_district.split(', ', 1)[1]
+                if paras[0] == 'Deputy Mayor':
+                    index = 1
+                else:
+                    index = 0
+
+                district = paras[index].replace('\xa0', ' ').split(', ', 1)[1]
                 role = 'Councillor'
 
             url = councillor.xpath('.//@href')[0]
             page = self.lxmlize(url)
 
             p = Person(primary_org='legislature', name=name, district=district, role=role)
+            p.add_source(COUNCIL_PAGE)
+            p.add_source(url)
+
             p.image = councillor.xpath('.//img[@typeof="foaf:Image"]/@src')[0]
             p.add_contact('email', self.get_email(page))
             p.add_contact('voice', self.get_phone(page, area_codes=[506]), 'legislature')
-            p.add_source(COUNCIL_PAGE)
-            p.add_source(url)
+
             yield p
