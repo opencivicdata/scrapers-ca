ceac560b747b94a37ff2aac44934b9a0dc00fada
James McKinney <james@slashpoundbang.com>
2018-11-06 16:09:47 -0500

    ca_qc_sainte_anne_de_bellevue: re-enable

---
+++ ca_qc_sainte_anne_de_bellevue/people.py
@@ -0,0 +1,30 @@
+from utils import CanadianScraper, CanadianPerson as Person
+
+import re
+
+COUNCIL_PAGE = 'https://www.ville.sainte-anne-de-bellevue.qc.ca/fr/199/elus-municipaux'
+
+
+class SainteAnneDeBellevuePersonScraper(CanadianScraper):
+    def scrape(self):
+        page = self.lxmlize(COUNCIL_PAGE)
+
+        councillors = page.xpath('//div[@class="block text"]')
+        assert len(councillors), 'No councillors found'
+        for i, councillor in enumerate(councillors):
+            name = councillor.xpath('.//div[@class="content-writable"]/p/text()')[0]
+            district = councillor.xpath('.//h2/text()')[0]
+
+            if 'Maire' in district:
+                district = 'Sainte-Anne-de-Bellevue'
+                role = 'Maire'
+            else:
+                district = 'District {}'.format(re.search(r'\d+', district)[0])
+                role = 'Conseiller'
+
+            p = Person(primary_org='legislature', name=name, district=district, role=role)
+            p.add_source(COUNCIL_PAGE)
+
+            p.image = councillor.xpath('.//@src')[0]
+            p.add_contact('email', self.get_email(councillor))
+            yield p
