6f8a6cd535eebb5c80adb36f9098e4df2673fbfd
James McKinney <james@slashpoundbang.com>
2018-11-06 15:21:25 -0500

    ca_on_newmarket: add comment

--- ca_on_newmarket/people.py
+++ ca_on_newmarket/people.py
@@ -2,7 +2,7 @@ from utils import CSVScraper
 
 
 class NewmarketPersonScraper(CSVScraper):
-    # There's no permalink to the ZIP or CSV, so we must manually upload a copy to S3.
+    # There's no permalink to the ZIP or CSV, so we must manually upload a copy to S3. Last retrieved: 2018-11-06.
     # http://open.newmarket.ca/opendata/navigo/#/show/SQLQ6R6C_12?disp=D156E60C6F85
     csv_url = 'http://represent.opennorth.ca.s3.amazonaws.com/data/2016-11-15-newmarket.csv'
     corrections = {
