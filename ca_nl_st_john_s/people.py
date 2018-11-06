fc048f9ff7655f37bf6f1967ca08d749f4944c05
James McKinney <james@slashpoundbang.com>
2018-11-06 10:37:53 -0500

    ca_nl_st_john_s: fix

--- ca_nl_st_john_s/people.py
+++ ca_nl_st_john_s/people.py
@@ -1,5 +1,7 @@
 from utils import CanadianScraper, CanadianPerson as Person
 
+import re
+
 COUNCIL_PAGE = 'http://www.stjohns.ca/city-hall/about-city-hall/council'
 
 
@@ -12,23 +14,28 @@ class StJohnsPersonScraper(CanadianScraper):
         assert len(councillors), 'No councillors found'
         for node in councillors:
             fields = node.xpath('./div')
-            role = fields[0].xpath('./div//text()')[0]
-            name = fields[2].xpath('.//a//text()')[0].title().split(role)[-1].strip()
+            district_or_role = fields[0].xpath('./div//text()')[0]
+            if 'At Large' in district_or_role:
+                district_or_role = re.sub(r' \d', '', district_or_role)
+
+            name = fields[2].xpath('.//a//text()')[0].title().split(district_or_role)[-1].strip()
             if name == 'Vacant':
                 continue
 
-            if 'Ward' in role:
-                district = role
+            if 'Ward' in district_or_role:
+                district = district_or_role
                 role = 'Councillor'
             else:
-                if 'At Large' in role:
-                    role = 'Councillor at Large'
+                if 'At Large' in district_or_role:
                     district = "St. John's (seat {})".format(councillor_seat_number)
+                    role = 'Councillor at Large'
                     councillor_seat_number += 1
                 else:
                     district = "St. John's"
+                    role = district_or_role
+
             phone = fields[3].xpath('./div//text()')[0]
-            email = self.get_email(fields[5])
+            email = self.get_email(fields[4])
             photo_url = node.xpath('.//img/@src')[0]
 
             p = Person(primary_org='legislature', name=name, district=district, role=role)
