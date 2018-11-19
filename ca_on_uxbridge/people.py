66ac62d5cf2401f799dc589a2ff3982b02894171
James McKinney <james@slashpoundbang.com>
2018-11-19 15:33:19 -0500

    Update Uxbridge date

--- ca_on_uxbridge/people.py
+++ ca_on_uxbridge/people.py
@@ -5,5 +5,5 @@ from datetime import date
 
 class UxbridgePersonScraper(CSVScraper):
     csv_url = 'https://docs.google.com/spreadsheets/d/1NIW_hM8gm0AlbTvWGBXaBU9DTeom74z5ZhrA10Z94G4/pub?gid=171849574&single=true&output=csv'
-    updated_at = date(2018, 4, 16)
+    updated_at = date(2018, 11, 13)
     contact_person = 'joe.murray@jmaconsulting.biz'
