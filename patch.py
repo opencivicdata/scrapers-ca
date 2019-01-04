6660024450e3b01754f174c96e7a98bddc1805fa
James McKinney <james@slashpoundbang.com>
2019-01-03 21:37:10 -0500

    ca_qc: fix

--- patch.py
+++ patch.py
@@ -125,7 +125,7 @@ name_fragment = r'(?:'           \
     r"(?:D'|d'|De|de|Des|Di|Du|L'|La|Le|Mac|Mc|O'|San|Van|Vander?|vanden)?\p{Lu}\p{Ll}+|" \
     r'\p{Lu}\p{Ll}+Anne?|Marie\p{Lu}\p{Ll}+|' \
     r"Ch'ng|Prud'homme|"         \
-    r"D!ONNE"                    \
+    r"D!ONNE|IsaBelle"           \
     r')'
 
 # Name components can be joined by apostrophes, hyphens or spaces.
