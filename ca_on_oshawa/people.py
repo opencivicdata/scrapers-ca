c5d1b6b4eaf2ee824659be6839ca23a6df9b655b
James McKinney <james@slashpoundbang.com>
2019-01-03 23:39:24 -0500

    ca_on_oshawa: fix

--- ca_on_oshawa/people.py
+++ ca_on_oshawa/people.py
@@ -1,35 +1,25 @@
 from utils import CanadianScraper, CanadianPerson as Person
 
-COUNCIL_PAGE = 'http://www.oshawa.ca/city-hall/city-council-members.asp'
+COUNCIL_PAGE = 'https://www.oshawa.ca/city-hall/city-council-members.asp'
 
 
 class OshawaPersonScraper(CanadianScraper):
     def scrape(self):
-        councillor_seat_number = 1
-        regional_councillor_seat_number = 1
-
         page = self.lxmlize(COUNCIL_PAGE)
         councillors = page.xpath('//table//td[*]')
 
         assert len(councillors), 'No councillors found'
         for councillor in councillors:
-            if councillor.xpath('./p[1]/text()'):
-                name, role = councillor.xpath('./p[1]/text()')
-            else:
-                name, role = councillor.xpath('./span[1]/text()')
-
+            district, role, name = councillor.xpath('./p[1]/text()')
             role = role.strip()
+
+            if district == 'City of Oshawa':
+                district = 'Oshawa'
 
             if role == 'City Councillor':
                 role = 'Councillor'
-                district = 'Oshawa (seat {})'.format(councillor_seat_number)
-                councillor_seat_number += 1
-            elif role == 'Regional and City Councillor':
+            elif role == 'Regional & City Councillor':
                 role = 'Regional Councillor'
-                district = 'Oshawa (seat {})'.format(regional_councillor_seat_number)
-                regional_councillor_seat_number += 1
-            else:
-                district = 'Oshawa'
 
             photo_url = councillor.xpath('./p/img/@src')[0]
             phone = self.get_phone(councillor.xpath('./p[contains(.//text(), "Phone")]')[0], area_codes=[905])
