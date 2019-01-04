70f4122361a54bf00d668f85aa2f37465d4d818a
James McKinney <james@slashpoundbang.com>
2019-01-03 23:31:52 -0500

    ca_on_belleville: fix

--- ca_on_belleville/people.py
+++ ca_on_belleville/people.py
@@ -12,41 +12,41 @@ class BellevillePersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
 
-        mayor_node = page.xpath('//div[@class="content-field"]/h3[contains(./text(), "Mayor")]/following-sibling::p[2]')[0]
-        yield self.scrape_mayor(mayor_node)
-
-        councillors = page.xpath('//h3[contains(text(), "Councillors")]')
-        assert len(councillors), 'No councillors found'
-        for ward_elem in councillors:
-            ward = re.search(r'(Ward.+) Councillors', ward_elem.text).group(1)
-            councillor_name_elems = ward_elem.xpath('./following-sibling::div[1]//strong')
-            for name_elem in councillor_name_elems:
-                self.seat_numbers[ward] += 1
-                district = '{} (seat {})'.format(ward, self.seat_numbers[ward])
-                yield self.person_from_elem(name_elem, district, 'Councillor')
-
-    def person_from_elem(self, name_elem, district, role):
-        name = name_elem.text_content()
-        phone = name_elem.xpath('./following-sibling::text()[2]')[0].split(': ')[1]
-        email = name_elem.xpath('./following-sibling::a//text()')[0]
-        photo_url = name_elem.xpath('./preceding::img[1]/@src')[0]
-
-        p = Person(primary_org='legislature', name=name, district=district, role=role, image=photo_url)
-        p.add_source(COUNCIL_PAGE)
-        p.add_contact('voice', phone, 'legislature')
-        p.add_contact('email', email)
-        return p
-
-    def scrape_mayor(self, node):
+        node = page.xpath('//div[@class="content-field"]/h3[contains(./text(), "Mayor")]/following-sibling::p[2]')[0]
         name = node.xpath('./strong/text()')[0]
         phone = node.xpath('./text()')[2].split(': ')[1]
         fax = node.xpath('./text()')[3].split(': ')[1]
         email = node.xpath('./a/text()')[0]
-        photo_url = node.xpath('./preceding::p//img/@src')[0]
+        image = node.xpath('./preceding::p//img/@src')[0]
 
-        p = Person(primary_org='legislature', name=name, district='Belleville', role='Mayor', image=photo_url)
+        p = Person(primary_org='legislature', name=name, district='Belleville', role='Mayor')
         p.add_source(COUNCIL_PAGE)
         p.add_contact('voice', phone, 'legislature')
         p.add_contact('fax', fax, 'legislature')
         p.add_contact('email', email)
-        return p
+        p.image = image
+
+        yield p
+
+        wards = page.xpath('//h3[contains(text(), "Councillors")]')
+        assert len(wards), 'No councillors found'
+        for ward in wards:
+            ward_name = re.search(r'(Ward.+) Councillors', ward.text).group(1)
+            councillors = ward.xpath('./following-sibling::div[1]//strong')
+            for councillor in councillors:
+                self.seat_numbers[ward_name] += 1
+                district = '{} (seat {})'.format(ward_name, self.seat_numbers[ward_name])
+                role = 'Councillor'
+
+                name = councillor.text_content()
+                phone = councillor.xpath('./following-sibling::text()[2]')[0].split(':')[1]
+                email = councillor.xpath('./following-sibling::a//text()')[0]
+                image = councillor.xpath('./preceding::img[1]/@src')[0]
+
+                p = Person(primary_org='legislature', name=name, district=district, role=role)
+                p.add_source(COUNCIL_PAGE)
+                p.add_contact('voice', phone, 'legislature')
+                p.add_contact('email', email)
+                p.image = image
+
+                yield p
