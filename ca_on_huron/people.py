81064a5cb98e547abfcfae54278c8c2fda6cdb2b
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-03-19 16:45:41 -0400

    Add ca_on_huron

---
+++ ca_on_huron/people.py
@@ -0,0 +1,13 @@
+from utils import CSVScraper
+
+
+class HuronPersonScraper(CSVScraper):
+    # https://data-huron.opendata.arcgis.com/datasets/051e72a02edc4337af8ca2606ab58644_0
+    csv_url = 'https://opendata.arcgis.com/datasets/051e72a02edc4337af8ca2606ab58644_0.csv'
+    many_posts_per_area = True
+    corrections = {
+        'primary role': {
+            'Councilor': 'Councillor',
+        },
+        'phone': lambda value: value.replace(', ', ';')
+    }
