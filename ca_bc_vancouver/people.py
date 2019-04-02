ad6715e53aa76b0d6a29cbfa146afc739fe739ff
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-04-02 11:09:31 -0400

    ca_bc_vancouver: add correction for "N/A"

--- ca_bc_vancouver/people.py
+++ ca_bc_vancouver/people.py
@@ -5,3 +5,8 @@ class VancouverPersonScraper(CSVScraper):
     # https://data.vancouver.ca/datacatalogue/councilContactInfo.htm
     csv_url = 'ftp://webftp.vancouver.ca/OpenData/csv/ElectedOfficialsContactInformation.csv'
     many_posts_per_area = True
+    corrections = {
+        'fax': {
+            'N/A': None,
+        },
+    }
