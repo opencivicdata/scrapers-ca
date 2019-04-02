06ecc169142817e5ff5ea87db6524aa9ef7f1af1
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-04-02 11:00:57 -0400

    Fix DATABASE_URL for local development

--- pupa_settings.py
+++ pupa_settings.py
@@ -3,7 +3,7 @@ import os
 # Use the same directories as scrapers_ca_app.
 CACHE_DIR = os.path.join(os.getcwd(), '..', '_cache')
 SCRAPED_DATA_DIR = os.path.join(os.getcwd(), '..', '_data')
-DATABASE_URL = os.environ.get('DATABASE_URL', 'postgis://root:root@localhost/pupa')
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgis://localhost/pupa')
 os.environ['OCD_DIVISION_CSV'] = os.environ.get('OCD_DIVISION_CSV', os.path.join(os.path.abspath(os.path.dirname(__file__)), 'country-{}.csv'))
 
 # @see https://github.com/opencivicdata/pupa/blob/master/pupa/settings.py
