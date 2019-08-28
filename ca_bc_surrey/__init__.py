cc93b468049a658baa1ddb8206bf03a026f9281b
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-08-28 16:34:25 -0400

    Restore Surrey URL

--- ca_bc_surrey/__init__.py
+++ ca_bc_surrey/__init__.py
@@ -5,4 +5,4 @@ class Surrey(CanadianJurisdiction):
     division_id = 'ocd-division/country:ca/csd:5915004'
     division_name = 'Surrey'
     name = 'Surrey City Council'
-    url = 'http://www.surrey.ca/city-government/2999.aspx'
+    url = 'http://www.surrey.ca'
