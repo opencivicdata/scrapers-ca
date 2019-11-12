bceb7114a393a3f45377d145b7808abb39fb8c56
Yi Yang <yi@newmode.net>
2019-11-12 13:32:09 -0800

    ET-3857: fixed most of the Members of Parliment data, other than constituencyoffices contact

--- ca/people.py
+++ ca/people.py
@@ -1,9 +1,10 @@
 # coding: utf-8
 from utils import CanadianScraper, CanadianPerson as Person
+#from pprint import pprint
 
 import hashlib
 
-COUNCIL_PAGE = 'http://www.parl.gc.ca/Parliamentarians/en/members?view=ListAll'
+COUNCIL_PAGE = 'https://www.ourcommons.ca/Members/en/search?view=ListAll'
 IMAGE_PLACEHOLDER_SHA1 = ['e4060a9eeaf3b4f54e6c16f5fb8bf2c26962e15d']
 
 
@@ -17,34 +18,41 @@ class CanadaPersonScraper(CanadianScraper):
 
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
-        rows = page.xpath('//div[@class="content-primary"]//tr')[1:]
+        rows = page.xpath('//div[@class="ce-mip-mp-tile-container"]')[1:]
         assert len(rows), 'No members found'
         for row in rows:
-            name_cell = row.xpath('./td[1]')[0]
-            last_name = name_cell.xpath('.//span[1]//text()')[0]
-            first_name = name_cell.xpath('.//span[2]//text()')[0]
-            name = '{} {}'.format(first_name, last_name)
-            constituency = row.xpath('./td[2]//text()')[0].replace('–', '—')  # n-dash, m-dash
+            name = row.xpath('.//div[@class="ce-mip-mp-name"][1]')[0].text_content()
+            #pprint(name)
+            constituency = row.xpath('.//div[@class="ce-mip-mp-constituency"][1]')[0].text_content()
+            constituency = constituency.replace('–', '—')  # n-dash, m-dash
             if constituency == 'Mont-Royal':
                 constituency = 'Mount Royal'
-            province = row.xpath('./td[3]//text()')[0]
-            party = row.xpath('string(./td[4])')  # allow string()
-            url = name_cell.xpath('.//a/@href')[0]
+
+            province = row.xpath('.//div[@class="ce-mip-mp-province"][1]')[0].text_content()
+
+            party = row.xpath('.//div[@class="ce-mip-mp-party"][1]')[0].text_content()
+
+            url = row.xpath('.//a[@class="ce-mip-mp-tile"]/@href')[0]
+            #pprint(url)
             if province == 'Québec':
                 url = url.replace('/en/', '/fr/')
 
             mp_page = self.lxmlize(url)
-            email = self.get_email(mp_page, '//span[@class="caucus"]', error=False)
-            photo = mp_page.xpath('//div[@class="profile overview header"]//img/@src')[0]
+            email = self.get_email(mp_page, '//*[@id="contact"]/div/p/a', error=False)
+            #pprint(email)
+
+            photo = mp_page.xpath('.//div[@class="ce-mip-mp-profile-container"]//img/@src')[0]
+            #pprint(photo)
 
             m = Person(primary_org='lower', name=name, district=constituency, role='MP', party=party)
             m.add_source(COUNCIL_PAGE)
             m.add_source(url)
-            # @see http://www.parl.gc.ca/Parliamentarians/en/members/David-Yurdiga%2886260%29
+            # @see https://www.ourcommons.ca/Members/en/ziad-aboultaif(89156)
             if email:
                 m.add_contact('email', email)
-            elif name == 'Adam Vaughan':
-                m.add_contact('email', 'Adam.Vaughan@parl.gc.ca')
+            # Adam Vaughan's email has been fixed on the new site.
+            # elif name == 'Adam Vaughan':
+            #     m.add_contact('email', 'Adam.Vaughan@parl.gc.ca')
 
             if photo:
                 # Determine whether the photo is actually a generic silhouette
@@ -52,11 +60,13 @@ class CanadaPersonScraper(CanadianScraper):
                 if (photo_response.status_code == 200 and hashlib.sha1(photo_response.content).hexdigest() not in IMAGE_PLACEHOLDER_SHA1):
                     m.image = photo
 
-            personal_url = mp_page.xpath('//a[contains(@title, "Personal Web Site")]/@href')
+            # I don't think the new parliment website has personal website anymore
+            personal_url = mp_page.xpath('.//a[contains(@title, "Personal Web Site")]/@href')
             if personal_url:
                 m.add_link(personal_url[0])
+                #pprint(personal_url[0])
 
-            preferred_languages = mp_page.xpath('//span[@class="label"][contains(., "Preferred Language")]/following-sibling::span[@class="constituency"]/text()')
+            preferred_languages = mp_page.xpath('.//dt[contains(., "Preferred Language")]/following-sibling::dd/text()')
             if preferred_languages:
                 m.extras['preferred_languages'] = [language.replace('/', '').strip() for language in preferred_languages]
 
@@ -64,33 +74,46 @@ class CanadaPersonScraper(CanadianScraper):
                 m.add_contact('address', 'Chambre des communes\nOttawa ON  K1A 0A6', 'legislature')
             else:
                 m.add_contact('address', 'House of Commons\nOttawa ON  K1A 0A6', 'legislature')
-            voice = mp_page.xpath('//div[@class="hilloffice"]//span//text()[contains(., "Telephone:")]|//div[@class="hilloffice"]//span//text()[contains(., "Téléphone :")]')[0].replace('Telephone: ', '').replace('Téléphone : ', '')
-            if voice:
-                m.add_contact('voice', voice, 'legislature')
-            fax = mp_page.xpath('//div[@class="hilloffice"]//span//text()[contains(., "Fax:")]|//div[@class="hilloffice"]//span//text()[contains(., "Télécopieur :")]')[0].replace('Fax: ', '').replace('Télécopieur : ', '')
-            if fax:
-                m.add_contact('fax', fax, 'legislature')
 
-            for i, li in enumerate(mp_page.xpath('//div[@class="constituencyoffices"]//li')):
-                spans = li.xpath('./span[not(@class="spacer")]')
-                note = 'constituency'
-                if i:
-                    note += ' ({})'.format(i + 1)
-                m.add_contact('address', '\n'.join([
-                    spans[0].text_content(),  # address line 1
-                    spans[1].text_content(),  # address line 2
-                    spans[2].text_content(),  # city, region
-                    spans[3].text_content(),  # postal code
-                ]), note)
-                voice = li.xpath('./span//text()[contains(., "Telephone:")]|./span//text()[contains(., "Téléphone :")]')
+            # Now phone and fax are in the same element
+            # <p>
+            #   Telephone: xxx-xxx-xxxx<br/>
+            #   Fax: xxx-xxx-xxx
+            # </p>
+            phone_and_fax_el = mp_page.xpath('.//h4[contains(., "Hill Office")]/../p[contains(., "Telephone")]|.//h4[contains(., "Hill Office")]/../p[contains(., "Téléphone :")]')
+            if len(phone_and_fax_el):
+                phone_and_fax = phone_and_fax_el[0].text_content().strip().splitlines()
+                voice = phone_and_fax[0].replace('Telephone: ', '').replace('Téléphone : ', '').strip()
+                fax = phone_and_fax[1].replace('Fax: ', '').replace('Télécopieur : ', '').strip()
+                #pprint(voice)
+                #pprint(fax)
                 if voice:
-                    voice = voice[0].replace('Telephone: ', '').replace('Téléphone : ', '')
-                    if voice:
-                        m.add_contact('voice', voice, note)
-                fax = li.xpath('./span//text()[contains(., "Fax:")]|./span//text()[contains(., "Télécopieur :")]')
+                    m.add_contact('voice', voice, 'legislature')
+
                 if fax:
-                    fax = fax[0].replace('Fax: ', '').replace('Télécopieur : ', '')
-                    if fax:
-                        m.add_contact('fax', fax, note)
+                    m.add_contact('fax', fax, 'legislature')
+
+
+            # for i, li in enumerate(mp_page.xpath('.//div[@class="constituencyoffices"]//li')):
+            #     spans = li.xpath('./span[not(@class="spacer")]')
+            #     note = 'constituency'
+            #     if i:
+            #         note += ' ({})'.format(i + 1)
+            #     m.add_contact('address', '\n'.join([
+            #         spans[0].text_content(),  # address line 1
+            #         spans[1].text_content(),  # address line 2
+            #         spans[2].text_content(),  # city, region
+            #         spans[3].text_content(),  # postal code
+            #     ]), note)
+            #     voice = li.xpath('./span//text()[contains(., "Telephone:")]|./span//text()[contains(., "Téléphone :")]')
+            #     if voice:
+            #         voice = voice[0].replace('Telephone: ', '').replace('Téléphone : ', '')
+            #         if voice:
+            #             m.add_contact('voice', voice, note)
+            #     fax = li.xpath('./span//text()[contains(., "Fax:")]|./span//text()[contains(., "Télécopieur :")]')
+            #     if fax:
+            #         fax = fax[0].replace('Fax: ', '').replace('Télécopieur : ', '')
+            #         if fax:
+            #             m.add_contact('fax', fax, note)
 
             yield m
