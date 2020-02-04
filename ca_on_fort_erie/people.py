34b629267c9359b43f341eec2a0f5c49c5862e6f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-02-04 17:01:06 -0500

    ca_on_fort_erie: Update URL

--- ca_on_fort_erie/people.py
+++ ca_on_fort_erie/people.py
@@ -2,5 +2,5 @@ from utils import CSVScraper
 
 
 class FortEriePersonScraper(CSVScraper):
-    # https://niagaraopendata.ca/dataset/town-of-fort-erie-elected-officials
-    csv_url = 'https://niagaraopendata.ca/dataset/744965a4-ed48-4ca9-a838-48d07c76a36b/resource/731205b6-bc59-4e08-90bf-80705033e4d4/download/fort-erie-elected-officials.csv'
+    # https://niagaraopendata.ca/dataset/town-of-fort-erie-elected-officials-2018
+    csv_url = 'https://niagaraopendata.ca/dataset/0ab66777-7cf7-4e0b-bbbb-5c7c8f025dc5/resource/96120e11-fc21-4b33-baa7-983b0953dd9c/download/fort-erie-elected-officials-2018.csv'
