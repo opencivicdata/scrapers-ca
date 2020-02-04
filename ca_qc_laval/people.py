c950f469eacb6243a4a737eec418d4010bd7d542
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-02-04 18:48:36 -0500

    ca_qc_laval: Ignore previous councils

--- ca_qc_laval/people.py
+++ ca_qc_laval/people.py
@@ -4,7 +4,7 @@ from utils import CSVScraper
 
 class LavalPersonScraper(CSVScraper):
     # https://www.donneesquebec.ca/recherche/fr/dataset/liste-des-elus
-    csv_url = 'https://www.donneesquebec.ca/recherche/dataset/8fe69713-fade-4751-a0b4-7e57a81886b1/resource/bb38e19e-26ab-495c-a0f7-ed6b3268b6e6/download/liste-elus.csv'
+    csv_url = 'https://www.donneesquebec.ca/recherche/fr/dataset/8fe69713-fade-4751-a0b4-7e57a81886b1/resource/bb38e19e-26ab-495c-a0f7-ed6b3268b6e6/download/liste-elus.csv'
     encoding = 'utf-8-sig'
     locale = 'fr'
     # Laval also removes accents and cedillas from data.
@@ -35,3 +35,6 @@ class LavalPersonScraper(CSVScraper):
             'telecopieur': 'fax',
             'url photo': 'photo url',
         }.get(s, s)
+
+    def is_valid_row(self, row):
+        return row['année de à'] == '2017-2021'
