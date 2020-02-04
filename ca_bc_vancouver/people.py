2e4dbe424fb184c8c2af4b6a593eb0958e803567
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-02-04 16:39:09 -0500

    ca_bc_vancouver: Update URL

--- ca_bc_vancouver/people.py
+++ ca_bc_vancouver/people.py
@@ -3,8 +3,9 @@ from utils import CSVScraper
 
 class VancouverPersonScraper(CSVScraper):
     # https://data.vancouver.ca/datacatalogue/councilContactInfo.htm
-    csv_url = 'ftp://webftp.vancouver.ca/OpenData/csv/ElectedOfficialsContactInformation.csv'
+    csv_url = 'https://opendata.vancouver.ca/explore/dataset/elected-officials-contact-information/download/?format=csv&timezone=America/New_York&use_labels_for_header=true&csv_separator=%3B'
     many_posts_per_area = True
+    delimiter = ';'
     corrections = {
         'fax': {
             'N/A': None,
