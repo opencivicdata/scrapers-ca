2fe7fa279beebbb06daf9844a6331db161582250
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-02-04 20:11:19 -0500

    ca_qc_terrebonne: Fix

--- ca_qc_terrebonne/people.py
+++ ca_qc_terrebonne/people.py
@@ -6,34 +6,29 @@ COUNCIL_PAGE = 'http://www.ville.terrebonne.qc.ca/fr/10/Conseil_municipal'
 class TerrebonnePersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE, 'utf-8')
-        councillors = page.xpath('//div[@class="member-box member-box--gray"]')
+        councillors = page.xpath('//div[contains(@class, "member-box member-box--")]')
         assert len(councillors), 'No councillors found'
-        for councillor_elem in councillors:
-            name = councillor_elem.xpath('.//div[@class="fiche__name"]/text()')[0]
-            district = councillor_elem.xpath('.//div[@class="fiche__category"]/text()')[0]
-            phone = councillor_elem.xpath('.//div[@class="fiche__social"]/span/text()')[0].split('T')[1]
-            email_mailto = councillor_elem.xpath('.//div[@class="fiche__social"]/a[contains(@href, "mailto")]/@href')
-            photo_url = councillor_elem.xpath('.//img')[0].attrib['src']
+        for councillor in councillors:
+            name = councillor.xpath('.//div[@class="fiche__name"]/text()')[0]
+            phone = councillor.xpath('.//div[@class="fiche__social"]/span/text()')[0].split('T')[1]
+            email_mailto = councillor.xpath('.//div[@class="fiche__social"]/a[contains(@href, "mailto")]/@href')
+            photo_url = councillor.xpath('.//img')[0].attrib['src']
 
-            p = Person(primary_org='legislature', name=name, district=district, role='Conseiller',
-                       image=photo_url)
+            page = self.lxmlize(councillor.xpath('.//a[@class="member-box__calltoaction"]/@href')[0])
+            district = page.xpath('.//div[@class="fiche__category"]/text()')[0]
+
+
+            if district == 'Maire':
+                district = 'Terrebonne'
+                role = 'Maire'
+            else:
+                district = 'District {}'.format(district)
+                role = 'Conseiller'
+
+            p = Person(primary_org='legislature', name=name, district=district, role=role, image=photo_url)
             p.add_source(COUNCIL_PAGE)
             p.add_contact('voice', phone, 'legislature')
             if email_mailto:
                 email = email_mailto[0].split('mailto:')[1]
                 p.add_contact('email', email)
             yield p
-
-        mayor_elem = page.xpath('//div[@class="member-box member-box--main"]')[0]
-        name = mayor_elem.xpath('.//div[@class="fiche__name"]/text()')[0]
-        phone = mayor_elem.xpath('.//div[@class="fiche__social"]/span/text()')[0].split('T')[1]
-        email_mailto = mayor_elem.xpath('.//div[@class="fiche__social"]/a[contains(@href, "mailto")]/@href')
-        photo_url = councillor_elem.xpath('.//img')[0].attrib['src']
-        p = Person(primary_org='legislature', name=name, district='Terrebonne', role='Maire',
-                   image=photo_url)
-        p.add_source(COUNCIL_PAGE)
-        p.add_contact('voice', phone, 'legislature')
-        if email_mailto:
-            email = email_mailto[0].split('mailto:')[1]
-            p.add_contact('email', email)
-        yield p
