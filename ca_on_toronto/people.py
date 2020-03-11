9781b85d6e119a86a867ad2a9e0596dd4aa20664
Andrew Lindsay (mirabuck) <andrew@newmode.net>
2020-03-11 08:32:55 -0400

    ET-4334 : removing unused CSV urls.

--- ca_on_toronto/people.py
+++ ca_on_toronto/people.py
@@ -3,8 +3,6 @@ from utils import CSVScraper
 
 class TorontoPersonScraper(CSVScraper):
     # https://www.toronto.ca/city-government/data-research-maps/open-data/open-data-catalogue/#3bdfa14a-7792-04e7-6c8a-c33b6395b5e4
-    #csv_url = 'https://www.toronto.ca/ext/open_data/catalog/data_set_files/Councillor_Contact_Data_December_2018.xlsx'
-    #csv_url = 'https://open.toronto.ca/dataset/elected-officials-contact-information/2018-2022-elected-officials-contact.xlsx/'
     csv_url = 'https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/27aa4651-4548-4e57-bf00-53a346931251/resource/e8370139-b9c3-46e3-b57d-b7f663ad6ed9/download/2018-2022-elected-officials-contact.xlsx'
     encoding = 'windows-1252'
     other_names = {
