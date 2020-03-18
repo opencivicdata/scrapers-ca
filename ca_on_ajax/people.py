79c1c8a87fd4507e3eb96f7e3aca57252c691b96
akarshikalowe <50383511+akarshikalowe@users.noreply.github.com>
2020-03-18 02:24:29 -0400

    ET-4334_fixed_scraper_ajax

--- ca_on_ajax/people.py
+++ ca_on_ajax/people.py
@@ -2,48 +2,44 @@ from utils import CanadianScraper, CanadianPerson as Person, CONTACT_DETAIL_TYPE
 
 import re
 
-COUNCIL_PAGE = 'http://www.ajax.ca/en/insidetownhall/mayorcouncillors.asp'
+COUNCIL_PAGE = 'https://www.ajax.ca/en/inside-townhall/council-members.aspx'
 
 
 class AjaxPersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
 
-        councillors = page.xpath('//ul[@class="subNav top"]/li/ul//li/a[contains(text(), "Councillor")] | //ul[@class="subNav top"]/li/ul//li/a[contains(text(), "Mayor")]')
-        assert len(councillors), 'No councillors found'
+        councillors = page.xpath('//table[@class="councilTable"]')
         for councillor in councillors:
-            name = councillor.text_content()
+            image = councillor.xpath('.//@src')[0]
+            councillor_name = councillor.xpath('.//tr/td[1]/p[1]/img/@alt')
+            assert len(councillors), 'No councillors found'
+       
+            for i in councillor_name:
+                sep = '-'
+                name = i.split(sep, 1)[0]
 
-            url = councillor.attrib['href']
-            page = self.lxmlize(url)
-
-            if councillor == councillors[0]:
+            if name == councillor_name[0]:
                 district = 'Ajax'
                 role = 'Mayor'
+
             else:
-                district = re.findall(r'Ward.*', page.xpath('//div[@id="printAreaContent"]//h1')[0].text_content())[0].strip()
-                role = page.xpath('//div[@id="printAreaContent"]//h1')[0].text_content()
-                role = re.search('((?:Regional )?Councillor)', role).group(1)
+                role=i.split(sep, 1)[-1]
+        
+            cell = councillor.xpath('.//p[contains(.,"Cel")]/text()')[0]
+            tel= councillor.xpath('.//p[contains(.,"Cel")]/text()')[1]
+            phone=cell.replace('\xa0', ' ')
+           
 
-            name = name.replace(role, '').strip()
-
-            p = Person(primary_org='legislature', name=name, district=district, role=role)
+            p = Person(primary_org='legislature', name=name, district='district', role='role')
             p.add_source(COUNCIL_PAGE)
-            p.add_source(url)
+            p.image=image
 
-            p.image = page.xpath('//div[@class="intQuicklinksPhoto"]//img/@src')[0]
+            if phone:
+                p.add_contact('cell', phone, 'legislature')
+            if tel:
+                p.add_contact('voice', tel, 'legislature')
 
-            contact_info = page.xpath('//table[@class="datatable"][1]//tr')[1:]
-            for line in contact_info:
-                contact_type = line.xpath('./td')[0].text_content().strip()
-                if re.match(r'(Home)|(Cell)|(Phone)|(Fax)|(Email)', contact_type):
-                    contact = line.xpath('./td')[1].text_content().strip()
-                    contact_type = CONTACT_DETAIL_TYPE_MAP[contact_type]
-                    p.add_contact(contact_type, contact, '' if contact_type == 'email' else 'legislature')
-                elif contact_type == 'Address':
-                    contact = ''.join(line.xpath('./td[2]//text()')).strip()
-                    p.add_contact(contact_type, contact, 'legislature')
-                else:
-                    contact = line.xpath('./td[2]/a/@href')[0]
-                    p.add_link(contact)
+            p.add_contact('email', self.get_email(councillor))                
+
             yield p
