fb2882b748608527c2c2ed99da8b7ecb9928849d
akarshikalowe <50383511+akarshikalowe@users.noreply.github.com>
2020-03-19 01:21:17 -0400

    ET-4334_saint_jerome_scraper_fix

--- ca_qc_saint_jerome/people.py
+++ ca_qc_saint_jerome/people.py
@@ -1,40 +1,33 @@
 # coding: utf-8
 from utils import CanadianScraper, CanadianPerson as Person
 
-COUNCIL_PAGE = 'http://www.vsj.ca/fr/membres-du-conseil.aspx'
-
+COUNCIL_PAGE = 'https://www.vsj.ca/conseil-municipal-et-comite-executif/membres-du-conseil-municipal'
 
 class SaintJeromePersonScraper(CanadianScraper):
     def scrape(self):
-        page = self.lxmlize(COUNCIL_PAGE)
+        page = self.lxmlize(COUNCIL_PAGE,encoding='utf-8')
+    
+        councillors = page.xpath('//div[contains(@class," inner_member")]')
 
-        councillors = [tr for tr in page.xpath('//table//tr[1]') if len(tr) == 2][:-1]
-        assert len(councillors), 'No councillors found'
-        for i, councillor in enumerate(councillors):
-            texts = [text.strip() for text in councillor.xpath('.//text()[normalize-space()]') if text.strip()]
-
-            if 'vacant' in texts[1]:
-                continue
-
-            if i == 0:
+        for councillor in councillors:
+           
+            name = councillor.xpath('.//h2/text()')[0]
+            assert len(councillors), 'No councillors found'
+                       
+            if name[0]:
+                district = 'saint-Jerome'
                 role = 'Maire'
-                district = 'Saint-Jérôme'
-                name = texts[0]
-                phone = texts[1]
             else:
-                role = 'Conseiller'
-                district = texts[0].replace('numéro ', '')
-                name = texts[1]
-                if len(texts) > 3:
-                    phone = texts[2]
+                role=councillor.xpath('.//div[contains(@class,"role")]/text()')
+                district=councillor.xpath('.//div[contains(@class,"district")]/text()')
 
-            email = texts[-1]
-
-            image = councillor.xpath('.//img/@src')[0]
-
+            image = councillor.xpath('.//div[@class="portrait_single"]/img/@data-lazy-src')[0]
+      
+            contact= councillor.xpath('.//div[contains(@class,"phone")]/text()')[0]
             p = Person(primary_org='legislature', name=name, district=district, role=role)
             p.add_source(COUNCIL_PAGE)
             p.image = image
-            p.add_contact('voice', phone, 'legislature')
-            p.add_contact('email', email)
+            p.add_contact('voice', contact, 'legislature')
+            p.add_contact('email', self.get_email(councillor))   
+
             yield p
