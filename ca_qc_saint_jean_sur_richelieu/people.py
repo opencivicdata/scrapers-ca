396737a1c3f02bf46d067078b93ae611b3a71d92
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-03-23 16:19:24 -0400

    flake8

--- ca_qc_saint_jean_sur_richelieu/people.py
+++ ca_qc_saint_jean_sur_richelieu/people.py
@@ -1,18 +1,17 @@
-from utils import CanadianScraper, CanadianPerson as Person, clean_string
-import re
+from utils import CanadianScraper, CanadianPerson as Person
 from urllib.parse import urljoin
 
 COUNCIL_PAGE = 'https://sjsr.ca/conseil-municipal/'
 
+
 class SaintJeanSurRichelieuPersonScraper(CanadianScraper):
     def scrape(self):
-        page = self.lxmlize(COUNCIL_PAGE,encoding='utf-8')
+        page = self.lxmlize(COUNCIL_PAGE, encoding='utf-8')
 
-        councillors = page.xpath('//div[contains(@class,"fl-module-content fl-node-content")]/div[@class="fl-rich-text"]//a')
+        councillors = page.xpath('//div[contains(@class, "fl-module-content fl-node-content")]/div[@class="fl-rich-text"]//a')
         assert len(councillors), 'No councillors found'
         for councillor in councillors:
-
-            name= councillor.xpath('.//text()')[0]
+            name = councillor.xpath('.//text()')[0]
             url = councillor.xpath('./@href')[0]
             if name[0]:
                 ward = 'Saint-Jean-sur-Richelieu'
@@ -27,14 +26,14 @@ class SaintJeanSurRichelieuPersonScraper(CanadianScraper):
             node = self.lxmlize(url)
             photo_url_rel = node.xpath('//div[@class="fl-photo-content fl-photo-img-jpg"]//img/@src')[0]
             photo_url = urljoin(url, photo_url_rel)
-            
+
             p = Person(primary_org='legislature', name=name, district=ward, role=role)
             p.add_source(COUNCIL_PAGE)
             p.add_source(url)
             p.image = photo_url
 
-            if node.xpath('//div[contains(@class,"fl-module-content fl-node-content")]/div[@class="fl-rich-text"]/p[2]/strong//text()'):
-                p.add_contact('voice', node.xpath('//div[contains(@class,"fl-module-content fl-node-content")]/div[@class="fl-rich-text"]/p[2]/strong//text()')[0],'legislature')
+            voice = node.xpath('//div[contains(@class, "fl-module-content fl-node-content")]/div[@class="fl-rich-text"]/p[2]/strong//text()')  # noqa: E501
+            if voice:
+                p.add_contact('voice', voice[0], 'legislature')
 
             yield p
-
