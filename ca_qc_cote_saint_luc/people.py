76a338bacf150c52690f7c71372207f5c5dcf69f
akarshikalowe <50383511+akarshikalowe@users.noreply.github.com>
2020-03-25 02:40:39 -0400

    ET-4334_ca_qc_cote_saint_luc
    fix for ca_qc_cote_saint_luc

--- ca_qc_cote_saint_luc/people.py
+++ ca_qc_cote_saint_luc/people.py
@@ -1,66 +1,41 @@
 # coding: utf-8
 from utils import CanadianScraper, CanadianPerson as Person, CUSTOM_USER_AGENT
 
-import re
-from urllib.parse import urljoin
-
 COUNCIL_PAGE = 'https://cotesaintluc.org/city-government/council/#councilmembers'
 
 
 class CoteSaintLucPersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE, user_agent=CUSTOM_USER_AGENT)
-
-        mayor_url = page.xpath('//a[contains(text(), "Mayor")]/@href')[0]
-        mayor = self.scrape_mayor(mayor_url)
-        if mayor:
-            yield mayor
-
-        councillors_url = page.xpath('//a[contains(text(), "Councillors")]/@href')[0]
-        cpage = self.lxmlize(councillors_url, user_agent=CUSTOM_USER_AGENT)
-
-        councillors = cpage.xpath('//tr[td//img]')[:-1]
-
+        councillors = page.xpath('//section[contains(@class,"avia-team-member")]')[:-1]
         assert len(councillors), 'No councillors found'
-        for councillor_row in councillors:
-            img_cell, info_cell = tuple(councillor_row)
-            if info_cell.xpath('.//p//text()[contains(., "Vacant")]'):
-                continue
-            cells = [x.strip() for x in info_cell.xpath('.//text()') if re.sub('\xa0', ' ', x).strip()]
-            name = cells[0].replace('Councillor ', '')
-            district = info_cell.xpath('.//p[contains(text(), "District")]//text()')[0]
-            email = self.get_email(info_cell)
-            phone = self.get_phone(info_cell, area_codes=[438, 514], error=False)
-            img_url_rel = img_cell.xpath('.//img/@src')[0]
-            img_url = urljoin(councillors_url, img_url_rel)
 
-            p = Person(primary_org='legislature', name=name, district=district, role='Conseiller')
+        for councillor in councillors:
+            name = councillor.xpath('.//h3/text()')[0]
+
+            if (councillor.xpath('.//div[contains(@class,"team-member-job-title")][contains(.,"Mayor")]/text()')):
+                role = "Mayor"
+                district = "Cote-Saint-Luc"
+
+            else:
+                roles = councillor.xpath('.//div[contains(@class,"team-member-job-title")]/text()')[0]
+                role, district = roles.split(',', 1)
+
+            image = councillor.xpath('.//img/@src')[0]
+            twitter = councillor.xpath('.//p[contains(.,"Twitter")]/a/text()')
+            web_address = councillor.xpath('.//p[contains(.,"Web")]/a/@href')
+            blog = councillor.xpath('.//p[contains(.,"Blog")]/a/@href')
+
+            p = Person(primary_org='legislature', name=name, district=district, role=role)
             p.add_source(COUNCIL_PAGE)
-            p.add_source(councillors_url)
-            p.add_contact('email', email)
-            if phone:
-                p.add_contact('voice', phone, 'legislature')
-            p.image = img_url
+            p.add_contact('email', self.get_email(councillor))
+            p.add_contact('voice', self.get_phone(councillor, area_codes=[514]), 'legislature')
+            p.image = image
+            if twitter:
+                p.add_link(twitter[0])
+            if web_address:
+                p.add_link(web_address[0])
+            if blog:
+                p.add_link(blog[0])
+
             yield p
-
-    def scrape_mayor(self, url):
-        page = self.lxmlize(url, user_agent=CUSTOM_USER_AGENT)
-        text = page.xpath('//h1//text()[contains(., "Mayor")]')[0]
-        if 'Acting Mayor' in text:
-            # A councillor is acting mayor. We would need to add two roles to
-            # the same person, which can be done with a little effort.
-            return
-
-        name = re.sub('(?:Acting )?Mayor ', '', text)
-
-        email = self.get_email(page)
-        phone = self.get_phone(page.xpath('//table[1]')[0])
-
-        p = Person(primary_org='legislature', name=name, district='Côte-Saint-Luc', role='Maire')
-        p.add_source(COUNCIL_PAGE)
-        p.add_source(url)
-        p.image = page.xpath('.//div[contains(@class,"content")]//img/@src')[0]
-        p.add_source(url)
-        p.add_contact('email', email)
-        p.add_contact('voice', phone, 'legislature')
-        return p
