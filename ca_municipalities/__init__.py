d6127722d7f081caf36ef3d4ec9126827dec389d
Kevin Porras <kporras07@gmail.com>
2020-03-26 16:24:40 -0600

    ET-4282: Switch to final csv and add some validation.

--- ca_municipalities/__init__.py
+++ ca_municipalities/__init__.py
@@ -7,8 +7,7 @@ class CanadaMunicipalities(CanadianJurisdiction):
     division_id = 'ocd-division/country:ca'
     division_name = 'Canada'
     name = 'Canada rural towns and villages municipal councils'
-    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQzUYBs5WnnMaFtdu6l98jPsJpTXR-mJqdRG6Beb02JMSmvq6FgZCGraBEUESuhEzNX8TDhqX2p1YM8/pub?output=csv'
-    #url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRrGXQy8qk16OhuTjlccoGB4jL5e8X1CEqRbg896ufLdh67DQk9nuGm-oufIT0HRMPEnwePw2HDx1Vj/pub?output=csv'
+    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRrGXQy8qk16OhuTjlccoGB4jL5e8X1CEqRbg896ufLdh67DQk9nuGm-oufIT0HRMPEnwePw2HDx1Vj/pub?gid=0&single=true&output=csv'
 
     def get_organizations(self):
         return []
