c480a259370897d48eedd1e290cddcbba46a3c07
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-03-27 00:22:23 -0400

    Fix strings for French jurisdiction

--- ca_qc_cote_saint_luc/people.py
+++ ca_qc_cote_saint_luc/people.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from utils import CanadianScraper, CanadianPerson as Person, CUSTOM_USER_AGENT
 
-COUNCIL_PAGE = 'https://cotesaintluc.org/city-government/council/#councilmembers'
+COUNCIL_PAGE = 'https://cotesaintluc.org/fr/municipalite/conseil/'
 
 
 class CoteSaintLucPersonScraper(CanadianScraper):
@@ -13,28 +13,29 @@ class CoteSaintLucPersonScraper(CanadianScraper):
         for councillor in councillors:
             name = councillor.xpath('.//h3/text()')[0]
 
-            if (councillor.xpath('.//div[contains(@class,"team-member-job-title")][contains(.,"Mayor")]/text()')):
-                role = "Mayor"
-                district = "Cote-Saint-Luc"
-
+            if councillor.xpath('.//div[contains(@class,"team-member-job-title")][contains(.,"Maire")]/text()'):
+                role = 'Maire'
+                district = 'Côte-Saint-Luc'
             else:
-                roles = councillor.xpath('.//div[contains(@class,"team-member-job-title")]/text()')[0]
-                role, district = roles.split(',', 1)
+                role, district = councillor.xpath('.//div[contains(@class,"team-member-job-title")]/text()')[0].split(',', 1)
+                if role == 'Conseillère':
+                    role = 'Conseiller'
 
             image = councillor.xpath('.//img/@src')[0]
             twitter = councillor.xpath('.//p[contains(.,"Twitter")]/a/text()')
-            web_address = councillor.xpath('.//p[contains(.,"Web")]/a/@href')
+            web = councillor.xpath('.//p[contains(.,"Web")]/a/@href')
             blog = councillor.xpath('.//p[contains(.,"Blog")]/a/@href')
 
             p = Person(primary_org='legislature', name=name, district=district, role=role)
             p.add_source(COUNCIL_PAGE)
+
             p.add_contact('email', self.get_email(councillor))
             p.add_contact('voice', self.get_phone(councillor, area_codes=[514]), 'legislature')
             p.image = image
             if twitter:
                 p.add_link(twitter[0])
-            if web_address:
-                p.add_link(web_address[0])
+            if web:
+                p.add_link(web[0])
             if blog:
                 p.add_link(blog[0])
 
