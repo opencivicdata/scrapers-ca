280ead2ea01ea5e679d27fe1a058cf6698223708
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-03-27 00:08:00 -0400

    Use `district` variable name

--- ca_qc_saint_jean_sur_richelieu/people.py
+++ ca_qc_saint_jean_sur_richelieu/people.py
@@ -11,28 +11,26 @@ class SaintJeanSurRichelieuPersonScraper(CanadianScraper):
         councillors = page.xpath('//div[contains(@class,"fl-module-content fl-node-content")]/div[@class="fl-rich-text"]')[1:]
         assert len(councillors), 'No councillors found'
         for councillor in councillors:
-
             name = councillor.xpath('.//a//text()')[0]
             url = councillor.xpath('.//a//@href')[0]
 
             if 'maire' in url:
-                ward = 'Saint-Jean-sur-Richelieu'
                 role = 'Maire'
+                district = 'Saint-Jean-sur-Richelieu'
             else:
                 role = 'Conseiller'
-                ward = councillor.xpath('.//p[contains(.,"District")]/text()')[-1]
+                district = councillor.xpath('.//p[contains(.,"District")]/text()')[-1]
 
             node = self.lxmlize(url)
             photo_url_rel = node.xpath('//div[@class="fl-photo-content fl-photo-img-jpg"]//img/@src')[0]
             photo_url = urljoin(url, photo_url_rel)
 
-            p = Person(primary_org='legislature', name=name, district=ward, role=role)
+            p = Person(primary_org='legislature', name=name, district=district, role=role)
             p.add_source(COUNCIL_PAGE)
             p.add_source(url)
             p.image = photo_url
 
             voice = node.xpath('//div[contains(@class,"fl-module-content fl-node-content")]/div[@class="fl-rich-text"]/p[2]/strong//text()')
-
             if voice:
                 p.add_contact('voice', voice[0], 'legislature')
 
