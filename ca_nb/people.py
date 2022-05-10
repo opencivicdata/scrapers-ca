8b4795cf00e2bc599cd6a942a269be264f254a59
Simon Meers <simon@simonmeers.com>
2022-05-10 21:24:50 +1000

    Update to legnb.ca

--- ca_nb/people.py
+++ ca_nb/people.py
@@ -3,38 +3,36 @@ from utils import CanadianScraper, CanadianPerson as Person
 import re
 from urllib.parse import urljoin
 
-COUNCIL_PAGE = 'https://www1.gnb.ca/legis/bios/60/index-e.asp'  # update each election
-
-PARTIES = {
-    'G': 'Green Party',
-    'IND': 'Independent',
-    'L': 'New Brunswick Liberal Association',
-    'PA': "People's Alliance",
-    'PC': 'Progressive Conservative Party of New Brunswick',
-}
-
-
-def get_party(abbr):
-    """Return full party name based on abbreviation"""
-    return PARTIES[abbr]
+COUNCIL_PAGE = 'https://www.legnb.ca/en/members/current'  # update each election
 
 
 class NewBrunswickPersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE, encoding='utf-8')
-        members = page.xpath('//table/tbody/tr')
+        members = page.xpath('//div[contains(@class, "member-card")]//a//@href')
         assert len(members), 'No members found'
-        for row in members:
-            riding, table_name, email = (' '.join(td.text_content().split()) for td in row[1:])
+        for url in members:
+            node = self.lxmlize(url, encoding='utf-8')
+            phone = ''
+            email = ''
+            hrefs = node.xpath(
+                '//div[contains(@class, "member-details-contacts")]//a//@href'
+            )
+            for href in hrefs:
+                if href.startswith('mailto:'):
+                    email = href.replace('mailto:', '')
+                if href.startswith('tel:'):
+                    phone = href.replace('tel:', '')
 
-            if 'Vacant' in table_name:
-                continue
-
+            party, riding = [span.text_content().strip() for span in node.xpath(
+                '//div[contains(@class, "member-details-meta")]//span'
+            )]
             district = riding.replace('\x97', '-')
-            name_with_status, party_abbr = re.match(r'(.+) \((.+)\)', table_name).groups()
-            name = name_with_status.split(',')[0]
-            photo_page_url = row[2][0].attrib['href']
-            photo_url = self.get_photo_url(photo_page_url)
+            name = node.xpath('//h1')[0].text_content()
+            name = name.replace(', Q.C.', '')
+            photo_url = node.xpath(
+                '//div[contains(@class, "member-details-portrait")]//img//@src'
+            )[0]
 
             # @see https://en.wikipedia.org/wiki/Charlotte-Campobello
             if district == 'Saint Croix':
@@ -44,13 +42,10 @@ class NewBrunswickPersonScraper(CanadianScraper):
                 district = 'Oromocto-Lincoln'
 
             p = Person(primary_org='legislature', name=name, district=district, role='MLA',
-                       party=get_party(party_abbr.strip()), image=photo_url)
-            p.add_contact('email', email)
-            p.add_source(photo_page_url)
-            p.add_source(COUNCIL_PAGE)
+                       party=party, image=photo_url)
+            if phone:
+                  p.add_contact('voice', phone, 'legislature')
+            if email:
+                  p.add_contact('email', email)
+            p.add_source(url)
             yield p
-
-    def get_photo_url(self, url):
-        page = self.lxmlize(url)
-        rel = page.xpath('//td/img/@src')[0]
-        return urljoin(url, rel)
