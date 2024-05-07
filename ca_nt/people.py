6ed41c41721304642d9943f82e747d9a35507df5
Seamus Lee <seamuslee001@gmail.com>
2024-05-07 15:56:40 +1000

    Update Northwest territories scraper to handle new site and updated ocd-division ids

--- ca_nt/people.py
+++ ca_nt/people.py
@@ -1,7 +1,7 @@
 from utils import CanadianPerson as Person
 from utils import CanadianScraper
 
-COUNCIL_PAGE = "https://www.ntassembly.ca/members"
+COUNCIL_PAGE = "https://www.ntlegislativeassembly.ca/members/members-legislative-assembly/members"
 
 
 class NorthwestPersonScraper(CanadianScraper):
@@ -9,38 +9,35 @@ class NorthwestPersonScraper(CanadianScraper):
         page = self.lxmlize(COUNCIL_PAGE)
 
         members = page.xpath(
-            '//*[@id="block-views-members-block-1"]/div/div/table//td[contains(@class, "views-row-members")]'
+            '//div[contains(@class, "view-id-member")]/div/div[contains(@class, "views-row")]'
         )
         assert len(members), "No members found"
         for member in members:
-            if "Vacant" not in member.xpath('./div[contains(@class, "views-field-title")]')[0].text_content():
-                url = member.xpath("./div/div[1]/a/@href")[0]
+            if "Vacant" not in member.xpath('//span[contains(@class, "field--name-title")]')[0].text_content():
+                url = member.xpath("./article/a/@href")[0]
                 page = self.lxmlize(url)
-                name = page.xpath('//*[@id="page-title"]')[0].text_content()
+                name = page.xpath('//*[contains(@class, "page-title")]')[0].text_content().strip()
                 district = (
-                    page.xpath('//*[@id="content"]/div/div[2]/div/div/p[1]')[0]
+                    page.xpath('//*[contains(@class, "field--name-field-constituency")]')[0]
                     .text_content()
                     .replace("Member ", "")
-                    .replace("-", " - ")
+                    .replace(' - ', '-')
                 )
-                if district == "Mackenzie Delta":
-                    district = "Mackenzie-Delta"
                 p = Person(primary_org="legislature", name=name, district=district, role="MLA")
                 p.add_source(COUNCIL_PAGE)
                 p.add_source(url)
                 try:
-                    p.image = page.xpath('//article[contains(@class, "member")]/p/img/@src')[0]
+                    p.image = page.xpath('//div[contains(@class, "field--name-field-media-image")]/img/@src')[0]
                 except IndexError:
                     pass
 
-                contact = page.xpath('//*[@id="content"]/div/div[2]/div/div')[0]
+                contact = page.xpath('//*[contains(@class, "paragraph--type--office")]')[0]
 
-                def handle_address(lines, address_type):
+                def handle_address(contact, address_type):
                     address_lines = []
-                    for line in lines:
-                        if line.endswith(":"):  # Room:, Phone:, Fax:
-                            break
-                        address_lines.append(line)
+                    po_box_line = 'PO Box ' + contact.xpath('./div[contains(@class, "office-address-wrapper")]/div[2]/div[2]')[0].text_content().strip()
+                    address_lines.append(po_box_line)
+                    address_lines.append(contact.xpath('./div[contains(@class, "office-address-wrapper")]/div[1]/p')[0].text_content().strip())
                     if address_lines:
                         p.add_contact(
                             "address",
@@ -51,8 +48,8 @@ class NorthwestPersonScraper(CanadianScraper):
                 def handle_phone(lines, phone_type):
                     first_phone_added = False
                     for line in lines:
-                        if "Phone number:" in line:
-                            number = line.replace("Phone number: (867) ", "").replace("Phone number: 867-", "")
+                        if "867" in line.strip():
+                            number = line.strip().replace("(867) ", "").replace("867-", "")
                             if first_phone_added:
                                 phone_type = "constituency"
                             if number[-4:] == "ext.":
@@ -61,10 +58,13 @@ class NorthwestPersonScraper(CanadianScraper):
                             first_phone_added = True
 
                 contact_lines = contact.xpath(".//text()")
-                handle_address(contact_lines[0:5], "legislature")
-                handle_phone(contact_lines[3:], "legislature")
+                handle_address(contact, "legislature")
+                handle_phone(contact_lines, "legislature")
 
-                email = self.get_email(contact, error=False)
-                if email:
-                    p.add_contact("email", email)
+                email_elements = page.xpath('//*[contains(@class, "field--paragraph--field-email")]/div[@class="field__item"]')
+                for element in email_elements:
+                    email = self.get_email(element, error=False)
+                    if email:
+                        p.add_contact("email", email)
+
                 yield p
