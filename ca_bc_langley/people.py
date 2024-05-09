9eab74ff91c51ea5025088382808d16494061e82
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-09 10:17:11 -0400

    Updated Langley scraper

--- ca_bc_langley/people.py
+++ ca_bc_langley/people.py
@@ -1,7 +1,38 @@
-from utils import CSVScraper
+from utils import CanadianPerson as Person
+from utils import CanadianScraper
 
+COUNCIL_PAGE = "https://www.tol.ca/en/the-township/councillors.aspx"
+MAYOR_PAGE = "https://www.tol.ca/en/the-township/mayor.aspx"
 
-class LangleyPersonScraper(CSVScraper):
-    # https://data-tol.opendata.arcgis.com/datasets/elected-officials
-    csv_url = "https://opendata.arcgis.com/datasets/daa3ea0f01d24e9b80d837cf2178eb71_0.csv"
-    many_posts_per_area = True
+class LangleyPersonScraper(CanadianScraper):
+    def scrape(self):
+        page = self.lxmlize(COUNCIL_PAGE)
+        seat_number = 1
+        councillors = page.xpath('//a[contains(@target,"_self")]/@href')
+        for url in councillors:
+            page = self.lxmlize(url)
+            name = page.xpath('//h1')[0].text_content().strip()
+            district = "Langley (seat {})".format(seat_number)
+            seat_number +=1
+            email = self.get_email(page)
+            phone = self.get_phone(page)
+
+            p =Person(primary_org="legislature",name=name,role="Councillor",district=district)
+            p.add_source(COUNCIL_PAGE)
+            p.add_source(url)
+            p.add_contact("email",email)
+            p.add_contact("voice",phone,"legislature")
+
+            yield p
+        page = self.lxmlize(MAYOR_PAGE)
+        name = page.xpath('//h1')[0].text_content().replace("Mayor", "").strip()
+        email = self.get_email(page)
+        phone = self.get_phone(page)
+        address_block = page.xpath('//p/a[@rel="noopener noreferrer"]/parent::p')[0].text_content()
+        address = ", ".join(address_block.split("\n")[1:2])
+        p = Person(primary_org="legislature",name=name,role="Mayor",district="Langley")
+        p.add_contact("email",email)
+        p.add_contact("voice",phone,"legislature")
+        p.add_contact("address",address,"legislature")
+        yield p
+            
