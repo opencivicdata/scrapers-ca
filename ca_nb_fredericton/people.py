a6e9cf028d0309f18ce794442a96e3fb9317ce23
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-09 14:40:47 -0400

    Update Fredericton scraper

--- ca_nb_fredericton/people.py
+++ ca_nb_fredericton/people.py
@@ -1,30 +1,25 @@
 from utils import CanadianPerson as Person
 from utils import CanadianScraper
 
-COUNCIL_PAGE = "http://www.fredericton.ca/en/city-hall/city-council-committees/mayor-council"
+COUNCIL_PAGE = "https://www.fredericton.ca/en/your-government/mayor-council"
 
 
 class FrederictonPersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
 
-        councillors = page.xpath('//div[contains(@class, "view-people")]//div[contains(@class, "views-row")]')
+        councillors = page.xpath('//div[contains(@class, "view view-councillor view-id-councillor view-display-id-block_1 js-view-dom-id-60ab7690fd8036f968c4406929bf5bec3a3f03554afaf4ff046cb5487dadf8da contextual-region")]//div[contains(@class, "views-row")]')
         assert len(councillors), "No councillors found"
         for councillor in councillors:
-            name = councillor.xpath('.//div[@property="dc:title"]//text()')[0]
-            paras = councillor.xpath('.//div[contains(@class, "field-name-field-sub-title")]//p/text()')
-
-            if paras[0] == "Mayor":
+            name = councillor.xpath('.//h3/a')[0].text_content()
+            text = councillor.xpath('.//div[@class="views-field views-field-field-councillor-title"]/div')[0].text_content()
+            ward_start = text.find("Ward")
+            if ward_start+1:
+                district = text[ward_start:ward_start+7].strip()
+                role = "Councillor"
+            else:
                 district = "Fredericton"
                 role = "Mayor"
-            else:
-                if paras[0] == "Deputy Mayor":
-                    index = 1
-                else:
-                    index = 0
-
-                district = paras[index].replace("\xa0", " ").split(", ", 1)[1]
-                role = "Councillor"
 
             url = councillor.xpath(".//@href")[0]
             page = self.lxmlize(url)
