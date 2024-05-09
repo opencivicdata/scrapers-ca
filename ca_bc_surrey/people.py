c3b025a1861449a007bf6a432c2d62b9564c3b7f
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-09 10:21:43 -0400

    Updated Surrey scraper

--- ca_bc_surrey/people.py
+++ ca_bc_surrey/people.py
@@ -3,52 +3,59 @@ import re
 from utils import CanadianPerson as Person
 from utils import CanadianScraper
 
-COUNCIL_PAGE = "https://www.surrey.ca/city-government/2999.aspx"
-
+COUNCIL_PAGE = "https://www.surrey.ca/city-government/mayor-council/city-councillors"
+MAYOR_PAGE = "https://www.surrey.ca/city-government/mayor-council/mayor-brenda-locke"
 
 class SurreyPersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
-        members = page.xpath("//a[@class='gtm-grid']")
+        members = page.xpath("//article[@class='teaser']")
 
         assert len(members), "No members found"
         seat_number = 1
         for member in members:
-            if not member.text_content().strip():
-                continue
 
-            name = member.text_content().strip()
+            role,name = member.xpath('.//a[@class="teaser__link"]/h4')[0].text_content().split(" ",1)
             district = "Surrey (seat {})".format(seat_number)
             seat_number += 1
-            role = "Councillor"
+            photo_url = member.xpath(".//figure//img/@src")[0]
 
-            url = member.attrib["href"]
+            url = member.xpath('.//a[@class="teaser__link"]/@href')[0]
             ext_infos = self.scrape_extended_info(url)
             p = Person(primary_org="legislature", name=name, district=district, role=role)
             p.add_source(COUNCIL_PAGE)
             p.add_source(url)
+            p.image = photo_url
 
             if ext_infos:  # member pages might return errors
-                email, phone, photo_url = ext_infos
-                if photo_url:
-                    p.image = photo_url
+                email, phone = ext_infos
                 if email:
                     p.add_contact("email", email)
                 if phone:
                     p.add_contact("voice", phone, "legislature")
             yield p
 
+        page = self.lxmlize(MAYOR_PAGE)
+        role,name = page.xpath('//h1/span')[0].text_content().split(" ",1)
+        ext_infos = self.scrape_extended_info(MAYOR_PAGE)
+        photo_url = page.xpath('//div[@class="page-header__hero js-cover-image"]/img/@src')[0]
+        
+        p = Person(primary_org="legislature", name=name, district = "Surrey", role=role)
+        p.add_source(MAYOR_PAGE)
+        p.image = photo_url
+
+        if ext_infos:  # member pages might return errors
+            email, phone = ext_infos
+            if email:
+                p.add_contact("email", email)
+            if phone:
+                p.add_contact("voice", phone, "legislature")
+        yield p
+
     def scrape_extended_info(self, url):
         phone = None
         email = None
         root = self.lxmlize(url)
-        main = root.xpath("//div[@class='inner-wrapper']")[0]
-        photo_url = main.xpath(".//img/@src")
-        paras = main.xpath(".//p")
-        for para in paras:
-            pattern = re.compile("(?:Office: )(.+?)\n(Email: )(.+)")
-            matches = re.search(pattern, para.text_content())
-            if matches:
-                phone = matches[1]
-                email = matches[3]
-        return email, phone, photo_url[0]
+        email = self.get_email(root)
+        phone = self.get_phone(root)
+        return email, phone
