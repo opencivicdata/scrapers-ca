7842261e5dcb757bb79239da8010bb9c414b1afa
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-10 16:45:09 -0400

    Update Belleville scraper

--- ca_on_belleville/people.py
+++ ca_on_belleville/people.py
@@ -4,45 +4,45 @@ from collections import defaultdict
 from utils import CanadianPerson as Person
 from utils import CanadianScraper
 
-COUNCIL_PAGE = "http://www.belleville.ca/city-hall/page/city-council"
+COUNCIL_PAGE = "https://www.belleville.ca/en/city-hall/councillors.aspx"
+MAYOR_PAGE = "https://www.belleville.ca/en/city-hall/mayors-office.aspx"
 
 
 class BellevillePersonScraper(CanadianScraper):
     seat_numbers = defaultdict(int)
 
     def scrape(self):
-        page = self.lxmlize(COUNCIL_PAGE)
+        page = self.lxmlize(MAYOR_PAGE)
 
-        node = page.xpath('//div[@class="content-field"]/h3[contains(./text(), "Mayor")]/following-sibling::p[2]')[0]
-        name = node.xpath("./strong/text()")[0]
-        phone = node.xpath("./text()")[2].split(": ")[1]
-        fax = node.xpath("./text()")[3].split(": ")[1]
-        email = node.xpath("./a/text()")[0]
-        image = node.xpath("./preceding::p//img/@src")[0]
+        node = page.xpath('//tbody/tr')[0]
+        name = node.xpath("./td")[0].text_content()
+        phone = node.xpath("./td")[2].text_content()
+        email = self.get_email(node)
+        image = page.xpath('//div[@class = "iCreateDynaToken"]//img/@src')[0]
 
         p = Person(primary_org="legislature", name=name, district="Belleville", role="Mayor")
-        p.add_source(COUNCIL_PAGE)
+        p.add_source(MAYOR_PAGE)
         p.add_contact("voice", phone, "legislature")
-        p.add_contact("fax", fax, "legislature")
         p.add_contact("email", email)
         p.image = image
 
         yield p
 
-        wards = page.xpath('//h3[contains(text(), "Councillors")]')
+        page = self.lxmlize(COUNCIL_PAGE)
+        wards = page.xpath('//h2[contains(text(), "Councillors")]')
         assert len(wards), "No councillors found"
         for ward in wards:
             ward_name = re.search(r"(Ward.+) Councillors", ward.text).group(1)
-            councillors = ward.xpath("./following-sibling::div[1]//strong")
+            councillors = ward.xpath("./following-sibling::*[img]")
             for councillor in councillors:
                 self.seat_numbers[ward_name] += 1
                 district = "{} (seat {})".format(ward_name, self.seat_numbers[ward_name])
                 role = "Councillor"
 
-                name = councillor.text_content()
-                phone = councillor.xpath("./following-sibling::text()[2]")[0].split(":")[1]
-                email = councillor.xpath("./following-sibling::a//text()")[0]
-                image = councillor.xpath("./preceding::img[1]/@src")[0]
+                name = councillor.xpath('./following-sibling::p')[0].text_content()
+                phone = councillor.xpath("./following-sibling::p/text()")[2].split(":")[1]
+                email = councillor.xpath("./following-sibling::p/a//text()")[0]
+                image = councillor.xpath("./img/@src")[0]
 
                 p = Person(primary_org="legislature", name=name, district=district, role=role)
                 p.add_source(COUNCIL_PAGE)
@@ -51,3 +51,5 @@ class BellevillePersonScraper(CanadianScraper):
                 p.image = image
 
                 yield p
+                if self.seat_numbers[ward_name] >= 6: # Assigning councillors to correct Ward
+                    break
