ff5890121d0de70d862ba8a417eb715f34f43c70
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-05-15 18:03:45 -0400

    fix(ca_nt): Add "the" to name

--- ca_nt/__init__.py
+++ ca_nt/__init__.py
@@ -5,5 +5,5 @@ class NorthwestTerritories(CanadianJurisdiction):
     classification = "legislature"
     division_id = "ocd-division/country:ca/territory:nt"
     division_name = "Northwest Territories"
-    name = "Legislative Assembly of Northwest Territories"
+    name = "Legislative Assembly of the Northwest Territories"
     url = "https://www.ntassembly.ca/"
