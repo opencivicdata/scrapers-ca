a26255b082c9126365b8402458e3e475f68255fa
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-15 10:48:46 -0400

    Fix getting emails for New Westminster officials

--- ca_bc_new_westminster/people.py
+++ ca_bc_new_westminster/people.py
@@ -1,8 +1,11 @@
+import re
+
 from utils import CanadianPerson as Person
 from utils import CanadianScraper
 
 COUNCIL_PAGE = "https://www.newwestcity.ca/city_hall/mayor_and_council/councillors.php"
 MAYOR_PAGE = "https://www.newwestcity.ca/city_hall/mayor_and_council/mayor.php"
+
 
 class NewWestminsterPersonScraper(CanadianScraper):
     def scrape(self):
@@ -10,26 +13,27 @@ class NewWestminsterPersonScraper(CanadianScraper):
         seat_number = 1
         councillors = page.xpath('//div[@id="main-bottom"]//li')
         for councillor in councillors:
-            name = councillor.xpath('.//a[@name]')[0].text_content()
+            name = councillor.xpath(".//a[@name]")[0].text_content()
             district = "New Westminster (seat {})".format(seat_number)
             seat_number += 1
             p = Person(primary_org="legislature", name=name, role="Councillor", district=district)
-            photo = councillor.xpath('//img/@src')[0]
-            # email = councillor.xpath('//div[@class="text"]//p/a[@href]')[0].text_content()
+            photo = councillor.xpath("//img/@src")[0]
+            email_parts = re.sub(r"[\"\[\]]", "", councillor.xpath('//div[@class="text"]//a/@data-ob')[0])
+            email = "@".join(email_parts.split(",")[:2])
             phone = self.get_phone(councillor)
-            # p.add_contact("email", email)
+            p.add_contact("email", email)
             p.add_contact("voice", phone, "legislature")
             p.add_source(COUNCIL_PAGE)
             p.image = photo
             yield p
         page = self.lxmlize(MAYOR_PAGE)
         name = page.xpath('//p[@class="page_description"]/strong')[0].text_content()
-        # email = page.xpath('//p/a/@href')[0]
+        email_parts = re.sub(r"[\"\[\]]", "", page.xpath("//a/@data-ob")[0])
+        email = "@".join(email_parts.split(",")[:2])
         phone = self.get_phone(page)
 
-        p = Person(primary_org="legislature",name=name, role="Mayor",district="New Westminster")
+        p = Person(primary_org="legislature", name=name, role="Mayor", district="New Westminster")
         p.add_source(MAYOR_PAGE)
-        # p.add_contact("email",email)
-        p.add_contact("voice",phone,"legislature")
+        p.add_contact("email", email)
+        p.add_contact("voice", phone, "legislature")
         yield p
-        
