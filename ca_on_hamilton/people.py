3fbb6675cf1032fe4a1b26ea206b8271b7cb834b
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-16 13:51:28 -0400

    Update Hamilton scraper

--- ca_on_hamilton/people.py
+++ ca_on_hamilton/people.py
@@ -1,27 +1,29 @@
 from utils import CanadianPerson as Person
 from utils import CanadianScraper
 
-COUNCIL_PAGE = "http://www.hamilton.ca/council-committee/mayor-council/city-councillors"
+COUNCIL_PAGE = "https://www.hamilton.ca/city-council/council-committee/city-council-members/city-councillors"
+MAYOR_PAGE = "https://www.hamilton.ca/city-council/council-committee/city-council-members/mayors-office"
 
 
 class HamiltonPersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
 
-        mayor_url = page.xpath('//section/h3[contains(., "Mayor\'s Office")]/a/@href')[0]
-        yield self.mayor_data(mayor_url)
+        yield self.mayor_data(MAYOR_PAGE)
 
-        councillors = page.xpath('//div[contains(@class, "menu-name-menu-service-structure")]/li/a')
+        councillors = page.xpath('//div[contains(@class, "image-cta-card")]//@href[1]')
         assert len(councillors), "No councillors found"
-        for a in councillors:
-            yield self.councillor_data(a.attrib["href"])
+        for url in councillors:
+            yield self.councillor_data(url)
 
     def councillor_data(self, url):
         page = self.lxmlize(url)
 
-        district = page.xpath('//h1[contains(., "Ward")]/text()')[0]
-        name = page.xpath('//div[@id="wb-pri"]//div[contains(@class, "coh-column third")]/p/strong/text()')[0]
-        info_node = page.xpath('//div[@id="wb-pri"]')[0]
+        district = page.xpath("//h1/text()")[0]
+        name = page.xpath('//h2[@class="title"]/text()')[0].split("(")[0].strip()
+        info_node = page.xpath(
+            '//div[@class="section container-md image--right post-card post-card--large bg--#FFFFFF"]'
+        )[0]
         phone = self.get_phone(info_node, area_codes=[289, 365, 905])
         email = self.get_email(info_node)
         photo_url = info_node.xpath(".//img/@src")  # can be empty
@@ -40,17 +42,17 @@ class HamiltonPersonScraper(CanadianScraper):
 
     def mayor_data(self, url):
         page = self.lxmlize(url)
+        name = page.xpath('//h2[@class="title"]/text()')[0]
 
-        name = page.xpath('//h1[contains(., "Mayor")]/text()')[0].split("-")[1].replace("Mayor", "")
-
-        info_node = page.xpath('//div[@id="wb-pri"]')[0]
+        info_node = page.xpath(
+            '//div[@class="section container-md image--right post-card post-card--large bg--#00578E"]'
+        )[0]
         phone = self.get_phone(info_node, area_codes=[289, 365, 905])
         email = self.get_email(info_node)
         photo_url = info_node.xpath(".//img/@src")[0]
 
         p = Person(primary_org="legislature", name=name, district="Hamilton", role="Mayor")
-        p.add_source(COUNCIL_PAGE)
-        p.add_source(url)
+        p.add_source(MAYOR_PAGE)
         p.add_contact("email", email)
         p.add_contact("voice", phone, "legislature")
         p.image = photo_url
