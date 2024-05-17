1d08606659b1ee267afe92c6cd589e97018e1514
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-17 15:58:56 -0400

    Clean up code for Grande Prairie County no. 1 scraper

--- ca_ab_grande_prairie_county_no_1/people.py
+++ ca_ab_grande_prairie_county_no_1/people.py
@@ -3,11 +3,12 @@ from utils import CanadianScraper
 
 COUNCIL_PAGE = "https://www.countygp.ab.ca/en/county-government/council.aspx"
 
+
 class GrandePrairieCountyNo1PersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
         councillors = page.xpath('//div[contains(@class, "fbg-row lmRow ui-sortable")]')
-        
+
         # the first two matching containers have no councillor
         councillors = councillors[3:]
 
@@ -17,17 +18,18 @@ class GrandePrairieCountyNo1PersonScraper(CanadianScraper):
             name = councillor.xpath('.//div[@class="lb-imageBox_header {headColor}"]')[0].text_content()
             if "Reeve" in name:
                 name = name.split("Reeve")[1].strip()
-            district = councillor.xpath('.//h3|.//h2')[0].text_content()
+            district = councillor.xpath(".//h3|.//h2")[0].text_content()
 
             if district == "Division 5":
                 # district 5's councillor is the Reeve
-                p = Person(primary_org="legislature",name=name, district = district, role = "Reeve")
+                p = Person(primary_org="legislature", name=name, district=district, role="Reeve")
             elif district == "Division 7":
-                p = Person(primary_org="legislature",name=name, district = district, role = "Deputy Reeve")
+                # district 7's councillor is the Deputy Reeve
+                p = Person(primary_org="legislature", name=name, district=district, role="Deputy Reeve")
             else:
-                p= Person(primary_org="legislature",name=name,district=district,role=role)
-                        
+                p = Person(primary_org="legislature", name=name, district=district, role=role)
+
             p.add_source(COUNCIL_PAGE)
-            p.image = councillor.xpath('.//img/@src')[0]
+            p.image = councillor.xpath(".//img/@src")[0]
 
             yield p
