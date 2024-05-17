b19654e04082a7db4605cd36280a70c6c3ce9ac9
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-17 15:26:06 -0400

    Change Edmonton ward names

--- ca_ab_edmonton/__init__.py
+++ ca_ab_edmonton/__init__.py
@@ -1,5 +1,3 @@
-from pupa.scrape import Organization
-
 from utils import CanadianJurisdiction
 
 
@@ -9,29 +7,3 @@ class Edmonton(CanadianJurisdiction):
     division_name = "Edmonton"
     name = "Edmonton City Council"
     url = "http://www.edmonton.ca"
-
-    def get_organizations(self):
-        organization = Organization(self.name, classification=self.classification)
-
-        organization.add_post(role="Mayor", label=self.division_name, division_id=self.division_id)
-        for ward_number, ward_name in enumerate(
-            (
-                "Anirniq",
-                "Dene",
-                "Ipiihkoohkanipiaohtsi",
-                "Karhiio",
-                "Métis",
-                "Nakota Isga",
-                "O-day'min",
-                "papastew",
-                "pihêsiwin",
-                "sipiwiyiniwak",
-                "Sspomitapi",
-                "tastawiyiniwak",
-            ),
-            1,
-        ):
-            organization.add_post(
-                role="Councillor", label=ward_name, division_id="{}/ward:{}".format(self.division_id, ward_number)
-            )
-        yield organization
