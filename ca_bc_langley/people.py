afe7764c94634dd81a5bb896513cf2403c2d834b
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-17 11:49:53 -0400

    Remove incorrect formatting of name vanPopta in Langley scraper

--- ca_bc_langley/people.py
+++ ca_bc_langley/people.py
@@ -4,6 +4,7 @@ from utils import CanadianScraper
 COUNCIL_PAGE = "https://www.tol.ca/en/the-township/councillors.aspx"
 MAYOR_PAGE = "https://www.tol.ca/en/the-township/mayor.aspx"
 
+
 class LangleyPersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
@@ -11,33 +12,31 @@ class LangleyPersonScraper(CanadianScraper):
         councillors = page.xpath('//a[contains(@target,"_self")]/@href')
         for url in councillors:
             page = self.lxmlize(url)
-            name = page.xpath('//h1')[0].text_content().strip()
-            if name == "Misty vanPopta":
-                name = "Misty Vanpopta"
+            name = page.xpath("//h1")[0].text_content().strip()
+
             district = "Langley (seat {})".format(seat_number)
-            seat_number +=1
+            seat_number += 1
             email = self.get_email(page)
             phone = self.get_phone(page)
 
-            p =Person(primary_org="legislature",name=name,role="Councillor",district=district)
+            p = Person(primary_org="legislature", name=name, role="Councillor", district=district)
             p.add_source(COUNCIL_PAGE)
             p.add_source(url)
-            p.add_contact("email",email)
-            p.add_contact("voice",phone,"legislature")
+            p.add_contact("email", email)
+            p.add_contact("voice", phone, "legislature")
 
             yield p
         page = self.lxmlize(MAYOR_PAGE)
-        name = page.xpath('//h1')[0].text_content().replace("Mayor", "").strip()
+        name = page.xpath("//h1")[0].text_content().replace("Mayor", "").strip()
         email = self.get_email(page)
         phone = self.get_phone(page)
         address_block = page.xpath('//p/a[@rel="noopener noreferrer"]/parent::p')[0].text_content()
-        line1 = address_block[address_block.find("Facility")+8:address_block.find("Langley,")]
-        line2 = address_block[address_block.find("Langley,"):address_block.find("Phone")-1]
-        address = ", ".join([line1,line2])
-        p = Person(primary_org="legislature",name=name,role="Mayor",district="Langley")
-        p.add_contact("email",email)
-        p.add_contact("voice",phone,"legislature")
-        p.add_contact("address",address,"legislature")
+        line1 = address_block[address_block.find("Facility") + 8 : address_block.find("Langley,")]
+        line2 = address_block[address_block.find("Langley,") : address_block.find("Phone") - 1]
+        address = ", ".join([line1, line2])
+        p = Person(primary_org="legislature", name=name, role="Mayor", district="Langley")
+        p.add_contact("email", email)
+        p.add_contact("voice", phone, "legislature")
+        p.add_contact("address", address, "legislature")
         p.add_source(MAYOR_PAGE)
         yield p
-            
