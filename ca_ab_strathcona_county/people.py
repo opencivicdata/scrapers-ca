5c9685b3ae4b1a0f844bf4bc97e573d2c082f5b4
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-05-17 11:48:27 -0400

    fix(ca_ab_strathcona_county): URLs were reversed

--- ca_ab_strathcona_county/people.py
+++ ca_ab_strathcona_county/people.py
@@ -2,10 +2,8 @@ from utils import CSVScraper
 
 
 class StrathconaCountyPersonScraper(CSVScraper):
-    # https://opendata.arcgis.com/api/v3/datasets/b7b1aa0fbad6445c8aa8ab66cb13347f_0/downloads/data?format=csv&spatialRefId=3857&where=1%3D1
-    csv_url = (
-        "https://opendata-strathconacounty.hub.arcgis.com/datasets/StrathconaCounty::county-council-2021-2025/about"
-    )
+    # https://opendata-strathconacounty.hub.arcgis.com/datasets/StrathconaCounty::county-council-2021-2025/about
+    csv_url = "https://opendata.arcgis.com/api/v3/datasets/b7b1aa0fbad6445c8aa8ab66cb13347f_0/downloads/data?format=csv&spatialRefId=3857&where=1%3D1"
     corrections = {
         "district name": {
             "Strathcona": "Strathcona County",
