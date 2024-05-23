7727b5d74ef7dfd4399fd7c8ec4dbf77cc5ac21a
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-23 10:42:38 -0400

    Remove Reeve and Deputy Reeve roles for Grande Prairie County no. 1

--- ca_ab_grande_prairie_county_no_1/people.py
+++ ca_ab_grande_prairie_county_no_1/people.py
@@ -13,21 +13,13 @@ class GrandePrairieCountyNo1PersonScraper(CanadianScraper):
         councillors = councillors[3:]
 
         assert len(councillors), "No councillors found"
-        role = "Councillor"
         for councillor in councillors:
             name = councillor.xpath('.//div[@class="lb-imageBox_header {headColor}"]')[0].text_content()
             if "Reeve" in name:
                 name = name.split("Reeve")[1].strip()
             district = councillor.xpath(".//h3|.//h2")[0].text_content()
 
-            if district == "Division 5":
-                # district 5's councillor is the Reeve
-                p = Person(primary_org="legislature", name=name, district=district, role="Reeve")
-            elif district == "Division 7":
-                # district 7's councillor is the Deputy Reeve
-                p = Person(primary_org="legislature", name=name, district=district, role="Deputy Reeve")
-            else:
-                p = Person(primary_org="legislature", name=name, district=district, role=role)
+            p = Person(primary_org="legislature", name=name, district=district, role="Councillor")
 
             p.add_source(COUNCIL_PAGE)
             p.image = councillor.xpath(".//img/@src")[0]
