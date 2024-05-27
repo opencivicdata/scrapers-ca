d513ebb283799f8138ad75ff101fef1d59fc81d4
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-27 16:33:06 -0400

    Fix encoding and seat numbering for Huron County

--- ca_on_huron/people.py
+++ ca_on_huron/people.py
@@ -1,9 +1,12 @@
+import json
+
 from utils import CSVScraper
 
 
 class HuronPersonScraper(CSVScraper):
     # https://data-huron.opendata.arcgis.com/datasets/051e72a02edc4337af8ca2606ab58644_0
     csv_url = "https://opendata.arcgis.com/datasets/051e72a02edc4337af8ca2606ab58644_0.csv"
+    encoding = "utf-8-sig"
     many_posts_per_area = True
     corrections = {
         "primary role": {
@@ -11,3 +14,16 @@ class HuronPersonScraper(CSVScraper):
         },
         "phone": lambda value: value.replace(", ", ";"),
     }
+
+    # removing the seat number added to districts with just 1 Councillor
+    def scrape(self):
+        single_councillor_districts = ["Howick", "Morris-Turnberry", "North Huron"]
+        people = super().scrape()
+        for p in people:
+            data = p._related[0].post_id.replace("~", "")
+            membership = json.loads(data)
+            for district in single_councillor_districts:
+                if membership["label"] == "{} (seat 1)".format(district):
+                    membership["label"] = district
+                    p._related[0].post_id = "~" + json.dumps(membership)
+            yield p
