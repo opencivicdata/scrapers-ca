12faf9009e8b20aa37775cb118258535bdee98ac
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-27 15:38:23 -0400

    Change Welland csv url

--- ca_on_welland/people.py
+++ ca_on_welland/people.py
@@ -3,6 +3,6 @@ from utils import CSVScraper
 
 class WellandPersonScraper(CSVScraper):
     # https://niagaraopendata.ca/dataset/city-of-welland-mayor-and-council-members
-    csv_url = "https://niagaraopendata.ca/dataset/b38e2f85-bcd4-43fd-95d5-f513919514d9/resource/590c9110-01f3-4256-8ac1-9c5cbeca88c5/download/city-of-welland-mayor-and-council-members.csv"
+    csv_url = "https://opendata.arcgis.com/api/v3/datasets/e4f572b37d934bce9e73ca16a4a2e48d_1/downloads/data?format=csv&spatialRefId=4326&where=1%3D1"
     encoding = "windows-1252"
     many_posts_per_area = True
