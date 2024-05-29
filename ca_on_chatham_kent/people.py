9e52c4b26b1f2219d6469b3b8836e43b432bb63c
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-29 12:53:38 -0400

    Update Chatham Kent scraper

--- ca_on_chatham_kent/people.py
+++ ca_on_chatham_kent/people.py
@@ -1,81 +1,94 @@
 import re
 from collections import defaultdict
 
-from utils import CONTACT_DETAIL_TYPE_MAP
+import requests
+from lxml import etree
+
 from utils import CanadianPerson as Person
 from utils import CanadianScraper
 
-COUNCIL_PAGE = "https://www.chatham-kent.ca/local-government/council/council-members"
+COUNCIL_DATA_URL = "https://www.chatham-kent.ca/localgovernment/councillors/_vti_bin/Lists.asmx"
+COUNCIL_PAGE = "https://www.chatham-kent.ca/localgovernment/councillors/Pages/Councillors-by-Ward.aspx"
+MAYOR_CONTACT_PAGE = "https://www.chatham-kent.ca/localgovernment/mayor/Pages/Connect-with-the-Mayor.aspx"
 
 
 class ChathamKentPersonScraper(CanadianScraper):
     def scrape(self):
         seat_numbers = defaultdict(int)
-        voice_notes = ["legislature", "office"]
+
+        headers = {"content-type": "text/xml"}
+        body = '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><GetListItems xmlns="http://schemas.microsoft.com/sharepoint/soap/"><listName>councillorsByWard</listName><viewName></viewName><query><Query><OrderBy Override="TRUE"><FieldRef Ascending="True" Name="Title" /></OrderBy></Query></query><viewFields><ViewFields Properties="True" /></viewFields><rowLimit>50</rowLimit><queryOptions><QueryOptions></QueryOptions></queryOptions></GetListItems></soap:Body></soap:Envelope>'
+
+        response = requests.post(url=COUNCIL_DATA_URL, data=body, headers=headers)
+        page = etree.fromstring(response.content)
+        namespace = {"z": "#RowsetSchema", "rs": "urn:schemas-microsoft-com:rowset"}
+
+        councillors = page.findall(".//z:row", namespace)
+        assert len(councillors), "No councillors found"
+        for councillor in councillors:
+            title = councillor.xpath("./@ows_Title")[0]
+            ward, name = re.split(r"(?<=\d)\s", title)
+            name.replace("Councillor ", "")
+            seat_numbers[ward] += 1
+            district = "{} (seat {})".format(ward, seat_numbers[ward])
+
+            url = councillor.xpath("./@ows_URL")[0].split(",")[0]
+            page = self.lxmlize(url, user_agent="Mozilla/5.0")
+            p = Person(primary_org="legislature", name=name, district=district, role="Councillor")
+            p.add_source(COUNCIL_PAGE)
+            p.add_source(url)
+
+            p.image = page.xpath('//div[@id="mainContent"]//img/@src')[0]
+
+            address = page.xpath("//hr/following-sibling::*")[1].text_content()
+            p.add_contact("address", address, "legislature")
+            email = self.get_email(page)
+            p.add_contact("email", email)
+            phone = self.get_phone(page)
+            p.add_contact("voice", phone, "legislature")
+            yield p
 
         page = self.lxmlize(COUNCIL_PAGE)
 
-        wards = page.xpath('//div[@id="ctl00_PlaceHolderMain_ctl03__ControlWrapper_RichHtmlField"]//h4')
-        assert len(wards), "No wards found"
-        for ward in wards:
-            match = re.search(r"Ward \d+", ward.text_content())
-            if match:
-                area = match.group(0)
-                role = "Councillor"
-                number = int(re.search(r"\((\d+)", ward.text_content()).group(1))
-            else:
-                area = "Chatham-Kent"
-                role = "Mayor"
-                number = 1
+        # getting 1 councillor with incomplete information
+        if seat_numbers["Ward 3"] < 2:
+            councillor_url = page.xpath(
+                '//a[contains(@href, "Councillors-by-Ward.aspx")]/following-sibling::ul[1]/li/a/@href'
+            )[-1]
+            councillor_page = self.lxmlize(councillor_url)
 
-            councillors = ward.xpath('./following-sibling::*//a[contains(@href, "/Council/")]')[:number]
-            assert len(councillors), "No councillors found"
-            for councillor in councillors:
-                name = councillor.text_content()
-                url = councillor.attrib["href"]
-                page = self.lxmlize(url)
+            name = councillor_page.xpath("//h1")[0].text_content()
+            ward = "Ward 3"
+            seat_numbers[ward] += 1
+            district = "{} (seat {})".format(ward, seat_numbers[ward])
 
-                if role == "Councillor":
-                    seat_numbers[area] += 1
-                    district = "{} (seat {})".format(area, seat_numbers[area])
-                else:
-                    district = area
+            p = Person(primary_org="legislature", name=name, district=district, role="Councillor")
+            p.add_source(councillor_url)
+            address = councillor_page.xpath("//hr/following-sibling::*")[1].text_content()
+            p.add_contact("address", address, "legislature")
+            email = self.get_email(councillor_page)
+            p.add_contact("email", email)
+            phone = self.get_phone(councillor_page)
+            p.add_contact("voice", phone, "legislature")
+            yield p
 
-                p = Person(primary_org="legislature", name=name, district=district, role=role)
-                p.add_source(COUNCIL_PAGE)
-                p.add_source(url)
+        mayor_url = page.xpath('//@href[contains(., "Mayor-")]')[0]
+        page = self.lxmlize(mayor_url)
+        contact_page = self.lxmlize(MAYOR_CONTACT_PAGE)
 
-                image = page.xpath('//div[@id="ctl00_PlaceHolderMain_ctl03__ControlWrapper_RichHtmlField"]//img/@src')[
-                    0
-                ]
-                if "council_logo" not in image:
-                    p.image = image
+        name = page.xpath("//h1")[0].text_content().replace("Mayor ", "")
+        image = page.xpath('//img[@style="BORDER:0px solid;"]/@src')[0]
+        email = self.get_email(contact_page)
+        phone = self.get_phone(contact_page)
+        address = ",".join(
+            contact_page.xpath('//h2[contains(., "Mailing Address")]/following-sibling::p[1]/text()')[2:]
+        )
 
-                address = page.xpath(
-                    '//div[@id="div_contact_us_top_container_S"]//div[@class="div_contact_us_content_address"]'
-                )[0].text_content()
-                p.add_contact("address", address, "legislature")
+        p = Person(primary_org="legislature", name=name, district="Chatham-Kent", role="Mayor", image=image)
+        p.add_contact("email", email)
+        p.add_contact("voice", phone, "legislature")
+        p.add_contact("address", address, "legislature")
+        p.add_source(mayor_url)
+        p.add_source(MAYOR_CONTACT_PAGE)
 
-                contacts = page.xpath(
-                    '//div[@id="div_contact_us_top_container_S"]//div[@class="div_contact_us_content_kv"]/div'
-                )
-                voice_note_index = 0
-                for contact in contacts:
-                    content = contact.text_content().strip()
-                    if not content:
-                        continue
-
-                    contact_type, contact = contact.text_content().split(":")
-                    contact_type = CONTACT_DETAIL_TYPE_MAP[contact_type.strip()]
-
-                    if contact_type == "voice":
-                        note = voice_notes[voice_note_index]
-                        voice_note_index += 1
-                    elif contact_type == "email":
-                        note = ""
-                    else:
-                        note = "legislature"
-
-                    p.add_contact(contact_type, contact.strip(), note)
-
-                yield p
+        yield p
