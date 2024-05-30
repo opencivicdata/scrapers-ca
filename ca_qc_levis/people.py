471484292cb1d5458deb513460c597912424502d
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-30 16:55:42 -0400

    Add councillors for Levis arrondissments

--- ca_qc_levis/people.py
+++ ca_qc_levis/people.py
@@ -3,13 +3,27 @@ from utils import CanadianPerson as Person
 from utils import CanadianScraper
 
 COUNCIL_PAGE = "https://www.ville.levis.qc.ca/la-ville/conseil-municipal/elus/"
+ARRONDISSEMENTS_PAGE = "https://www.ville.levis.qc.ca/la-ville/arrondissements/conseils/"
 
 
 class LevisPersonScraper(CanadianScraper):
     def scrape(self):
-        page = self.lxmlize(COUNCIL_PAGE)
+        council_page = self.lxmlize(COUNCIL_PAGE)
+        arrondissements_page = self.lxmlize(ARRONDISSEMENTS_PAGE)
 
-        councillors = page.xpath('//div[@class="drawers"]//div[@class="dropdown"]')
+        presidents = {}
+        for president in arrondissements_page.xpath('//p[contains(./b, "Président")]'):
+            arrondissement = (
+                president.xpath('./ancestor::div[@class="drawer"]//a')[0]
+                .text_content()
+                .replace("Arrondissement ", "")
+                .replace("des", "Les")
+                .replace("de ", "")
+            )
+            name = president.xpath("./text()")[0].strip().replace("\xa0", " ")
+            presidents[name] = arrondissement
+
+        councillors = council_page.xpath('//div[@class="drawers"]//div[@class="dropdown"]')
         assert len(councillors), "No councillors found"
         for person in councillors:
             position, name = person.xpath("./h3/text()")[0].replace("–", "-").split(" - ")
@@ -27,6 +41,19 @@ class LevisPersonScraper(CanadianScraper):
 
             p = Person(primary_org="legislature", name=name, district=district, role=role)
             p.add_source(COUNCIL_PAGE)
+
+            if name in presidents:
+                person = Person(primary_org="legislature", name=name, district=presidents[name], role=role)
+                person.add_source(COUNCIL_PAGE)
+                person.image = photo_url
+                person.add_source(ARRONDISSEMENTS_PAGE)
+                person.add_contact("email", email)
+
+                p.add_source(ARRONDISSEMENTS_PAGE)  # making sure the sources match for both memberships
+
+                yield person
+
             p.image = photo_url
             p.add_contact("email", email)
+
             yield p
