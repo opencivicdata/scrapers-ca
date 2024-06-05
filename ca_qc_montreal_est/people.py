4d277796baceb35c8dfcd919a4fc10cb60d32da9
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-06-05 15:17:00 -0400

    Update Montreal Est scraper

--- ca_qc_montreal_est/people.py
+++ ca_qc_montreal_est/people.py
@@ -1,28 +1,26 @@
 from utils import CanadianPerson as Person
 from utils import CanadianScraper
 
-COUNCIL_PAGE = "http://ville.montreal-est.qc.ca/la-ville/conseil-municipal/conseils-municipaux/"
+COUNCIL_PAGE = "https://ville.montreal-est.qc.ca/vie-democratique/conseil-municipal/"
 
 
 class MontrealEstPersonScraper(CanadianScraper):
     def scrape(self):
         page = self.lxmlize(COUNCIL_PAGE)
-
-        councillors = page.xpath("//table")
+        councillors = page.xpath("//div[contains(@id, 'membres-conseil-block_')]")
         assert len(councillors), "No councillors found"
         for councillor in councillors:
-            name = councillor.xpath(".//h3")[0].text_content()
+            name, role_district = councillor.xpath(".//span[@class='h3 d-block']")[0].text_content().split(" – ", 1)
 
-            if "maire" in name:
-                name = name.split(" ", 2)[-1]
+            if "Maire" in role_district:
                 district = "Montréal-Est"
                 role = "Maire"
             else:
-                district = "District {}".format(councillor.xpath(".//h3")[1].text_content()[-1])
+                district = "District {}".format(role_district[-1])
                 role = "Conseiller"
 
             p = Person(primary_org="legislature", name=name, district=district, role=role)
-            p.image = councillor.xpath(".//@src")[0]
+            p.image = councillor.xpath(".//@data-lazy-src")[0]
             p.add_contact("email", self.get_email(councillor))
             p.add_source(COUNCIL_PAGE)
             yield p
