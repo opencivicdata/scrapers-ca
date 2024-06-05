018656639d13d755316feb408d8c71fd097744f4
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-06-05 18:40:08 -0400

    Update Saguenay scraper

--- ca_qc_saguenay/people.py
+++ ca_qc_saguenay/people.py
@@ -10,14 +10,12 @@ class SaguenayPersonScraper(CanadianScraper):
     def scrape(self):
         mayor_page = self.lxmlize(MAYOR_PAGE)
         contact_page = self.lxmlize(CONTACT_PAGE)
-
-        name = mayor_page.xpath('//span/text()[contains(., "maire")]')[0].split(", ", 1)[0]
+        name = mayor_page.xpath('//a[contains(., "maire")]/span/text()')[0]
         p = Person(primary_org="legislature", name=name, district="Saguenay", role="Maire")
         p.add_source(MAYOR_PAGE)
         p.add_source(CONTACT_PAGE)
         node = contact_page.xpath('//h2[contains(., "Coordonnées du cabinet")]/following-sibling::p')[1]
         p.add_contact("voice", self.get_phone(node, area_codes=[418]), "legislature")
-        p.add_contact("email", self.get_email(node))
         yield p
 
         page = self.lxmlize(COUNCIL_PAGE)
@@ -26,6 +24,19 @@ class SaguenayPersonScraper(CanadianScraper):
         for councillor in councillors:
             district = councillor.xpath("./h3/text()")[0].replace("#", "")
             name = councillor.xpath(".//p/text()")[0]
+            borough = None
+            borough_node = councillor.xpath(".//p/strong")
+            if borough_node:
+                text = borough_node[0].text_content()
+                if "Président" in text:
+                    borough = text.replace("Président de l'arrondissement de ", "")
+
+            if borough:
+                p = Person(primary_org="legislature", name=name, district=borough, role="Conseiller")
+                p.add_source(COUNCIL_PAGE)
+                p.add_contact("voice", self.get_phone(councillor), "legislature")
+                p.add_contact("email", self.get_email(councillor))
+                yield p
 
             p = Person(primary_org="legislature", name=name, district=district, role="Conseiller")
             p.add_source(COUNCIL_PAGE)
