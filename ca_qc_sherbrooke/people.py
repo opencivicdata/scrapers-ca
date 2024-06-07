4e974c07f34a9191ddf373710b80fc6ffd03e2c6
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-06-07 16:44:47 -0400

    fix: scrape Presidents of Arrondissements in Sherbrooke

--- ca_qc_sherbrooke/people.py
+++ ca_qc_sherbrooke/people.py
@@ -26,6 +26,7 @@ class SherbrookePersonScraper(CanadianScraper):
         councillors = page.xpath("//a[.//h3]")
         assert len(councillors), "No councillors found"
         for councillor in councillors:
+            districts = []
             name = councillor.xpath(".//h3")[0].text_content()
             role = councillor.xpath('.//div[@class="poste"]')[0].text_content()
 
@@ -33,9 +34,17 @@ class SherbrookePersonScraper(CanadianScraper):
                 role = "Maire"
                 district = "Sherbrooke"
             else:
-                role = "Conseiller"
                 district = councillor.xpath('.//div[@class="district"]')[0].text_content()
                 district = clean_french_prepositions(district).replace("District", "").strip()
+                if "président" in role:
+                    borough = councillor.xpath('.//div[@class="bloc_bas"]/p')[0].text_content()
+                    borough = clean_french_prepositions(borough).replace("Arrondissement", "").strip()
+
+                    if borough == "Brompton-Rock Forest-Saint-\u00c9lie-Deauville":
+                        borough = "Brompton–Rock Forest–Saint-Élie–Deauville"  # N-dashes
+                    if borough != district:  # Lennoxville
+                        districts.append(borough)
+                role = "Conseiller"
 
             if district == "Lac-Magog":
                 district = "Lac Magog"
@@ -43,17 +52,20 @@ class SherbrookePersonScraper(CanadianScraper):
             url = "https://www.sherbrooke.ca" + councillor.xpath("./@href")[0]
             page = get_content(url)
 
-            p = Person(primary_org="legislature", name=name, district=district, role=role)
-            p.add_source(COUNCIL_PAGE)
-            p.add_source(url)
+            phone = self.get_phone(page, error=False)
+            email = self.get_email(page, error=False)
             image = councillor.xpath(".//@src")[0]
             if "https://" not in image:
                 image = "https://contenu.maruche.ca" + image
-            p.image = image
-            phone = self.get_phone(page, error=False)
-            email = self.get_email(page, error=False)
-            if email:
-                p.add_contact("email", email)
-            if phone:
-                p.add_contact("voice", phone, "legislature")
-            yield p
+
+            for district in districts:
+                p = Person(primary_org="legislature", name=name, district=district, role=role)
+                p.add_source(COUNCIL_PAGE)
+                p.add_source(url)
+                p.image = image
+
+                if email:
+                    p.add_contact("email", email)
+                if phone:
+                    p.add_contact("voice", phone, "legislature")
+                yield p
