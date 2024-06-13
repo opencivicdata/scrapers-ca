0ae367b2b815b1dcbcbf6d88a23fe0923716c78e
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-06-13 17:11:15 -0400

    fix Ottawa scraper and update Ottawa divisions

--- ca_on_ottawa/people.py
+++ ca_on_ottawa/people.py
@@ -1,12 +1,27 @@
+import csv
+from io import BytesIO, StringIO
+
+import agate
+
 from utils import CSVScraper
 
 
 class OttawaPersonScraper(CSVScraper):
-    # http://data.ottawa.ca/dataset/elected-officials
-    csv_url = "http://data.ottawa.ca/dataset/fd26ae83-fe1a-40d8-8951-72df40021c82/resource/3cd1b14d-cb45-4c4d-b22a-a607946e2ec2/download/elected-officials-2018-2022.csv"
-    encoding = "utf-8-sig"
-    corrections = {
-        "district name": {
-            "Orl\u0082ans": "Orléans",
-        },
-    }
+    # https://open.ottawa.ca/documents/ottawa::elected-officials-2022-2026/about
+    csv_url = "https://www.arcgis.com/sharing/rest/content/items/a5e9dc2425274bb796d3ded47b0d7b00/data"
+    fallbacks = {"district name": "ward name"}
+
+    # Workaround for the download link not having the correct extension
+    def csv_reader(self, url, delimiter=",", header=False, encoding=None, skip_rows=0, data=None, **kwargs):
+        data = StringIO()
+        binary = BytesIO(self.get(url).content)
+        table = agate.Table.from_xls(binary)
+        table.to_csv(data)
+        data.seek(0)
+        if skip_rows:
+            for _ in range(skip_rows):
+                data.readline()
+        if header:
+            return csv.DictReader(data, delimiter=delimiter)
+        else:
+            return csv.reader(data, delimiter=delimiter)
