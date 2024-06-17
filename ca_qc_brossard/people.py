36ebfc840f6e81588929dd28055a041c0b25a9c4
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-06-17 12:36:22 -0400

    Fix Brossard scraper

--- ca_qc_brossard/people.py
+++ ca_qc_brossard/people.py
@@ -1,49 +1,108 @@
+import json
 import re
+
+import requests
 
 from utils import CanadianPerson as Person
 from utils import CanadianScraper
 
-COUNCIL_PAGE = "http://www.ville.brossard.qc.ca/Ma-ville/conseil-municipal/Municipal-council.aspx?lang=en-ca"
-CONTACT_PAGE = "http://www.ville.brossard.qc.ca/Ma-ville/conseil-municipal/Municipal-council/Municipal-council-members-%E2%80%93-Contact-information.aspx"
+DATA_PAGE = "https://www.brossard.ca/in/rest/public/contentGraphByPath?locale=fr&path=/elus-municipaux&propertyFilter=backup,site"
+COUNCIL_PAGE = "https://www.brossard.ca/elus-municipaux"
 
 
 class BrossardPersonScraper(CanadianScraper):
     def scrape(self):
-        page = self.lxmlize(COUNCIL_PAGE)
-        contact_page = self.lxmlize(CONTACT_PAGE)
+        def indexById(elementList):
+            result = {}
+            for element in elementList:
+                id = element["id"]
+                result[id] = element
+            return result
 
-        councillors = page.xpath('//a[contains(@class, "slide item-")]')
-        emails = contact_page.xpath('//a[contains(@href, "mailto:")]')
+        # Gets the ids of all children elements recursively
+        def getChildren(parentId, elementDict):
+            returnList = []
+            element = elementDict[parentId]
+            if element.get("children"):
+                for child in element.get("children"):
+                    if not re.search(r"^\d+$", child):
+                        continue
+                    returnList.append(child)
+                    if getChildren(child, elementDict):
+                        returnList.extend(getChildren(child, elementDict))
+            return returnList
+
+        # The whole page is rendered in javascript and stored as a massive json object
+        page = requests.get(DATA_PAGE)
+        page = json.loads(page.content)
+        containers = page["content"].values()
+        for container in containers:
+            if container.get("contentType") != "CMSPage":
+                continue
+            elements = indexById(container["properties"]["content"]["data"])
+
+        councillors = []
+        for element in elements.values():
+            if isinstance(element.get("children"), dict) and re.search(
+                r"DISTRICT \d+\s+[-|]\sSecteur", element.get("children").get("fr")
+            ):
+                councillors.append(element)
 
         assert len(councillors), "No councillors found"
         for councillor in councillors:
-            name = councillor.xpath('.//div[@class="titre"]/text()')[0]
-            if name == "Poste vacant":
-                continue
-            if name == "Sylvie Desgroseilliers":
-                name = "Sylvie DesGroseilliers"
+            district = re.search(r"DISTRICT (\d+)", councillor["children"]["fr"]).group(0).title()
+            parent_id = councillor["parent"]
+            children = getChildren(parent_id, elements)
+            phone = None
+            for id in children:
+                child = elements[id]
+                if child["tag"] == "Link":
+                    email = child["props"]["link"]["options"]["url"]["fr"].split(":")[1]
+                elif child["tag"] == "Image":
+                    photo = "https://www.brossard.ca/in/rest/public/AttachmentThumb?id=" + child["children"]["fr"]
+                elif child["tag"] == "TextBox":
+                    if not isinstance(child["children"], dict) or "DISTRICT" in child["children"]["fr"]:
+                        continue
+                    text = re.search(r"(?<=>).+(?=<)", child["children"]["fr"]).group(0)
+                    if child["parent"] == parent_id and "Conseill" not in text:
+                        name = text.replace("&nbsp;", "")
+                    elif not phone:
+                        phone_pattern = re.search(r"\d{3} \d{3}-\d{4}(, poste \d{4})?", text)
+                        if phone_pattern:
+                            phone = phone_pattern.group(0)
 
-            position = councillor.xpath('.//div[@class="poste"]/text()')[0]
-            role = "Conseiller"
-
-            district = re.search(r"District \d+", position)
-            if "Mayor" in position:
-                district = "Brossard"
-                role = "Maire"
-            else:
-                district = district.group(0)
-
-            photo = re.search(r"url\((.+)\)", councillor.attrib["style"]).group(1)
-
-            p = Person(primary_org="legislature", name=name, district=district, role=role, image=photo)
-            p.add_source(COUNCIL_PAGE)
-            p.add_source(CONTACT_PAGE)
-
-            index = [i for i, link in enumerate(emails) if name in link.text_content().replace("\u2019", "'")][0]
-            email = emails[index + 1]
-            p.add_contact("email", re.match("mailto:(.+@brossard.ca)", email.attrib["href"]).group(1))
-            phone = email.xpath('./preceding-sibling::text()[contains(., "450")]')
-            phone = phone[-1]
+            p = Person(primary_org="legislature", name=name, district=district, role="Conseiller", image=photo)
+            p.add_contact("email", email)
             p.add_contact("voice", phone, "legislature")
+            p.add_source(COUNCIL_PAGE)
 
             yield p
+
+        for element in elements.values():
+            if (
+                isinstance(element.get("children"), dict)
+                and re.search(r"MAIRE", element.get("children").get("fr"))
+                and not element.get("children").get("en")
+            ):
+                mayor = element
+        parent_id = mayor["parent"]
+        children = getChildren(parent_id, elements)
+        phone = None
+        for id in children:
+            child = elements[id]
+            if child["tag"] == "Image":
+                photo = "https://www.brossard.ca/in/rest/public/AttachmentThumb?id=" + child["children"]["fr"]
+            elif child["tag"] == "TextBox":
+                if not isinstance(child["children"], dict) or "MAIRE" in child["children"]["fr"]:
+                    continue
+                text = re.search(r"(?<=>).+(?=<)", child["children"]["fr"]).group(0)
+                if child["parent"] == parent_id:
+                    name = text.replace("&nbsp;", "")
+                elif not phone:
+                    phone_pattern = re.search(r"\d{3} \d{3}-\d{4}(, poste \d{4})?", text)
+                    if phone_pattern:
+                        phone = phone_pattern.group(0)
+        p = Person(primary_org="legislature", name=name, district="Brossard", role="Maire", image=photo)
+        p.add_contact("voice", phone, "legislature")
+        p.add_source(COUNCIL_PAGE)
+        yield p
