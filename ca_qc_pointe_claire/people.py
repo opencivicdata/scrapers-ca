f2a3ad0ba0a522cc7608b61f6567d93f216917b9
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-06-25 14:51:58 -0400

    fix(ca_qc_pointe_claire): Get photo url correctly

--- ca_qc_pointe_claire/people.py
+++ ca_qc_pointe_claire/people.py
@@ -26,7 +26,7 @@ class PointeClairePersonScraper(CanadianScraper):
                 assert False, "error parsing district"
 
             p = Person(primary_org="legislature", name=name, district=district, role=role)
-            p.image = councillor.xpath(".//@src")[0]
+            p.image = councillor.xpath(".//@data-src")[0]
             p.add_contact("email", self.get_email(councillor))
             p.add_contact("voice", self.get_phone(councillor, area_codes=[514]), "legislature")
             p.add_source(COUNCIL_PAGE)
