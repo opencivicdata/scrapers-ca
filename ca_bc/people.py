6de91eeaf3d6620590310fc2d95f0a1f587e47b6
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-11-18 15:56:58 -0500

    ca_bc: Use multiline string for readability

--- ca_bc/people.py
+++ ca_bc/people.py
@@ -1,21 +1,53 @@
 import json
-import requests
+from textwrap import dedent
 
 from utils import CanadianPerson as Person
 from utils import CanadianScraper
 
 COUNCIL_PAGE = "https://www.leg.bc.ca/members"
-JSON = {
-    "query": "query GetMLAsByConstituency($parliamentId: Int!) {\n  allMemberParliaments(condition: {parliamentId: $parliamentId, active: true}) {\n    nodes {\n      image: imageBySmallImageId {\n        path\n        description\n        __typename\n      }\n      constituency: constituencyByConstituencyId {\n        name\n        __typename\n      }\n      member: memberByMemberId {\n        firstName\n        lastName\n        __typename\n      }\n      isCounsel\n      isDoctor\n      isHonourable\n      party: partyByPartyId {\n        name\n        abbreviation\n        __typename\n      }\n      nodeId\n      __typename\n    }\n    __typename\n  }\n}",
-    "variables": {"parliamentId": 43},
-}
 
 
 class BritishColumbiaPersonScraper(CanadianScraper):
     def scrape(self):
-        response = requests.post(url="https://lims.leg.bc.ca/graphql", json=JSON)
+        response = self.post(url="https://lims.leg.bc.ca/graphql", json={
+            "query": dedent("""\
+                query GetMLAsByConstituency($parliamentId: Int!) {
+                  allMemberParliaments(condition: {parliamentId: $parliamentId, active: true}) {
+                    nodes {
+                      image: imageBySmallImageId {
+                        path
+                        description
+                        __typename
+                      }
+                      constituency: constituencyByConstituencyId {
+                        name
+                        __typename
+                      }
+                      member: memberByMemberId {
+                        firstName
+                        lastName
+                        __typename
+                      }
+                      isCounsel
+                      isDoctor
+                      isHonourable
+                      party: partyByPartyId {
+                        name
+                        abbreviation
+                        __typename
+                      }
+                      nodeId
+                      __typename
+                    }
+                    __typename
+                  }
+                }"""
+            ),
+            "variables": {"parliamentId": 43},
+        })
         data = json.loads(response.content.decode("utf-8"))
         members = data["data"]["allMemberParliaments"]["nodes"]
+
         assert len(members), "No members found"
         for member in members:
             image = "https://lims.leg.bc.ca/public" + member["image"]["path"]
