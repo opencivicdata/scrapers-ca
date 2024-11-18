4c6e6d493043f43599c5c434080caef058309596
bzhangjma <brighten.zhang@jmaconsulting.biz>
2024-11-18 15:09:47 -0500

    Update people.py

--- ca_bc/people.py
+++ ca_bc/people.py
@@ -5,15 +5,15 @@ from utils import CanadianPerson as Person
 from utils import CanadianScraper
 
 COUNCIL_PAGE = "https://www.leg.bc.ca/members"
-query = """
-query GetMLAsByConstituency($parliamentId: Int!) {\n  allMemberParliaments(condition: {parliamentId: $parliamentId, active: true}) {\n    nodes {\n      image: imageBySmallImageId {\n        path\n        description\n        __typename\n      }\n      constituency: constituencyByConstituencyId {\n        name\n        __typename\n      }\n      member: memberByMemberId {\n        firstName\n        lastName\n        __typename\n      }\n      isCounsel\n      isDoctor\n      isHonourable\n      party: partyByPartyId {\n        name\n        abbreviation\n        __typename\n      }\n      nodeId\n      __typename\n    }\n    __typename\n  }\n}
-"""
-variables = {"parliamentId": 43}
+JSON = {
+    "query": "query GetMLAsByConstituency($parliamentId: Int!) {\n  allMemberParliaments(condition: {parliamentId: $parliamentId, active: true}) {\n    nodes {\n      image: imageBySmallImageId {\n        path\n        description\n        __typename\n      }\n      constituency: constituencyByConstituencyId {\n        name\n        __typename\n      }\n      member: memberByMemberId {\n        firstName\n        lastName\n        __typename\n      }\n      isCounsel\n      isDoctor\n      isHonourable\n      party: partyByPartyId {\n        name\n        abbreviation\n        __typename\n      }\n      nodeId\n      __typename\n    }\n    __typename\n  }\n}",
+    "variables": {"parliamentId": 43},
+}
 
 
 class BritishColumbiaPersonScraper(CanadianScraper):
     def scrape(self):
-        response = requests.post(url="https://lims.leg.bc.ca/graphql", json={"query": query, "variables": variables})
+        response = requests.post(url="https://lims.leg.bc.ca/graphql", json=JSON)
         data = json.loads(response.content.decode("utf-8"))
         members = data["data"]["allMemberParliaments"]["nodes"]
         assert len(members), "No members found"
