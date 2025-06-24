30a102cdef6fb747fcf2725ee88a6d0ccd14b824
Rafe Murray <rafe.murray@jmaconsulting.biz>
2025-06-24 09:09:07 -0400

    chore: use cloudscrape method

--- ca_yt/people.py
+++ ca_yt/people.py
@@ -1,6 +1,3 @@
-import cloudscraper
-from lxml import html
-
 from utils import CanadianPerson as Person
 from utils import CanadianScraper
 
@@ -11,20 +8,14 @@ COUNCIL_PAGE = "https://yukonassembly.ca/mlas"
 # https://developers.cloudflare.com/fundamentals/reference/policies-compliances/cloudflare-cookies/
 class YukonPersonScraper(CanadianScraper):
     def scrape(self):
-        scraper = cloudscraper.create_scraper()
-        response = scraper.get(COUNCIL_PAGE)
-        page = html.fromstring(response.content)
-        # page = self.lxmlize(COUNCIL_PAGE)
+        page = self.cloudscrape(COUNCIL_PAGE)
 
         members = page.xpath('//*[@id="block-views-block-members-listing-block-1"]/div/div/div[2]/div')
         assert len(members), "No members found"
         for member in members:
             if "Vacant" not in member.xpath("./div/span")[0].text_content():
                 url = member.xpath("./div/span/a/@href")[0].strip()
-                url = "https://yukonassembly.ca" + url
-                response = scraper.get(url)
-                page = html.fromstring(response.content)
-                # page = self.lxmlize(url)
+                page = self.cloudscrape(url)
                 name = page.xpath("//html/body/div[1]/div/div/section/div[2]/article/div/h1/span/span")[
                     0
                 ].text_content()
